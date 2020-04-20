c16338a9db99fee11dc1545c4434e0b89fbadba1
Sebastian Korfmann <sebastian@korfmann.net>
2020-04-20 20:10:52 +0200

    Show help on error for "cdktf get"

--- packages/cdktf-cli/bin/cmds/get.ts
+++ packages/cdktf-cli/bin/cmds/get.ts
@@ -21,6 +21,7 @@ class Command implements yargs.CommandModule {
   public readonly describe = 'Generate CDK Constructs for Terraform providers and modules.';
 
   public readonly builder = (args: yargs.Argv) => args
+    .showHelpOnFail(true)
     .option('providers', { default: config.terraformProviders, desc: 'Generate Constructs for given providers. Example: "aws@~> 2.5"', alias: 'p', type: 'array' })
     .option('modules', { default: config.terraformModules, desc: 'Generate Constructs for given modules. Example: "terraform-aws-modules/vpc/aws"', alias: 'm', type: 'array' })
     .option('output', { default: DEFAULT_OUTDIR, type: 'string', desc: 'Output directory', alias: 'o' })
