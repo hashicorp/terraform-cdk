10f23be96ad382f558dee15de378a8f01ba5b7c9
Sebastian Korfmann <sebastian@korfmann.net>
2020-04-21 21:36:12 +0200

    Initial behaviour for resource schema generator

---
+++ packages/cdktf-cli/test/get/generator/complex-computed-types.test.ts
@@ -0,0 +1,16 @@
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+import { TerraformGenerator } from '../../../lib/get/generator/provider-generator';
+import { CodeMaker } from 'codemaker';
+
+test.only('generate an acm certifacte resource with complex computed types', async () => {
+  const code = new CodeMaker()
+  const workdir = fs.mkdtempSync(path.join(os.tmpdir(), 'complex-computed-types.test'));
+  const spec = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'aws_acm_certificate.test.fixture.json'), 'utf-8'));
+  new TerraformGenerator(code, spec);
+  await code.save(workdir);
+
+  const output = fs.readFileSync(path.join(workdir, 'providers/aws/acm-certificate.ts'), 'utf-8');
+  expect(output).toMatchSnapshot();
+});
