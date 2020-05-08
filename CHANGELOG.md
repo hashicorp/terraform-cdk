20c3c0909cb9c48c547e12672d897b40cd97627a
Anubhav Mishra <anubhavmishra@users.noreply.github.com>
2020-05-08 12:39:01 -0700

    Release packages (#54)
    * github registry release
    
    * skipping tests for now
    
    * fixing permissions on scripts
    
    * adding changelog parser as a dev dependency
    
    * changelog
    
    * using personal access token to publishing to github registry
    
    * adding owner to npm registry url
    
    * reverting npm registry url change
    
    * using actions to publish to github npm registry
    
    * fix version
    
    * using npm registry to publish packages
    
    * build workflow added and release updated to only run on master
    
    * Update CHANGELOG.md
    
    * Update CONTRIBUTING and LICENSE
    
    * Update license
    
    * Include issue template and code of conduct
    
    * Update to new repo name
    
    Co-authored-by: Rosemary Wang <915624+joatmon08@users.noreply.github.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+
+Initial release of `cdktf-cli` and `cdktf` to Github and NPM registry.
---
+++ tools/release-github.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# create a github release
+# Mostly copied from https://github.com/awslabs/cdk8s/blob/master/tools/release-github.sh
+
+set -euo pipefail
+
+root=$(cd $(dirname $0)/.. && pwd)
+cd ${root}
+
+repo="hashicorp/terraform-cdk"
+version="$(node -p "require('./package.json').version")"
+
+# skip if we already have a github release for this version
+echo "checking if we already have a release for ${version}"
+if curl --silent --fail -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${repo}/releases/tags/v${version}; then
+  echo "release already exists"
+  exit 0
+fi
+
+entry="/tmp/changelog.txt"
+tools/extract-changelog > ${entry}
+
+# prepare request body
+node <<HERE
+const fs = require('fs');
+const req = {
+  "tag_name": "v${version}",
+  "name": "v${version}",
+  "body": fs.readFileSync('${entry}', 'utf-8')
+};
+fs.writeFileSync('/tmp/req.json', JSON.stringify(req, undefined, 2));
+HERE
+
+echo "creating github release for ${version}"
+curl --request POST \
+  --header "Authorization: token ${GITHUB_TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data @/tmp/req.json \
+  https://api.github.com/repos/${repo}/releases
