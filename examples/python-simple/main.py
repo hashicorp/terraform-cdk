213150c56b25de9fbbad2f92120e6c8130649d9c
Sebastian Korfmann <sebastian@korfmann.net>
2020-05-10 22:29:03 +0200

    Make python example work

--- examples/python-simple/main.py
+++ examples/python-simple/main.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 from constructs import Construct
 from cdktf import App, TerraformStack
-from imports.aws import SnsTopic
+from imports.aws import SnsTopic, AwsProvider
+from imports.terraform_aws_modules.vpc.aws import Vpc
 
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
 
-        SnsTopic(self, 'Topic', display_name='my-first-sns-topic')
+        AwsProvider(self, 'Aws', region='eu-central-1')
 
+        Vpc(self, 'CustomVpc',
+        name='custom-vpc',
+        cidr='10.0.0.0/16',
+        azs=["us-east-1a", "us-east-1b"],
+        public_subnets=["10.0.1.0/24", "10.0.2.0/24"]
+        )
+        SnsTopic(self, 'Topic', display_name='my-first-sns-topic')
 
 app = App()
 MyStack(app, "python-simple")
