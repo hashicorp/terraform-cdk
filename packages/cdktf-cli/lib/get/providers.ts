f06816dfc4d05c67c3a861d61d0fd99433aad179
Sebastian Korfmann <sebastian@korfmann.net>
2020-05-12 22:47:53 +0200

    Write terraform.tf.json to codemaker directory on `get`
    This also sets the `TF_PLUGIN_CACHE_DIR` so the providers
    are preserved for subsequent runs. Should be respected during
    synth as well, so that a config file is written to synth output

--- packages/cdktf-cli/lib/get/providers.ts
+++ packages/cdktf-cli/lib/get/providers.ts
@@ -1,25 +1,44 @@
 // generates constructs from terraform providers schema
 import * as fs from 'fs-extra';
+import * as path from 'path';
 import { TerraformGenerator } from './generator/provider-generator';
 import { ProviderSchema, readSchema } from './generator/provider-schema';
 import { promisify } from 'util';
 import { CodeMaker } from 'codemaker';
 import { GetBase } from './base'
 
-const mkdirp = promisify(fs.mkdirp);
+const writeFile = promisify(fs.writeFile);
 
 export class GetProvider extends GetBase {
-  protected async generateTypeScript(code: CodeMaker, providers: string[], outdir: string): Promise<void> {
-    const schema = await this.fetchSchema(providers, outdir)
+  protected async generateTypeScript(code: CodeMaker, providers: string[], codeMakerOutput: string): Promise<void> {
+    const schema = await this.fetchSchema(providers)
     new TerraformGenerator(code, schema);
+    await this.writeLockFile(providers, codeMakerOutput)
   }
 
-  private async fetchSchema(providers: string[], outdir: string): Promise<ProviderSchema> {
-    await mkdirp(outdir);
-    return readSchema(outdir, providers);
+  private async fetchSchema(providers: string[]): Promise<ProviderSchema> {
+    return readSchema(providers);
   }
 
   protected typesPath(name: string): string {
     return `providers/${name}/index`;
   }
+
+  private async writeLockFile(providers: string[], codeMakerOutput: string) {
+    const terraformFile = path.join(codeMakerOutput, 'terraform.tf.json')
+    const provider: { [name: string]: string } = { };
+    for (const p of providers) {
+      const [ name, version ] = p.split('@');
+      provider[name] = version;
+    }
+
+    const versionConstraints = {
+      terraform: {
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        required_providers: provider
+      }
+    }
+    await writeFile(terraformFile, JSON.stringify(versionConstraints));
+  }
+
 }
