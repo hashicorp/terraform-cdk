215a3b1845f05fbe78888441527d037e3b1fce18
Anubhav Mishra <anubhavmishra@users.noreply.github.com>
2020-06-26 17:16:02 -0700

    Terraform cloud support (#107)
    * wip terraform cloud
    
    * adding terraform cloud client and validating credentials
    
    * init command implementation with Terraform Cloud configuration (wip)
    
    * added Terraform Cloud support for templates and ui improvements for init cmd
    
    * python stack example for terraform cloud and test error fixes
    
    * fixing test by adding two options for project name and description
    
    * adding a local option to allow users to skip terraform cloud configuration
    
    * fix token declaration
    
    * only show interactive text when project name and description aren't set
    
    * `yargs` doesn't support `import * as yargs from 'yargs';`
    
    This leads to weird, really hard to debug errors. https://github.com/yargs/yargs/issues/1432
    
    * improve Terraform Cloud related text during init and help now works for sub commands
    
    * fixing tests
    
    * addition comments
    
    Co-authored-by: Sebastian Korfmann <sebastian@korfmann.net>

---
+++ packages/cdktf-cli/bin/cmds/login.ts
@@ -0,0 +1,42 @@
+import yargs from 'yargs'
+import { TerraformLogin } from './helper/terraform-login'
+import * as terraformCloudClient from './helper/terraform-cloud-client'
+import * as chalk from 'chalk';
+const chalkColour = new chalk.Instance();
+
+class Command implements yargs.CommandModule {
+    public readonly command = 'login';
+    public readonly describe = 'Retrieves an API token to connect to Terraform Cloud.';
+    public readonly builder = (args: yargs.Argv) => args
+        .showHelpOnFail(true)
+
+    public async handler(argv: any) {
+        const args = argv as yargs.Arguments
+        if (args["_"].length > 1) {
+            console.error(chalkColour`{redBright ERROR: 'cdktf login' command cannot have more than one argument.}\n`);
+            yargs.showHelp();
+            process.exit(1);
+        }
+
+        const terraformLogin = new TerraformLogin
+        const token = await terraformLogin.askToLogin();
+        if (token == "") {
+            console.error(chalkColour`{redBright ERROR: couldn't configure Terraform Cloud credentials.}\n`);
+            process.exit(1);
+        }
+
+        // Get user details if token is set
+        const userAccount = await terraformCloudClient.getAccountDetails(token)
+        if (userAccount) {
+            const username = userAccount.data.attributes.username;
+            console.log(chalkColour`\n{greenBright cdktf has successfully configured Terraform Cloud credentials!}`);
+            console.log(chalkColour`\nWelcome {bold ${username}}!`);
+        } else {
+            console.error(chalkColour`{redBright ERROR: couldn't configure Terraform Cloud credentials.}\n`);
+            process.exit(1);
+        }
+    }
+}
+
+
+module.exports = new Command();
