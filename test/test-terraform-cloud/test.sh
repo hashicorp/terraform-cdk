816b759bd4075afed757ff747e3ed33a837d2826
Anubhav Mishra <anubhavmishra@me.com>
2020-07-15 14:15:21 -0700

    wip tests

---
+++ test/test-terraform-cloud/test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+# Disable spinner even when we have a TTY
+export CI='1'
+
+scriptdir=$(cd $(dirname $0) && pwd)
+
+cd $(mktemp -d)
+mkdir test && cd test
+
+# initialize an empty project
+# currently, we initialize this as a local project but we will use stack overrides
+# to define remote state backend and test
+# the deploy which should call into Terraform
+# Cloud for the remote state.
+cdktf init --template typescript --project-name="typescript-test" --project-description="typescript test app" --local
+
+# put some code in it
+cp ${scriptdir}/main.ts .
+
+# add null provider
+cp ${scriptdir}/cdktf.json .
+cdktf get
+
+# diff
+cdktf deploy --auto-approve > output
+diff output ${scriptdir}/expected/output
+
+# destroy
+cdktf destroy --auto-approve 
+
+echo "PASS"
