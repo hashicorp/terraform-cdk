b2b2e98ccf27e5393ce85c55ec54043992699010
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2020-07-15 15:31:00 -0700

    Example app with Docker and Nginx
    Uses free resources to run an Nginx container which can be viewed at http://localhost:8000/

---
+++ examples/typescript-docker/main.ts
@@ -0,0 +1,45 @@
+/*
+
+Starts a Docker container with an Nginx server.
+
+Steps:
+
+- Start Docker on your machine
+- Run `cdktf get` to install dependencies
+- Run `cdktf deploy` to run the Nginx container
+- Visit http://localhost:8000/ to view the Nginx default page
+- Optional: `docker ps` will show the running container
+- Destroy with `cdktf destroy`
+
+*/
+
+import { Construct } from 'constructs';
+import { App, TerraformStack } from 'cdktf';
+import { Container, Image } from './.gen/providers/docker';
+
+class MyStack extends TerraformStack {
+  public readonly dockerImage: Image
+
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    this.dockerImage = new Image(this, 'nginxImage', {
+      name         : "nginx:latest",
+      keepLocally : false
+    });
+
+    new Container(this, 'nginxContainer', {
+      image: this.dockerImage.latest,
+      name: "tutorial",
+      ports: [{
+        internal: 80,
+        external: 8000
+      }]
+    });    
+
+  }
+}
+
+const app = new App();
+new MyStack(app, 'demo-cdktf-ts-docker');
+app.synth();
