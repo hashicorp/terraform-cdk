797be09dd332fcaed0f4ae66bbc4696236c63365
Anubhav Mishra <anubhavmishra@users.noreply.github.com>
2020-07-15 17:10:42 -0700

    Terraform cloud integration test the remote state (#180)
    * wip tests
    
    * fix test
    
    * fix tests
    
    * terraform cloud token as env in github actions

---
+++ test/test-terraform-cloud/main.ts
@@ -0,0 +1,32 @@
+import { Construct } from 'constructs';
+import { App, TerraformStack, Testing  } from 'cdktf';
+import * as NullProvider from './.gen/providers/null';
+const token = process.env.TERRAFORM_CLOUD_TOKEN;
+
+export class HelloTerra extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const nullResouce = new NullProvider.Resource(this, 'test', {})
+
+    nullResouce.addOverride('provisioner', [{
+      'local-exec': {
+        command: `echo "hello deploy"`
+      }
+    }]);
+
+    this.addOverride('terraform.backend', {
+      remote: {
+        organization: 'cdktf-team',
+        workspaces: {
+          name: 'test'
+        },
+        token: token
+      }
+    });
+  }
+}
+
+const app = Testing.stubVersion(new App({}));
+new HelloTerra(app, 'hello-terra');
+app.synth();
