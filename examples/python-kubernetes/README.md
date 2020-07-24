f167026649695784ad5182055109f2f493fd94b7
Aareet Shermon <aareet@users.noreply.github.com>
2020-07-23 21:49:51 -0700

    Initial commit of working config that adds a namespace
    Add deployment and service to match Learn guide
    
    Add README
    
    Fix minor typo
    
    Remove unneeded Pod import
    
    Update README to include link to example
    
    Add cdktf deploy section

---
+++ examples/python-kubernetes/README.md
@@ -0,0 +1,48 @@
+# python-kubernetes
+
+A CDK for Terraform application in Python to demonstrate use of the Kubernetes Provider according to the [HashiCorp Learn Guide](https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes).
+
+## Usage
+
+Install Pipenv using Homebrew by running:
+  
+```bash
+$ brew install pipenv
+```
+You can install Pipenv by visiting the [website](https://pipenv.pypa.io/en/latest/).
+
+Install project dependencies
+
+```shell
+pipenv install
+```
+
+Generate CDK for Terraform constructs for Terraform providers and modules used in the project.
+
+```bash
+cdktf get
+```
+
+You can now edit the `main.py` file if you want to modify any code.
+
+Compile and generate Terraform configuration
+
+```bash
+cdktf synth
+```
+
+The above command will create a folder called `cdktf.out` that contains all Terraform JSON configuration that was generated.
+
+Next, deploy your Terraform configuration. 
+```bash
+cdktf deploy
+```
+
+Alternately, you can run Terraform commands to apply your configuration.
+
+```bash
+cd cdktf.out
+terraform init
+terraform plan
+terraform apply
+```
---
+++ examples/python-kubernetes/main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+from constructs import Construct
+from cdktf import App, TerraformStack
+from imports.kubernetes import Namespace, Service, Deployment, KubernetesProvider
+
+
+class MyStack(TerraformStack):
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
+
+        KubernetesProvider(self, 'kind', config_path="~/.kube/config")
+        example_namespace = Namespace(self, "tf-cdk-example",
+                                      metadata=[{
+                                          'name': 'tf-cdk-example'
+                                      }])
+        app_name = "nginx-example"
+        Deployment(self, 'nginx-deployment',
+                   metadata=[{
+                       'name': app_name,
+                       'namespace': example_namespace.metadata[0].name,
+                       'labels': {
+                           'app': app_name
+                       }
+                   }],
+                   spec=[{
+                       'replicas': 2,
+                       'selector': [{
+                           'matchLabels': {
+                               'app': app_name
+                           }
+                       }],
+                       'template': [{
+                           'metadata': [{
+                               'labels': {
+                                   'app': app_name
+                               }
+                           }],
+                           'spec': [{
+                               'container': [{
+                                   'image': 'nginx:1.7.9',
+                                   'name': 'example',
+                                   'ports': [{
+                                       'containerPort': 80
+                                   }]
+                               }]
+                           }]
+                       }]
+                   }])
+        Service(self, "tf-cdk-service",
+                metadata=[{
+                    'name': 'tf-cdk-service'
+                }],
+                spec=[{
+                    'selector': {
+                        'app': app_name
+                    },
+                    'port': [{
+                        'nodePort': 30201,
+                        'port': 80,
+                        'target_port': 80
+                    }],
+                    'type': 'NodePort'
+                }])
+
+
+app = App()
+MyStack(app, "python-kubernetes")
+
+app.synth()
