a3f9e19d4254adfa536b20861beb04902a4c3dbc
Anubhav Mishra <anubhavmishra@me.com>
2020-08-18 00:30:27 -0700

    checkpoint telemetry reporting for cdktf get command

--- packages/cdktf-cli/bin/cmds/helper/constructs-maker.ts
+++ packages/cdktf-cli/bin/cmds/helper/constructs-maker.ts
@@ -1,11 +1,15 @@
 import { GetProvider } from '../../../lib/get/providers';
 import { GetModule } from '../../../lib/get/modules';
 import { Language } from '../../../lib/get/base';
+import { ReportParams, ReportRequest } from '../../../lib/checkpoint'
+import { versionNumber } from '../version-check';
 
 export interface ConstructsOptions {
     codeMakerOutput: string;
     language: Language;
 }
+
+const product = "cdktf"
 
 export class ConstructsMaker {
 
@@ -13,6 +17,7 @@ export class ConstructsMaker {
         if (modules.length > 0) {
             await new GetModule().get(Object.assign({}, { codeMakerOutput: constructsOptions.codeMakerOutput,
                 targetLanguage: constructsOptions.language, isModule: true }, { targetNames: modules }));
+            await moduleTelemetry(constructsOptions.language, modules);
         }
     }
 
@@ -20,6 +25,33 @@ export class ConstructsMaker {
         if (providers.length > 0) {
             await new GetProvider().get(Object.assign({}, { codeMakerOutput: constructsOptions.codeMakerOutput,
                 targetLanguage: constructsOptions.language }, { targetNames: providers }));
+            await providerTelemetry(constructsOptions.language, providers);
         }
+    }
+}
+
+async function providerTelemetry(language: string, providers: string[]): Promise<void> {
+    for (const p of providers) {
+        const [fqname, version] = p.split('@');
+        const name = fqname.split('/').pop()
+        if (!name) { throw new Error(`Provider name should be properly set in ${p}`) }
+
+        const payload = { language: language, name: name, fullName: fqname, version: version, type: 'provider' };
+
+        const reportParams: ReportParams = { product: product, version: versionNumber(), payload: payload };
+
+        await ReportRequest(reportParams);
+    }
+}
+
+async function moduleTelemetry(language: string, modules: string[]): Promise<void> {
+    for (const module of modules) {
+        const [source, version] = module.split('@');
+
+        const payload = { language: language, source: source, version: version, type: 'module' };
+
+        const reportParams: ReportParams = { product: product, version: versionNumber(), payload: payload };
+
+        await ReportRequest(reportParams);
     }
 }
---
+++ packages/cdktf-cli/lib/checkpoint.ts
@@ -0,0 +1,81 @@
+import https = require('https');
+import { format } from 'url';
+import { v4 as uuidv4 } from 'uuid';
+import * as os from 'os';
+
+const BASE_URL = `https://checkpoint-api.hashicorp.com/v1/`;
+
+const VALID_ERROR_CODES = [200, 201];
+
+export interface ReportParams {
+    signature?: string;
+    signatureFile?: string;
+
+    startTime?: string;
+    endTime?: string;
+    arch?: string;
+    os?: string;
+    payload: {};
+    product: string;
+    runID?: string;
+    schemaVersion?: string;
+    version?: string;
+}
+
+async function post(url: string, data: string) {
+    return new Promise<any>((ok, ko) => {
+        const req = https.request(format(url), {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Length': data.length,
+                'User-Agent': 'HashiCorp/cdktf-cli'
+            },
+            method: 'POST'
+        }, res => {
+            if (res.statusCode) {
+                const statusCode = res.statusCode;
+                if (!VALID_ERROR_CODES.includes(statusCode)) {
+                    return ko(new Error(res.statusMessage));
+                }
+            }
+            const data = new Array<Buffer>();
+            res.on('data', chunk => data.push(chunk));
+
+            res.once('error', err => ko(err));
+            res.once('end', () => {
+                return ok();
+            });
+        });
+
+        req.write(data);
+
+        req.end();
+    })
+}
+
+export async function ReportRequest(reportParams: ReportParams): Promise<void> {
+    // we won't report when checkpoint is disabled.
+    if (process.env.CDKTF_CHECKPOINT_DISABLE == "1") {
+        return
+    }
+
+    if (!reportParams.runID) {
+        reportParams.runID = uuidv4();
+    }
+
+    if (!reportParams.arch) {
+        reportParams.arch = os.arch();
+    }
+
+    if (!reportParams.os) {
+        reportParams.os = os.platform();
+    }
+
+    const postData = JSON.stringify(reportParams);
+
+    await post(`${BASE_URL}telemetry/${reportParams.product}`, postData)
+
+} 
+
+
+
