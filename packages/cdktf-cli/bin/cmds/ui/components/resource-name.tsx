e751fa54496b4eff9c05bad1c19b47db99ee2f4f
Jon Steinich <jsteinich@gmail.com>
2020-08-21 23:05:13 +0000

    mostly fix cli resource name parsing

--- packages/cdktf-cli/bin/cmds/ui/components/resource-name.tsx
+++ packages/cdktf-cli/bin/cmds/ui/components/resource-name.tsx
@@ -8,26 +8,21 @@ export interface ResourceNameConfig {
 export const ResourceName = ({name}: ResourceNameConfig) => {
   const prettyName = name.replace(/(_[A-F\d]{8})$/, '')
 
-  if (prettyName === name) {
-    return <Text>{name}</Text>;
-  } else {
-    const [resource, resourceName] = prettyName.split('.')
-    const [, ...path] = resourceName.split('_')
+  const [resource, resourceName] = prettyName.split('.')
 
-    return(
-      <Box flexDirection="column" width={80}>
-        <Box>
-          <Box width={"25%"}>
-            <Text>{resource.toUpperCase()}</Text>
-          </Box>
-          <Box paddingLeft={1} width={"25%"}>
-            <Text>{path.join('_')}</Text>
-          </Box>
-          <Box paddingLeft={1} width={"50%"}>
-            <Color gray>{name}</Color>
-          </Box>
+  return(
+    <Box flexDirection="column" width={80}>
+      <Box>
+        <Box width={"25%"}>
+          <Text>{resource.toUpperCase()}</Text>
+        </Box>
+        <Box paddingLeft={1} width={"25%"}>
+          <Text>{resourceName}</Text>
+        </Box>
+        <Box paddingLeft={1} width={"50%"}>
+          <Color gray>{name}</Color>
         </Box>
       </Box>
-    )
-  }
+    </Box>
+  )
 }
