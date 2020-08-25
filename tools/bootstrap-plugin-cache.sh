7b292a86361d19580e367cfeeb294d7dadd3b69f
Sebastian Korfmann <sebastian@korfmann.net>
2020-08-25 10:32:48 +0200

    Bootstrap plugin cache

---
+++ tools/bootstrap-plugin-cache.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -euo pipefail
+
+# Disable spinner even when we have a TTY
+export CI='1'
+
+find ./examples -type f -name '*cdktf.json' |  tr '\n' " " | xargs jq -s "map(.terraformProviders) | add | unique | { \"language\": \"typescript\", \"terraformProviders\": . }" > bootstrap.json
+
+scriptdir=$(cd $(dirname $0) && pwd)
+
+cd $(mktemp -d)
+mkdir test && cd test
+
+cdktf init --template typescript-minimal --project-name="typescript-bootstrap" --project-description="typescript bootstrap plugin cache" --local
+
+# add null provider
+mv ${scriptdir}/../bootstrap.json ./cdktf.json
+cdktf get
+
+echo "DONE"
+
