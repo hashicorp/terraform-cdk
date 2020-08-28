b476b9f4421c8b263e061f29d3fc65f34a0663d3
Neil Guan <guan840912@gmail.com>
2020-08-28 19:48:51 +0800

    add azure app service example

---
+++ examples/typescript/azure-app-service/main.ts
@@ -0,0 +1,60 @@
+import { Construct } from 'constructs';
+import { App, TerraformStack, TerraformOutput } from 'cdktf';
+import {
+  AppService,
+  AppServicePlan,
+  AzurermProvider,
+  ResourceGroup,
+} from './.gen/providers/azurerm';
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    // define resources here
+    const imagename = 'nginx:latest';
+
+    new AzurermProvider(this, 'azureFeature', {
+      features: [{}],
+    });
+
+    const rg = new ResourceGroup(this, 'cdktf-rg', {
+      name: 'demo2020',
+      location: 'westeurope',
+    });
+
+    const asp = new AppServicePlan(this, 'cdktf-asp', {
+      kind: 'Linux',
+      reserved: true,
+      resourceGroupName: rg.name,
+      location: rg.location,
+      name: 'DockerCDKTF',
+      sku: [{ size: 'F1', tier: 'Free' }],
+      dependsOn: [rg],
+    });
+
+    const appsvc = new AppService(this, 'docker-cdktf', {
+      name: 'cdktfdemoneil',
+      appServicePlanId: `${asp.id}`,
+      location: rg.location,
+      resourceGroupName: rg.name,
+      clientAffinityEnabled: false,
+      httpsOnly: true,
+      dependsOn: [asp],
+    });
+    appsvc.addOverride('site_config', [
+      {
+        linux_fx_version: `DOCKER|${imagename}`,
+        use_32_bit_worker_process: true,
+      },
+    ]);
+    new TerraformOutput(this, 'appweburl', {
+      value: `https://${appsvc.name}.azurewebsites.net/`,
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, 'azure-app-service-docker');
+
+app.synth();
