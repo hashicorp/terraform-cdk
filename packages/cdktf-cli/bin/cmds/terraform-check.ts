780ef6a18e9d76379398d3a4d2446564769c70c1
Brecht Verhoeve <46007524+bverhoeve@users.noreply.github.com>
2020-09-19 13:02:16 +0200

    Check terraform version condition

--- packages/cdktf-cli/bin/cmds/terraform-check.ts
+++ packages/cdktf-cli/bin/cmds/terraform-check.ts
@@ -3,7 +3,16 @@ import { Terraform } from './ui/models/terraform'
 export const terraformCheck = async (): Promise<void> => {
   try {
     const terraform = new Terraform('./')
-    await terraform.version()
+    const terraformVersion = await terraform.version()
+
+    if (terraformVersion < SUPPORTED) {
+      const errorMessage = `Exiting due to unsupported Terraform version (${terraformVersion}. 
+        Only Terraform versions ${SUPPORTED} and up are supported,
+        please upgrade Terraform version)`;
+        console.error(errorMessage)
+        process.exit(1)
+    }
+
   } catch(e) {
     console.error(e.message)
     process.exit(1)
