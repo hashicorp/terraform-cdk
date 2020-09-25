41b25a358322ef10426f4b76ebc97de3bf055e67
Jon Steinich <jsteinich@gmail.com>
2020-09-25 03:23:42 +0000

    move provider definition up to fix #315

--- packages/cdktf/lib/terraform-hcl-module.ts
+++ packages/cdktf/lib/terraform-hcl-module.ts
@@ -1,25 +1,16 @@
 import { Construct } from "constructs";
 import { TerraformModuleOptions, TerraformModule } from "./terraform-module";
-import { TerraformProvider } from "./terraform-provider";
 
 export interface TerraformHclModuleOptions extends TerraformModuleOptions {
-    readonly providers?: (TerraformProvider | TerraformModuleProvider)[];
     readonly variables?: {[key: string]: any};
 }
 
-export interface TerraformModuleProvider {
-    readonly provider: TerraformProvider;
-    readonly moduleAlias: string;
-}
-
 export class TerraformHclModule extends TerraformModule {
-    private _providers?: (TerraformProvider | TerraformModuleProvider)[];
     private _variables?: {[key: string]: any};
 
     constructor(scope: Construct, id: string, options: TerraformHclModuleOptions) {
         super(scope, id, options);
 
-        this._providers = options.providers;
         this._variables = options.variables;
     }
 
@@ -32,17 +23,6 @@ export class TerraformHclModule extends TerraformModule {
             this._variables = {};
         }
         this._variables[variable] = value;
-    }
-
-    public get providers() {
-        return this._providers;
-    }
-
-    public addProvider(provider: TerraformProvider | TerraformModuleProvider) {
-        if (!this._providers) {
-            this._providers = [];
-        }
-        this._providers.push(provider);
     }
 
     public get(output: string): any {
@@ -66,16 +46,6 @@ export class TerraformHclModule extends TerraformModule {
     }
 
     protected synthesizeAttributes(): { [name: string]: any } {
-        return {
-            providers: this.providers?.map(p => {
-                if (p instanceof TerraformProvider) {
-                    return { [p.terraformResourceType]: p.fqn };
-                }
-                else {
-                    return { [`${p.provider.terraformResourceType}.${p.moduleAlias}`]: p.provider.fqn };
-                }
-            }),
-            ...this.variables
-        };
+        return { ...this.variables };
     }
 }
--- packages/cdktf/lib/terraform-module.ts
+++ packages/cdktf/lib/terraform-module.ts
@@ -1,22 +1,31 @@
 import { Construct } from "constructs";
 import { TerraformElement } from "./terraform-element";
+import { TerraformProvider } from "./terraform-provider";
 import { deepMerge } from "./util";
 
 export interface TerraformModuleOptions {
   readonly source: string;
   readonly version?: string;
+  readonly providers?: (TerraformProvider | TerraformModuleProvider)[];
+}
+
+export interface TerraformModuleProvider {
+  readonly provider: TerraformProvider;
+  readonly moduleAlias: string;
 }
 
 export abstract class TerraformModule extends TerraformElement {
 
   public readonly source: string;
   public readonly version?: string;
+  private _providers?: (TerraformProvider | TerraformModuleProvider)[];
 
   constructor(scope: Construct, id: string, options: TerraformModuleOptions) {
     super(scope, id);
 
     this.source = options.source;
     this.version = options.version;
+    this._providers = options.providers;
   }
 
   // jsii can't handle abstract classes?
@@ -28,11 +37,30 @@ export abstract class TerraformModule extends TerraformElement {
     return `\${module.${this.friendlyUniqueId}.${moduleOutput}}` as any;
   }
 
+  public get providers() {
+    return this._providers;
+}
+
+public addProvider(provider: TerraformProvider | TerraformModuleProvider) {
+    if (!this._providers) {
+        this._providers = [];
+    }
+    this._providers.push(provider);
+}
+
   public toTerraform(): any {
     const attributes = deepMerge({
       ...this.synthesizeAttributes(),
       source: this.source,
-      version: this.version
+      version: this.version,
+      providers: this.providers?.map(p => {
+        if (p instanceof TerraformProvider) {
+            return { [p.terraformResourceType]: p.fqn };
+        }
+        else {
+            return { [`${p.provider.terraformResourceType}.${p.moduleAlias}`]: p.provider.fqn };
+        }
+    }),
     },
       this.rawOverrides
     )
