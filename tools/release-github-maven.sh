0169b90fb2cd99cee7b66a5004660fcaf1d06a11
Jon Steinich <jsteinich@gmail.com>
2020-10-06 22:03:14 +0000

    copy over some fixes from test branch

--- tools/release-github-maven.sh
+++ tools/release-github-maven.sh
@@ -7,7 +7,7 @@ pom="$(find . -type f -name *.pom | head -1)"
 cd "$(dirname ${pom})" 
 
 release_output="${workdir}/release-output.txt"
-mvn deploy -f ${pom} -DaltDeploymentRepository=github::default::https://maven.pkg.github.com/hashicorp/terraform-cdk | tee ${release_output}
+mvn deploy -f "$(basename ${pom})" -DaltDeploymentRepository=github::default::https://maven.pkg.github.com/hashicorp/terraform-cdk | tee ${release_output}
 
 # If release failed, check if this was caused because we are trying to publish
 # the same version again, which is not an error. The magic string "409 Conflict"
