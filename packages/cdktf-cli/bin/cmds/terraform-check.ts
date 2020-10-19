8fe521debf12ce108891482c4970742a0ff7cf85
Brecht Verhoeve <46007524+bverhoeve@users.noreply.github.com>
2020-10-19 20:51:39 +0200

    Parse Terraform version explicitly

--- packages/cdktf-cli/bin/cmds/terraform-check.ts
+++ packages/cdktf-cli/bin/cmds/terraform-check.ts
@@ -2,19 +2,25 @@ import { Terraform } from './ui/models/terraform'
 import * as semver from 'semver';
 
 const MIN_SUPPORTED_VERSION = '0.12.0'
+const VERSION_REGEXP = /Terraform v\d+.\d+.\d+/
 
 export const terraformCheck = async (): Promise<void> => {
   try {
     const terraform: Terraform = new Terraform('./')
 
-    // string in format of 'Terraform v0.13.3'
     const terraformVersion = await terraform.version()
-    const cleanTerraformVersion = semver.clean(terraformVersion.substring(terraformVersion.indexOf('v')))
+    const terraformVersionMatches = terraformVersion.match(VERSION_REGEXP)
 
-    if (cleanTerraformVersion && semver.lt(cleanTerraformVersion, MIN_SUPPORTED_VERSION)) {
-      const errorMessage = `Error: unsupported Terraform version [${cleanTerraformVersion}] - please upgrade to >=${MIN_SUPPORTED_VERSION}`
-        console.error(errorMessage)
-        process.exit(1)
+    if ( terraformVersionMatches !== null) {
+
+      // Should always be the first match found in the string
+      let cleanTerraformVersion = semver.clean(terraformVersionMatches[0].substring(terraformVersionMatches[0].indexOf('v')))
+
+      if (cleanTerraformVersion && semver.lt(cleanTerraformVersion, MIN_SUPPORTED_VERSION)) {
+        const errorMessage = `Error: unsupported Terraform version [${cleanTerraformVersion}] - please upgrade to >=${MIN_SUPPORTED_VERSION}`
+          console.error(errorMessage)
+          process.exit(1)
+      }
     }
   } catch(e) {
     console.error(e.message)
