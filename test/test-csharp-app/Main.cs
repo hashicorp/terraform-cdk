d5d16d5aa3bf77311ae0b26c6962518551acb874
Jon Steinich <jsteinich@gmail.com>
2020-12-16 03:35:26 +0000

    integration test

---
+++ test/test-csharp-app/Main.cs
@@ -0,0 +1,29 @@
+using System;
+using aws;
+using Constructs;
+using Hashicorp.Cdktf;
+
+namespace MyCompany.MyApp
+{
+    class MyApp : TerraformStack
+    {
+        public MyApp(Construct scope, string id) : base(scope, id)
+        {
+            new AwsProvider(this, "Aws", new AwsProviderConfig {
+                Region = "eu-central-1"
+            });
+
+            SnsTopic topic = new SnsTopic(this, "Topic", new SnsTopicConfig {
+                DisplayName = "overwritten"
+            });
+            topic.AddOverride("display_name", "my-first-sns-topic");
+        }
+
+        public static void Main(string[] args)
+        {
+            App app = Testing.StubVersion(new App(new AppOptions { StackTraces = false }));
+            new MyApp(app, "csharp-simple");
+            app.Synth();
+        }
+    }
+}
