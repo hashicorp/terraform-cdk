hashicorp/terraform-cdk

commit 5b7e09cb62ccbceef8ea7a42dd3f1ea52d640ee7

Enable synth only without trying to contact TF cloud

packages/cdktf-cli/bin/cmds/ui/terraform-context.tsx
@@ -249,11 +249,13 @@ export const useTerraform = ({ targetDir, synthCommand, isSpeculative = false, a
     }
   }
 
-  const execTerraformSynth = async () => {
+  const execTerraformSynth = async (synthOnly = false) => {
     try {
       dispatch({ type: 'SYNTH' })
       const stacks = await SynthStack.synth(synthCommand, targetDir);
-      await excecutorForStack(stacks[0].content)
+      if (synthOnly) {
+        await excecutorForStack(stacks[0].content)
+      }
       dispatch({ type: 'NEW_STACK', stackName: stacks[0].name, stackJSON: stacks[0].content })
     } catch (e) {
       dispatch({ type: 'ERROR', error: e })
@@ -336,7 +338,7 @@ export const useTerraform = ({ targetDir, synthCommand, isSpeculative = false, a
   const synth = () => {
     React.useEffect(() => {
       const invoke = async () => {
-        await execTerraformSynth()
+        await execTerraformSynth(true)
       }
 
       invoke()

commit 5cdbcf8d61b1253f7dc0e9c9adfab35511d27005

Fix logic error introduced by 5b7e09cb62ccbceef8ea7a42dd3f1ea52d640ee7

packages/cdktf-cli/bin/cmds/ui/terraform-context.tsx
@@ -249,11 +249,11 @@ export const useTerraform = ({ targetDir, synthCommand, isSpeculative = false, a
     }
   }
 
-  const execTerraformSynth = async (synthOnly = false) => {
+  const execTerraformSynth = async (loadExecutor = true) => {
     try {
       dispatch({ type: 'SYNTH' })
       const stacks = await SynthStack.synth(synthCommand, targetDir);
-      if (synthOnly) {
+      if (loadExecutor) {
         await excecutorForStack(stacks[0].content)
       }
       dispatch({ type: 'NEW_STACK', stackName: stacks[0].name, stackJSON: stacks[0].content })
@@ -338,7 +338,7 @@ export const useTerraform = ({ targetDir, synthCommand, isSpeculative = false, a
   const synth = () => {
     React.useEffect(() => {
       const invoke = async () => {
-        await execTerraformSynth(true)
+        await execTerraformSynth(false)
       }
 
       invoke()