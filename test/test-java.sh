9248125d0823fd843440993d2a953103de858aaf
Sebastian Korfmann <sebastian@korfmann.net>
2021-02-08 23:49:11 +0100

    Full integration build matrix for each language

---
+++ test/test-java.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+scriptdir=$(cd $(dirname $0) && pwd)
+
+cd ${scriptdir}
+
+for dir in test-java*; do
+  [ ! -d $dir ] && continue
+
+  echo "--------------------------------------------------------------------"
+  echo $dir
+  echo "--------------------------------------------------------------------"
+  $dir/test.sh
+done
