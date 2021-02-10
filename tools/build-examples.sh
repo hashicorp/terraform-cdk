d30442430810dec9b0e17ac2ad74d1360e5badf0
Sebastian Korfmann <sebastian@korfmann.net>
2021-02-10 14:56:16 +0100

    Run Java examples parallel as well

--- tools/build-examples.sh
+++ tools/build-examples.sh
@@ -9,13 +9,7 @@ export CI='1'
 
 BUILD_TARGET=$1
 
-if  [ $BUILD_TARGET == "java" ] ; then
-  npx lerna run --stream --scope @examples/${BUILD_TARGET}* --concurrency 1 reinstall
-  npx lerna run --stream --scope @examples/${BUILD_TARGET}* --concurrency 1 build
-  npx lerna run --stream --scope @examples/${BUILD_TARGET}* --concurrency 1 synth
-else
-  npx lerna run --parallel --scope @examples/${BUILD_TARGET}* reinstall
-  npx lerna run --parallel --scope @examples/${BUILD_TARGET}* build
-  npx lerna run --parallel --scope @examples/${BUILD_TARGET}* synth
-fi
+npx lerna run --parallel --scope @examples/${BUILD_TARGET}* reinstall
+npx lerna run --parallel --scope @examples/${BUILD_TARGET}* build
+npx lerna run --parallel --scope @examples/${BUILD_TARGET}* synth
 
