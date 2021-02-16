f66498f02a10be5f4dbb2fddc6c7da9efba4ae31
Sebastian Korfmann <sebastian@korfmann.net>
2021-02-17 00:26:37 +0100

    Move python / java / csharp end2end tests to jest

---
+++ test/java/synth-app/test.ts
@@ -0,0 +1,20 @@
+/**
+ *
+ * @group java
+ */
+
+import { TestDriver } from "../../test-helper";
+
+describe("java full integration", () => {
+  let driver: TestDriver;
+
+  beforeAll(() => {
+    driver = new TestDriver(__dirname)
+    driver.setupJavaProject()
+  });
+
+  test("synth generates JSON", async () => {
+    driver.synth()
+    expect(driver.synthesizedStack()).toMatchSnapshot()
+  })
+})
