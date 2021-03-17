f8d67ad99cbfacaad2cd0a5b1501d20905b897bd
Jon Steinich <jsteinich@gmail.com>
2021-03-17 02:46:21 +0000

    do test in temp dir to fix path

--- packages/cdktf-cli/test/get/generator/module-generator.test.ts
+++ packages/cdktf-cli/test/get/generator/module-generator.test.ts
@@ -3,6 +3,7 @@ import * as os from 'os';
 import * as path from 'path';
 import { ConstructsMaker, Language } from '../../../lib/get/constructs-maker'
 import { TerraformModuleConstraint  } from '../../../lib/config'
+import { withTempDir } from '../../../lib/util';
 
 test('generate some modules', async () => {
   jest.setTimeout(20000)
@@ -18,19 +19,24 @@ test('generate some modules', async () => {
 });
 
 test('no module outputs', async () => {
-  const workdir = fs.mkdtempSync(path.join(os.tmpdir(), 'no-output-module.test'));
-  fs.mkdirSync(path.join(workdir, 'module'));
-  fs.copyFileSync(path.join(__dirname, 'fixtures', 'module-no-outputs.test.fixture.tf'), path.join(workdir, 'module', 'main.tf'));
+  await withTempDir('no-output-module.test', async () => {
+    const curdir = process.cwd();
+    fs.mkdirSync('module');
+    fs.copyFileSync(path.join(__dirname, 'fixtures', 'module-no-outputs.test.fixture.tf'), path.join(curdir, 'module', 'main.tf'));
 
-  const constraint = new TerraformModuleConstraint({
-    source: `./${path.relative(__dirname, path.join(workdir, 'module'))}`,
-    name: 'module',
-    fqn: 'module'
+    const constraint = new TerraformModuleConstraint({
+      source: './module',
+      name: 'module',
+      fqn: 'module'
+    });
+
+    fs.mkdirSync('work');
+    const workdir = path.join(curdir, 'work');
+  
+    const maker = new ConstructsMaker({codeMakerOutput: workdir, targetLanguage: Language.TYPESCRIPT}, [constraint])
+    await maker.generate();
+  
+    const output = fs.readFileSync(path.join(workdir, 'modules/module.ts'), 'utf-8');
+    expect(output).toMatchSnapshot();
   });
-
-  const maker = new ConstructsMaker({codeMakerOutput: workdir, targetLanguage: Language.TYPESCRIPT}, [constraint])
-  await maker.generate();
-
-  const output = fs.readFileSync(path.join(workdir, 'modules/module.ts'), 'utf-8');
-  expect(output).toMatchSnapshot();
 });
