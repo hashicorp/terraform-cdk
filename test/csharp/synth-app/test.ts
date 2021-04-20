24018d849c493df7725ed37b22d6344f30c20e82
Ansgar Mertens <ansgar@hashicorp.com>
2021-04-20 22:34:35 +0200

    increase more timeouts even more trying to fix tests

--- test/csharp/synth-app/test.ts
+++ test/csharp/synth-app/test.ts
@@ -11,10 +11,10 @@ describe("csharp full integration test synth", () => {
   beforeAll(async () => {
     driver = new TestDriver(__dirname)
     await driver.setupCsharpProject()
-  }, 120_000);
+  }, 180_000);
 
   test("synth generates JSON", async () => {
     await driver.synth()
     expect(driver.synthesizedStack()).toMatchSnapshot()
-  }, 120_000);
+  }, 180_000);
 })
