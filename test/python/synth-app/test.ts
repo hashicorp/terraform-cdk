8f6a0f081d238b3d0d661645a23240c15880db00
Ansgar Mertens <ansgar@hashicorp.com>
2021-04-20 23:16:29 +0200

    heavily increase timeout for python integration test as it still fails on windows

--- test/python/synth-app/test.ts
+++ test/python/synth-app/test.ts
@@ -11,10 +11,10 @@ describe("python full integration test synth", () => {
   beforeAll(async () => {
     driver = new TestDriver(__dirname)
     await driver.setupPythonProject()
-  }, 180_000);
+  }, 240_000);
 
   test("synth generates JSON", async () => {
     await driver.synth()
     expect(driver.synthesizedStack()).toMatchSnapshot()
-  }, 180_000);
+  }, 240_000);
 })
