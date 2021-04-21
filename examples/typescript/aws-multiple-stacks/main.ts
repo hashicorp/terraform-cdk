3159db83adaeea9aa31de42d16208c8b644bd52d
Sebastian Korfmann <sebastian@korfmann.net>
2021-04-21 11:06:39 +0200

    Explicit multiple stacks example

---
+++ examples/typescript/aws-multiple-stacks/main.ts
@@ -0,0 +1,35 @@
+import { Construct } from "constructs";
+import { App, TerraformStack } from "cdktf";
+import { AwsProvider, Instance } from "./.gen/providers/aws";
+
+interface MyStackConfig {
+  environment: string,
+  region?: string;
+}
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, id: string, config: MyStackConfig) {
+    super(scope, id);
+
+    const { region = 'us-east-1' } = config
+
+    new AwsProvider(this, "aws", {
+      region,
+    });
+
+    new Instance(this, "Hello", {
+      ami: "ami-2757f631",
+      instanceType: "t2.micro",
+      tags: {
+        environment: config.environment
+      }
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, "multiple-stacks-dev", { environment: 'dev' });
+new MyStack(app, "multiple-stacks-staging", { environment: 'staging' });
+new MyStack(app, "multiple-stacks-production-us", { environment: 'production', region: 'us-east-1' });
+new MyStack(app, "multiple-stacks-production-eu", { environment: 'production', region: 'eu-central-1' });
+app.synth();
