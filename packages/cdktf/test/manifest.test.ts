4023a455a501de8883d7c240131399ba2083e0a6
Sebastian Korfmann <sebastian@korfmann.net>
2021-04-21 11:01:21 +0200

    Test manifest

---
+++ packages/cdktf/test/manifest.test.ts
@@ -0,0 +1,66 @@
+import { TerraformStack, Manifest, App } from "../lib";
+import * as fs from "fs";
+import * as path from "path";
+import * as os from "os";
+
+test("filename", () => {
+  expect(Manifest.fileName).toEqual("manifest.json");
+});
+
+test("stacksFolder", () => {
+  expect(Manifest.stacksFolder).toEqual("stacks");
+});
+
+test("stacksFilename", () => {
+  expect(Manifest.stackFileName).toEqual("cdk.tf.json");
+});
+
+test("create stacks folder", () => {
+  const outdir = fs.mkdtempSync(path.join(os.tmpdir(), "cdktf.outdir."));
+  new Manifest("0.0.0", outdir);
+  expect(fs.existsSync(path.join(outdir, Manifest.stacksFolder))).toBeTruthy();
+});
+
+test("get stack manifest", () => {
+  const outdir = fs.mkdtempSync(path.join(os.tmpdir(), "cdktf.outdir."));
+  const manifest = new Manifest("0.0.0", outdir);
+
+  const app = new App();
+  const stackManifest = manifest.forStack(
+    new TerraformStack(app, "this-is-a-stack")
+  );
+
+  expect(stackManifest).toMatchInlineSnapshot(`
+    Object {
+      "constructPath": "this-is-a-stack",
+      "name": "this-is-a-stack",
+      "synthesizedStackPath": "stacks/this-is-a-stack/cdk.tf.json",
+      "workingDirectory": "stacks/this-is-a-stack",
+    }
+  `);
+});
+
+test("write manifest", () => {
+  const outdir = fs.mkdtempSync(path.join(os.tmpdir(), "cdktf.outdir."));
+  const manifest = new Manifest("0.0.0", outdir);
+
+  const app = new App();
+  manifest.forStack(new TerraformStack(app, "this-is-a-stack"));
+
+  manifest.writeToFile();
+
+  expect(fs.readFileSync(path.join(outdir, Manifest.fileName)).toString())
+    .toMatchInlineSnapshot(`
+    "{
+      \\"version\\": \\"0.0.0\\",
+      \\"stacks\\": {
+        \\"this-is-a-stack\\": {
+          \\"name\\": \\"this-is-a-stack\\",
+          \\"constructPath\\": \\"this-is-a-stack\\",
+          \\"workingDirectory\\": \\"stacks/this-is-a-stack\\",
+          \\"synthesizedStackPath\\": \\"stacks/this-is-a-stack/cdk.tf.json\\"
+        }
+      }
+    }"
+  `);
+});
