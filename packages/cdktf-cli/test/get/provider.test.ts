ff835a50bfe94029a3d483b3e4b111e97ca7f2e3
Jon Steinich <jsteinich@gmail.com>
2021-05-04 03:25:11 +0000

    add a test case

--- packages/cdktf-cli/test/get/provider.test.ts
+++ packages/cdktf-cli/test/get/provider.test.ts
@@ -5,3 +5,5 @@ const getProvider = (constraint: TerraformDependencyConstraint) =>
   expectImportMatchSnapshot(constraint);
 
 getProvider(new TerraformProviderConstraint('aws@= 2.60.0'));
+
+getProvider(new TerraformProviderConstraint('phillbaker/elasticsearch@= 1.5.1'));
