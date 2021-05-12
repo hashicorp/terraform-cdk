1580117c41a1833db2e70b92b39e31c4b37ec78d
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-05-12 17:34:59 +0200

    test(asset): fix assertion

--- test/typescript/asset/test.ts
+++ test/typescript/asset/test.ts
@@ -29,7 +29,7 @@ describe("full integration test", () => {
       fs.readFileSync(
         path.resolve(
           driver.stackDirectory("stack"),
-          "localasset/local-asset.txt"
+          "assets/localasset/local-asset.txt"
         ),
         "utf-8"
       )
@@ -40,19 +40,19 @@ describe("full integration test", () => {
     await driver.synth();
     expect(
       fs.readFileSync(
-        path.resolve(driver.stackDirectory("stack"), "fixtures/a.txt"),
+        path.resolve(driver.stackDirectory("stack"), "assets/fixtures/a.txt"),
         "utf-8"
       )
     ).toMatchSnapshot();
     expect(
       fs.readFileSync(
-        path.resolve(driver.stackDirectory("stack"), "fixtures/b.txt"),
+        path.resolve(driver.stackDirectory("stack"), "assets/fixtures/b.txt"),
         "utf-8"
       )
     ).toMatchSnapshot();
     expect(
       fs.readFileSync(
-        path.resolve(driver.stackDirectory("stack"), "fixtures/foo/bar/c.txt"),
+        path.resolve(driver.stackDirectory("stack"), "assets/fixtures/foo/bar/c.txt"),
         "utf-8"
       )
     ).toMatchSnapshot();
@@ -62,7 +62,7 @@ describe("full integration test", () => {
     await driver.synth();
 
     const stat = fs.statSync(
-      path.resolve(driver.stackDirectory("stack"), "zippedfixtures/archive.zip")
+      path.resolve(driver.stackDirectory("stack"), "assets/zippedfixtures/archive.zip")
     );
     expect(stat.isFile()).toBe(true);
     expect(stat.size).toBe(1799);
