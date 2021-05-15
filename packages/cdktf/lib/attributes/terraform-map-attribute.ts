e2f88e9ca40fd73e8e19bafabb53b66611c527e3
Jon Steinich <jsteinich@gmail.com>
2021-05-15 19:41:33 +0000

    add core attribute types

---
+++ packages/cdktf/lib/attributes/terraform-map-attribute.ts
@@ -0,0 +1,8 @@
+import { TerraformAttribute, TerraformAttributeOptions } from "./terraform-attribute";
+import { ITerraformAddressable } from "../terraform-addressable";
+
+export abstract class TerraformMapAttribute extends TerraformAttribute {
+    public constructor(parent: ITerraformAddressable, terraformAttribute: string, value?: { [key: string]: any }, options?: TerraformAttributeOptions) {
+        super(parent, terraformAttribute, value, options);
+    }
+}
