1a5c66e25721aa2975b6c8324eb906572e5ade0b
Jon Steinich <jsteinich@gmail.com>
2021-05-15 19:41:33 +0000

    add core attribute types

---
+++ packages/cdktf/lib/attributes/terraform-list-attribute.ts
@@ -0,0 +1,8 @@
+import { TerraformAttribute, TerraformAttributeOptions } from "./terraform-attribute";
+import { ITerraformAddressable } from "../terraform-addressable";
+
+export abstract class TerraformListAttribute extends TerraformAttribute {
+    public constructor(parent: ITerraformAddressable, terraformAttribute: string, value?: any[], options?: TerraformAttributeOptions) {
+        super(parent, terraformAttribute, value, options);
+    }
+}
