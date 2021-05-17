4d480ccb4e55d826936cb2941b84493cf1a661eb
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-05-17 16:26:10 +0200

    test(asset): improve test execution time

--- test/python/asset/main.py
+++ test/python/asset/main.py
@@ -2,16 +2,14 @@
 import os
 from constructs import Construct
 from cdktf import App, TerraformStack, Testing, TerraformAsset, AssetType
-from imports.aws import SnsTopic, AwsProvider
+from imports.null import NullProvider, Resource
 
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
 
-        AwsProvider(self, 'Aws', region='eu-central-1')
-        topic = SnsTopic(self, 'Topic', display_name='overwritten')
-        topic.add_override('display_name', 'my-first-sns-topic')
-
+        NullProvider(self, "null")
+        Resource(self, "null-resource")
         self.add_override('terraform.backend', {
             'remote': {
                 'organization': 'test',
