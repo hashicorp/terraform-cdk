31e0bd6ed3489e1796eecd9965508d9c239feb90
Ansgar Mertens <ansgar@hashicorp.com>
2021-05-21 14:27:35 +0200

    tests: add integration test for go

---
+++ test/go/synth-app/test.ts
@@ -0,0 +1,20 @@
+/**
+ *
+ * @group go
+ */
+
+import { TestDriver } from "../../test-helper";
+
+describe("Go full integration test synth", () => {
+  let driver: TestDriver;
+
+  beforeAll(async () => {
+    driver = new TestDriver(__dirname)
+    await driver.setupGoProject()
+  });
+
+  test("synth generates JSON", async () => {
+    await driver.synth()
+    expect(driver.synthesizedStack('go-simple')).toMatchSnapshot()
+  });
+})
