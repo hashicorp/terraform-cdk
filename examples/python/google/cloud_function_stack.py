23ba51835402d11df98f49f03530f6aa56c814d5
Prashanth Basani <prashanth95r@gmail.com>
2021-06-11 17:08:07 -0500

    add python google gcp example

---
+++ examples/python/google/cloud_function_stack.py
@@ -0,0 +1,51 @@
+import os
+
+from constructs import Construct
+from cdktf import App, TerraformStack, TerraformOutput
+
+from imports.google import (
+    CloudfunctionsFunctionIamBinding,
+    GoogleProvider,
+    CloudfunctionsFunction,
+    StorageBucket,
+    StorageBucketObject,
+)
+
+class CloudFunctionStack(TerraformStack):
+
+    def __init__(self, scope: Construct, ns, project_id) -> None:
+        super().__init__(scope, ns)
+
+        GoogleProvider(self, id=project_id, region="us-central1", project=project_id)
+
+        code_bucket = StorageBucket(self, "bucket", name="terraform-cf-zip-files", project=project_id)
+
+        code_object = StorageBucketObject(self, "archive", \
+            name="source.zip",
+            bucket = code_bucket.name,
+            source = os.path.join(os.path.abspath("./"), "function", "source.zip"),
+            )
+
+        cloud_function = CloudfunctionsFunction(self, 'addition function', \
+            name="addition",
+            project=project_id,
+            region="us-central1",
+            runtime="python38",
+            available_memory_mb=128,
+            source_archive_bucket=code_bucket.name,
+            source_archive_object=code_object.name,
+            trigger_http=True,
+            entry_point="main"
+        )
+
+        invoker = CloudfunctionsFunctionIamBinding(self, "invoker",\
+            project=cloud_function.project,
+            region=cloud_function.region,
+            cloud_function=cloud_function.name,
+            members=["allUsers"],
+            role="roles/cloudfunctions.invoker"
+            )
+
+        url = TerraformOutput(self, "cloud function url",\
+            value=cloud_function.https_trigger_url
+            )
