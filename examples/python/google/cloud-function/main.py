e6973acbed3cb241190d695e2b19f537687b9c89
Prashanth Basani <prashanth95r@gmail.com>
2021-06-13 17:46:41 -0500

    python example for gcp cloud function

---
+++ examples/python/google/cloud-function/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from abc import get_cache_token
+from constructs import Construct
+from cdktf import App
+
+from cloud_function_stack import CloudFunctionStack
+
+GCP_PROJECT_NAME = "gcp-project-name"
+
+app = App()
+CloudFunctionStack(app, "cdktf-python-gcp-cf", GCP_PROJECT_NAME)
+
+app.synth()
