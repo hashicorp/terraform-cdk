8ffc367b4ffede3d49ee2faa1099ce0729dc5825
Sebastian Korfmann <sebastian@korfmann.net>
2021-06-22 12:52:41 +0200

    Token in Python
    Run it with JSII_DEBUG=1 pipenv run python main.py

--- examples/python/aws/main.py
+++ examples/python/aws/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from constructs import Construct
-from cdktf import App, TerraformStack
+from cdktf import App, TerraformStack, TerraformOutput, Token
 from imports.aws import SnsTopic, AwsProvider
 from imports.terraform_aws_modules.vpc.aws import TerraformAwsModulesVpcAws
 
@@ -17,7 +17,16 @@ class MyStack(TerraformStack):
             azs=["us-east-1a", "us-east-1b"],
             public_subnets=["10.0.1.0/24", "10.0.2.0/24"]
             )
-        SnsTopic(self, 'Topic', display_name='my-first-sns-topic')
+        topic = SnsTopic(self, 'Topic', display_name='my-first-sns-topic')
+        tokenAsString = Token.as_string(1234)
+        tokenAsNumber = Token.as_number("1234")
+        tokenAsList = Token.as_list("1234")
+        tokenAsAny = Token.as_any(True)
+        TerraformOutput(self, 'TopicStringOutput', value=tokenAsString)
+        TerraformOutput(self, 'TopicAnyOutput', value=tokenAsAny)
+        TerraformOutput(self, 'TopicNumberOutput', value=tokenAsNumber)
+        TerraformOutput(self, 'TopicListOutput', value=tokenAsList)
+
 
 
 app = App()
