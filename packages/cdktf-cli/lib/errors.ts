f59543fd17b91937aaa31cff0d32ba11e571e9d8
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-07-12 16:50:59 +0200

    chore(cli): add errors that do telemetry

---
+++ packages/cdktf-cli/lib/errors.ts
@@ -0,0 +1,37 @@
+import { ReportParams, ReportRequest } from "./checkpoint";
+import { versionNumber } from "../bin/cmds/version-check";
+
+// Errors that will emit telemetry events
+async function report(command: string, payload: Record<string, any>) {
+  const reportParams: ReportParams = {
+    command,
+    product: "cdktf",
+    version: versionNumber(),
+    dateTime: new Date(),
+    payload,
+  };
+
+  await ReportRequest(reportParams);
+}
+
+function reportPrefixedError(type: string) {
+  return async (
+    command: string,
+    message: string,
+    context: Record<string, any>
+  ) => {
+    await report(command, { ...context, message, type });
+    const err: any = new Error(`${type} Error: ${message}`);
+    Object.entries(context).forEach(([key, value]) => {
+      err[key] = value;
+    });
+    return err;
+  };
+}
+
+export const Errors = {
+  // Error within our control
+  Internal: reportPrefixedError("Internal"),
+  // Error in the usage
+  Usage: reportPrefixedError("Usage"),
+};
