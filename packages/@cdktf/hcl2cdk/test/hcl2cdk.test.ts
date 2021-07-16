a918930ec252cc3ce14b41dd8277c413be4717fc
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-07-16 09:05:24 +0200

    fix(hcl2cdk): fix references in overrides

--- packages/@cdktf/hcl2cdk/test/hcl2cdk.test.ts
+++ packages/@cdktf/hcl2cdk/test/hcl2cdk.test.ts
@@ -616,6 +616,49 @@ describe("convert", () => {
       }        
       `,
     ],
+    [
+      "complex for each loops",
+      `
+      resource "aws_acm_certificate" "example" {
+        domain_name       = "example.com"
+        validation_method = "DNS"
+      }
+      
+      data "aws_route53_zone" "example" {
+        name         = "example.com"
+        private_zone = false
+      }
+      
+      resource "aws_route53_record" "example" {
+        for_each = {
+          for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+            name   = dvo.resource_record_name
+            record = dvo.resource_record_value
+            type   = dvo.resource_record_type
+          }
+        }
+      
+        allow_overwrite = true
+        name            = each.value.name
+        records         = [each.value.record]
+        ttl             = 60
+        type            = each.value.type
+        zone_id         = data.aws_route53_zone.example.zone_id
+      }
+      
+      resource "aws_acm_certificate_validation" "example" {
+        certificate_arn         = aws_acm_certificate.example.arn
+        validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
+      }
+      
+      resource "aws_lb_listener" "example" {
+        # ... other configuration ...
+      
+        certificate_arn = aws_acm_certificate_validation.example.certificate_arn
+      }
+              
+      `,
+    ],
   ])("%s configuration", async (_name, hcl) => {
     const { all } = await convert(`file.hcl`, hcl, {
       language: "typescript",
