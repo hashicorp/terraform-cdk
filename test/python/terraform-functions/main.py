3297adb11670f487c3ff28d0fb935f530b20947e
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-08-17 13:22:26 +0200

    test(lib): integration test in python

--- test/python/terraform-functions/main.py
+++ test/python/terraform-functions/main.py
@@ -11,7 +11,7 @@ class MyStack(TerraformStack):
 
         AwsProvider(self, 'Aws', region='eu-central-1')
         modules = TerraformAwsModulesVpcAws(self, 'CustomVpc', name='custom-vpc')
-        TerraformOutput(self, "computed", value=Fn.cidrsubnet(Fn.element(Fn.merge(modules.public_subnets_cidr_blocks, modules.public_subnets_ipv6_cidr_blocks), 3), 4, 2))
+        TerraformOutput(self, "computed", value=Fn.cidrsubnet(Fn.element(Fn.merge(modules.public_subnets_cidr_blocks_output, modules.public_subnets_ipv6_cidr_blocks_output), 3), 4, 2))
 
 app = Testing.stub_version(App(stack_traces=False))
 MyStack(app, "python-simple")
