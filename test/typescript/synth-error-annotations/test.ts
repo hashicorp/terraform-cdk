d7c67620d30dcf07de5aeb0d46595ad7fb3dc854
Ansgar Mertens <ansgar@hashicorp.com>
2021-08-24 15:13:48 +0200

    chore(tests): add integration test for annotations

---
+++ test/typescript/synth-error-annotations/test.ts
@@ -0,0 +1,34 @@
+/**
+ * Testing synthing typescript to json
+ *
+ * @group typescript
+ */
+
+import { TestDriver } from "../../test-helper";
+
+describe("full integration test synth", () => {
+  let driver: TestDriver;
+
+  beforeAll(async () => {
+    driver = new TestDriver(__dirname);
+    await driver.setupTypescriptProject();
+  });
+
+  test("synth prints error message on failure", async () => {
+    try {
+      await driver.synth();
+      fail("Expected synth to fail but no error was thrown");
+    } catch (e) {
+      expect(e.code).toBe(1);
+      const stdout = e.stdout.toString();
+      console.log(stdout);
+      expect(stdout).toContain("INFO [hello-terra/dummy]: info annotation");
+      expect(stdout).toContain("WARN [hello-terra/dummy]: warning annotation");
+      expect(stdout).toContain("ERROR [hello-terra/dummy]: error annotation");
+      const errorString = e.stderr.toString();
+      expect(errorString).toContain(
+        `While synthesizing one or more error annotations have been encountered. Please check the log output above.`
+      );
+    }
+  });
+});
