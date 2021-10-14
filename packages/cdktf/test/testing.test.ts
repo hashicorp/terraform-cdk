76698e5fa4c158ecc0b8704481daba9c6f939b7a
Ansgar Mertens <ansgar@hashicorp.com>
2021-10-14 15:55:46 +0200

    feat(lib): invoke Aspects when using `Testing.synth` and `Testing.synthScope`
    resolves #1076

---
+++ packages/cdktf/test/testing.test.ts
@@ -0,0 +1,48 @@
+import { TerraformStack, TerraformResource, Testing } from "../lib";
+
+import { Aspects } from "../lib/aspect";
+import { IConstruct } from "constructs";
+
+class MyResource extends TerraformResource {}
+
+it("Testing.synth() executes Aspects added to constructs", () => {
+  const app = Testing.app();
+  const stack = new TerraformStack(app, "MyStack");
+
+  const StackAspect = { visit: jest.fn() };
+  const ResourceAspect = {
+    visit: jest.fn().mockImplementation((resource: IConstruct) => {
+      const inferredStack = TerraformStack.of(resource);
+      new MyResource(inferredStack, "Resource2", {
+        terraformResourceType: "aws_bucket",
+        terraformGeneratorMetadata: {
+          providerName: "test-provider",
+        },
+      });
+    }),
+  };
+
+  const resource = new MyResource(stack, "Resource1", {
+    terraformResourceType: "aws_bucket",
+    terraformGeneratorMetadata: {
+      providerName: "test-provider",
+    },
+  });
+
+  Aspects.of(stack).add(StackAspect);
+  Aspects.of(resource).add(ResourceAspect);
+
+  expect(() => Testing.synth(stack)).not.toThrow();
+
+  expect(StackAspect.visit).toHaveBeenNthCalledWith(1, stack);
+  expect(StackAspect.visit).toHaveBeenNthCalledWith(2, resource);
+  expect(ResourceAspect.visit).toHaveBeenCalledWith(resource);
+
+  expect(Testing.renderConstructTree(app)).toMatchInlineSnapshot(`
+      "App
+      └── MyStack (TerraformStack)
+          ├── Resource1 (MyResource)
+          └── Resource2 (MyResource)
+      "
+    `);
+});
