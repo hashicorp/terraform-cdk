64639e33f42225eb0421ab333c552f1bff4961f9
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-12-13 18:49:53 +0100

    feat(lib): identify backends

--- packages/cdktf/lib/terraform-backend.ts
+++ packages/cdktf/lib/terraform-backend.ts
@@ -2,9 +2,16 @@ import { Construct } from "constructs";
 import { TerraformElement } from "./terraform-element";
 import { deepMerge } from "./util";
 
+const BACKEND_SYMBOL = Symbol.for("cdktf/TerraformBackend");
+
 export abstract class TerraformBackend extends TerraformElement {
   constructor(scope: Construct, id: string, protected readonly name: string) {
     super(scope, id);
+    Object.defineProperty(this, BACKEND_SYMBOL, { value: true });
+  }
+
+  public static isBackend(x: any): x is TerraformBackend {
+    return x !== null && typeof x === "object" && BACKEND_SYMBOL in x;
   }
 
   // jsii can't handle abstract classes?
