a02fb120950fe3302de81d8bfe6180153642d752
Ansgar Mertens <ansgar@hashicorp.com>
2022-01-19 15:11:14 +0100

    feat(tests): add test which confirms that modules now support sets

--- test/typescript/modules/local-module/main.tf
+++ test/typescript/modules/local-module/main.tf
@@ -4,6 +4,11 @@ variable "enabled" {
   default     = true
 }
 
+variable "set" {
+  description = "Making sure bindings can be created for modules using the set type"
+  type = set(string)
+}
+
 output "create_cmd_bin" {
   description = "The full bin path & command used on create"
   value       = "foo"
