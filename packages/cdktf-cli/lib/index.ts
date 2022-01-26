7f20c2de982ccd2d7953a43009907768f0cfbee7
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-01-26 14:56:52 +0100

    chore: add empty entrypoint

---
+++ packages/cdktf-cli/lib/index.ts
@@ -0,0 +1,2 @@
+// This is the programmatic entrypoint that the CLI uses.
+// While this is the closest we have to a programmatic API, please understand that the interfaces in this file are not stable.
