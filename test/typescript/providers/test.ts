4b914b95a8c2832a400c9190d47395059b0a50b7
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-02-18 11:27:15 +0100

    chore: restructure tests
    for whatever reason deploy was called twice before

--- test/typescript/providers/test.ts
+++ test/typescript/providers/test.ts
@@ -1,118 +1,129 @@
 import { TestDriver } from "../../test-helper";
 
 describe("full integration test", () => {
-  let driver: TestDriver;
-
-  beforeAll(async () => {
-    driver = new TestDriver(__dirname);
-    await driver.setupTypescriptProject();
-  });
-
-  test("build providers", () => {
-    expect(driver.deploy("using-all-providers")).toMatchSnapshot();
-  });
-
-  describe("namespaces", () => {
-    let stack;
-    beforeAll(() => {
-      stack = driver.synthesizedStack("namespaces");
-    });
-
-    test("has resources", () => {
-      expect(stack.byId("instance")).toBeTruthy();
-      expect(stack.byId("lambdaFn")).toBeTruthy();
-    });
-
-    test("has data source", () => {
-      expect(stack.byId("ami")).toBeTruthy();
-      expect(stack.byId("callerIdentity")).toBeTruthy();
+  describe("deploy", () => {
+    test("build providers", async () => {
+      // We have an arcane bug where the deploy is invoked twice although it's just called once.
+      // This is a workaround for that, by havig a new driver every run we don't h
+      const deployDriver = new TestDriver(__dirname);
+      await deployDriver.setupTypescriptProject();
+      expect(deployDriver.deploy("using-all-providers")).toMatchSnapshot();
     });
   });
 
-  describe("provider functionality", () => {
+  describe("synth", () => {
+    let driver: TestDriver;
+
     beforeAll(async () => {
+      driver = new TestDriver(__dirname);
+      await driver.setupTypescriptProject();
       await driver.synth();
     });
 
-    describe("references", () => {
+    describe("namespaces", () => {
       let stack;
       beforeAll(() => {
-        stack = driver.synthesizedStack("references");
+        stack = driver.synthesizedStack("namespaces");
       });
 
-      // HCL does not support referencing provider values
-      test("provider references are no references", () => {
-        const providerAddress = stack.output("provideraddress");
-        expect(providerAddress).toEqual("http://127.0.0.1");
+      test("has resources", () => {
+        expect(stack.byId("instance")).toBeTruthy();
+        expect(stack.byId("lambdaFn")).toBeTruthy();
       });
 
-      test("simple references", () => {
-        const jobSpec = stack.byId("secondJob").jobspec;
-        expect(jobSpec).toContain("${");
-        expect(jobSpec).toContain("firstJob");
-        expect(jobSpec).toContain("jobspec");
-      });
-
-      test("single-item references", () => {
-        const namespace = stack.byId("myDeployment").metadata.namespace;
-        expect(namespace).toContain("${");
-        expect(namespace).toContain("myNamespace");
-        expect(namespace).toContain(".metadata[0].name");
+      test("has data source", () => {
+        expect(stack.byId("ami")).toBeTruthy();
+        expect(stack.byId("callerIdentity")).toBeTruthy();
       });
     });
 
-    describe("mutation", () => {
-      describe("Simple", () => {
-        let instance;
+    describe("provider functionality", () => {
+      beforeAll(async () => {
+        await driver.synth();
+      });
+
+      describe("references", () => {
+        let stack;
         beforeAll(() => {
-          instance = driver.synthesizedStack("mutation").byId("instance");
+          stack = driver.synthesizedStack("references");
         });
 
-        test("direct primitive mutation", () => {
-          expect(instance.instance_type).toBe("t2.small");
+        // HCL does not support referencing provider values
+        test("provider references are no references", () => {
+          const providerAddress = stack.output("provideraddress");
+          expect(providerAddress).toEqual("http://127.0.0.1");
         });
 
-        test("direct reference mutation", () => {
-          expect(instance.ami).toEqual("${data.aws_ami.ami.id}");
+        test("simple references", () => {
+          const jobSpec = stack.byId("secondJob").jobspec;
+          expect(jobSpec).toContain("${");
+          expect(jobSpec).toContain("firstJob");
+          expect(jobSpec).toContain("jobspec");
         });
 
-        test("reset method mutation", () => {
-          expect(instance.availability_zone).not.toBeDefined();
-        });
-
-        test("put method mutation", () => {
-          expect(instance.metadata_options).toEqual({
-            http_endpoint: "127.0.0.1",
-          });
+        test("single-item references", () => {
+          const namespace = stack.byId("myDeployment").metadata.namespace;
+          expect(namespace).toContain("${");
+          expect(namespace).toContain("myNamespace");
+          expect(namespace).toContain(".metadata[0].name");
         });
       });
 
-      describe("single-item nesting", () => {
-        let deployment;
-        beforeAll(() => {
-          deployment = driver.synthesizedStack("mutation").byId("myDeployment");
-        });
-        test("direct primitive mutation", () => {
-          expect(deployment.spec.replicas).toBe("2");
+      describe("mutation", () => {
+        describe("Simple", () => {
+          let instance;
+          beforeAll(() => {
+            instance = driver.synthesizedStack("mutation").byId("instance");
+          });
+
+          test("direct primitive mutation", () => {
+            expect(instance.instance_type).toBe("t2.small");
+          });
+
+          test("direct reference mutation", () => {
+            expect(instance.ami).toEqual("${data.aws_ami.ami.id}");
+          });
+
+          test("reset method mutation", () => {
+            expect(instance.availability_zone).not.toBeDefined();
+          });
+
+          test("put method mutation", () => {
+            expect(instance.metadata_options).toEqual({
+              http_endpoint: "127.0.0.1",
+            });
+          });
         });
 
-        test("direct reference mutation", () => {
-          expect(deployment.metadata.namespace).toContain("${");
-          expect(deployment.metadata.namespace).toContain("var.");
-          expect(deployment.metadata.namespace).toContain("namespace");
-        });
+        describe("single-item nesting", () => {
+          let deployment;
+          beforeAll(() => {
+            deployment = driver
+              .synthesizedStack("mutation")
+              .byId("myDeployment");
+          });
+          test("direct primitive mutation", () => {
+            expect(deployment.spec.replicas).toBe("2");
+          });
 
-        test("reset method mutation", () => {
-          expect(deployment.spec.min_ready_seconds).not.toBeDefined();
-        });
+          test("direct reference mutation", () => {
+            expect(deployment.metadata.namespace).toContain("${");
+            expect(deployment.metadata.namespace).toContain("var.");
+            expect(deployment.metadata.namespace).toContain("namespace");
+          });
 
-        test("put method mutation", () => {
-          expect(deployment.spec.strategy).toEqual({
-            type: "RollingUpdate",
-            rolling_update: {
-              max_surge: "10%",
-              max_unavailable: "10%",
-            },
+          test("reset method mutation", () => {
+            expect(deployment.spec.min_ready_seconds).not.toBeDefined();
+          });
+
+          test("put method mutation", () => {
+            expect(deployment.spec.strategy).toEqual({
+              type: "RollingUpdate",
+              rolling_update: {
+                max_surge: "10%",
+                max_unavailable: "10%",
+              },
+            });
           });
         });
       });
