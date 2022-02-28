d98c83918b5e76b4bbf02f50c1272c9b6cba7ca8
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-02-28 23:46:54 +0100

    chore: fix test assertions

--- test/typescript/diff-deploy-output-destroy/test.ts
+++ test/typescript/diff-deploy-output-destroy/test.ts
@@ -15,27 +15,18 @@ describe("full integration test", () => {
   });
 
   test("deploy", () => {
-    expect(driver.deploy()).toMatchInlineSnapshot(`
-      " Deploying Stack: hello-deploy
-      Resources
-       ✔ NULL_RESOURCE       test                null_resource.test
-
-
-      Summary: 1 created, 0 updated, 0 destroyed.
-
-      Output: output = hello
-              output2 = <sensitive>
-      "
-    `);
+    const output = driver.deploy();
+    expect(output).toContain(`null_resource.test will be created`);
+    expect(output).not.toContain(`"world"`);
+    expect(output).toContain(`output  = "hello"`);
+    expect(output).toContain(`output2 = <sensitive>`);
   });
 
   test("output", () => {
-    expect(driver.output()).toMatchInlineSnapshot(`
-      "
-      Output: output = hello
-              output2 = <sensitive>
-      "
-    `);
+    const output = driver.output();
+    expect(output).not.toContain(`"world"`);
+    expect(output).toContain(`output = hello`);
+    expect(output).toContain(`output2 = <sensitive>`);
   });
 
   it("deploy and output write the same outputs file", () => {
@@ -64,14 +55,8 @@ describe("full integration test", () => {
   });
 
   test("destroy", () => {
-    expect(driver.destroy()).toMatchInlineSnapshot(`
-      " Destroying Stack: hello-deploy
-      Resources
-       ✔ NULL_RESOURCE       test                null_resource.test
-
-
-      Summary: 1 destroyed.
-      "
-    `);
+    const output = driver.destroy();
+    expect(output).toContain(`null_resource.test will be destroyed`);
+    expect(output).toContain(`Destroy complete! Resources: 1 destroyed.`);
   });
 });
