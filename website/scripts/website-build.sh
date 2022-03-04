973fe80ef2c1011851aa256c8dbed3930a9ff998
Bryce Kalow <bkalow@hashicorp.com>
2022-03-04 14:08:59 -0600

    Adjust build script to handle build cache

--- website/scripts/website-build.sh
+++ website/scripts/website-build.sh
@@ -1,9 +1,22 @@
 # Set the subdirectory name for the terraform-website app
 PREVIEW_DIR=website-preview
+CLONE_DIR=website-preview
+
+from_cache=false
+
+if [ -d "$PREVIEW_DIR" ]; then
+  echo "$PREVIEW_DIR found"
+  CLONE_DIR="$PREVIEW_DIR-tmp"
+  from_cache=true
+fi
+
 # Clone the terraform-website project, if needed
-if [ ! -d "$PREVIEW_DIR" ]; then
-    echo "⏳ Cloning the terraform-website repo, this might take a while..."
-    git clone --depth=1 --branch=brk.feat/tfcdk-remote-content https://github.com/hashicorp/terraform-website.git "$PREVIEW_DIR"
+echo "⏳ Cloning the terraform-website repo, this might take a while..."
+git clone --depth=1 --branch=brk.feat/tfcdk-remote-content https://github.com/hashicorp/terraform-website.git "$CLONE_DIR"
+
+if [ "$from_cache" = true ]; then
+  echo "Setting up $PREVIEW_DIR"
+  rsync -a "./$CLONE_DIR/" "./$PREVIEW_DIR"
 fi
 
 # cd into the preview directory project
