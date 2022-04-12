19d0838f8e2e6838bc13e3b3510fa48631269053
Kevin Wang <kwangsan@gmail.com>
2022-04-12 13:50:16 -0400

    test deploy preview

--- website/scripts/website-build.sh
+++ website/scripts/website-build.sh
@@ -7,22 +7,27 @@ CLONE_DIR=website-preview
 from_cache=false
 
 if [ -d "$PREVIEW_DIR" ]; then
-  echo "$PREVIEW_DIR found"
-  CLONE_DIR="$PREVIEW_DIR-tmp"
-  from_cache=true
+    echo "$PREVIEW_DIR found"
+    CLONE_DIR="$PREVIEW_DIR-tmp"
+    from_cache=true
 fi
 
 # Clone the terraform-website project, if needed
 echo "⏳ Cloning the terraform-website repo, this might take a while..."
-git clone --depth=1 --branch=master https://github.com/hashicorp/$REPO_TO_CLONE.git "$CLONE_DIR"
+git clone --depth=1 --branch=kevin/terraform-split https://github.com/hashicorp/$REPO_TO_CLONE.git "$CLONE_DIR"
 
 if [ "$from_cache" = true ]; then
-  echo "Setting up $PREVIEW_DIR"
-  cp -R "./$CLONE_DIR/." "./$PREVIEW_DIR"
+    echo "Setting up $PREVIEW_DIR"
+    cp -R "./$CLONE_DIR/." "./$PREVIEW_DIR"
 fi
 
 # cd into the preview directory project
 cd "$PREVIEW_DIR"
 
 # Run the terraform-website content-repo start script
-REPO=cdktf IS_CONTENT_PREVIEW=true npm run build:deploy-preview
+REPO=cdktf \
+PREVIEW_FROM_REPO=terraform-cdk \
+NAV_DATA_DIRNAME=../data \
+CONTENT_DIRNAME=../docs \
+IS_CONTENT_PREVIEW=true \
+npm run build:deploy-preview
