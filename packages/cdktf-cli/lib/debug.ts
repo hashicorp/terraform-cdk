d3a215b78caf176228a72fc66c2a9a146d612a4d
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-04-20 18:07:40 +0200

    fix: dont send output to stderr

--- packages/cdktf-cli/lib/debug.ts
+++ packages/cdktf-cli/lib/debug.ts
@@ -25,7 +25,7 @@ async function getBinaryVersion(
     const result = await exec(
       binary,
       [versionCommand],
-      { env: process.env, shell: true },
+      { env: process.env, stdio: ["inherit", "inherit", "ignore"] },
       noOp,
       noOp
     );
