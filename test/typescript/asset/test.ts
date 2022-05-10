72453a9dc9364afd6e80da67de41fff6140dfa99
gene-sexton <gene.sexton@axonius.com>
2022-05-10 09:15:55 -0600

    chore: Resolve Issue #1644 (Introduced by PR 1476)

--- test/typescript/asset/test.ts
+++ test/typescript/asset/test.ts
@@ -1,6 +1,7 @@
 import { TestDriver } from "../../test-helper";
 import * as path from "path";
 import * as fs from "fs";
+import { App, TerraformHclModule, TerraformStack, Testing } from "cdktf";
 
 describe("full integration test", () => {
   let driver: TestDriver;
@@ -123,5 +124,117 @@ describe("full integration test", () => {
     );
     await driver.deploy(["normal"]);
     expect(await driver.diff("normal")).toContain("No changes");
+  });
+});
+
+describe("createAssetsFromLocalModules test", () => {
+  test("remote source without createAssetsFromLocalModules", () => {
+    const remoteSource = "terraform-aws-modules/eks/aws";
+
+    const app = Testing.stubVersion(new App({ stackTraces: false }));
+    const stack = new TerraformStack(app, "MyStack");
+
+    const moduleOptionsNotExists = new TerraformHclModule(
+      stack,
+      "moduleOptionsNotExists",
+      {
+        source: remoteSource,
+      }
+    );
+
+    expect(moduleOptionsNotExists.source).toEqual(remoteSource);
+  });
+
+  test("remote source with createAssetsFromLocalModules set to true", () => {
+    const remoteSource = "terraform-aws-modules/eks/aws";
+
+    const app = Testing.stubVersion(new App({ stackTraces: false }));
+    const stack = new TerraformStack(app, "MyStack");
+
+    const moduleOptionsTrue = new TerraformHclModule(
+      stack,
+      "moduleOptionsTrue",
+      {
+        source: remoteSource,
+        createAssetsFromLocalModules: true,
+      }
+    );
+
+    expect(moduleOptionsTrue.source).toEqual(remoteSource);
+  });
+
+  test("remote source with createAssetsFromLocalModules set to false", () => {
+    const remoteSource = "terraform-aws-modules/eks/aws";
+
+    const app = Testing.stubVersion(new App({ stackTraces: false }));
+    const stack = new TerraformStack(app, "MyStack");
+
+    const moduleOptionsFalse = new TerraformHclModule(
+      stack,
+      "moduleOptionsFalse",
+      {
+        source: remoteSource,
+        createAssetsFromLocalModules: false,
+      }
+    );
+
+    expect(moduleOptionsFalse.source).toEqual(remoteSource);
+  });
+
+  test("local source without createAssetsFromLocalModules", () => {
+    const localSource = "../";
+    const assetRegex =
+      /^.\/assets\/MyStack_localmoduleid_[a-zA-z\d]{8}\/[a-zA-z\d]+$/;
+
+    const app = Testing.stubVersion(new App({ stackTraces: false }));
+    const stack = new TerraformStack(app, "MyStack");
+
+    const moduleOptionsNotExists = new TerraformHclModule(
+      stack,
+      "moduleOptionsNotExists",
+      {
+        source: localSource,
+      }
+    );
+
+    expect(moduleOptionsNotExists.source).toMatch(assetRegex);
+  });
+
+  test("local source with createAssetsFromLocalModules set to true", () => {
+    const localSource = "../";
+    const assetRegex =
+      /^.\/assets\/MyStack_localmoduleid_[a-zA-z\d]{8}\/[a-zA-z\d]+$/;
+
+    const app = Testing.stubVersion(new App({ stackTraces: false }));
+    const stack = new TerraformStack(app, "MyStack");
+
+    const moduleOptionsTrue = new TerraformHclModule(
+      stack,
+      "moduleOptionsTrue",
+      {
+        source: localSource,
+        createAssetsFromLocalModules: true,
+      }
+    );
+
+    expect(moduleOptionsTrue.source).toMatch(assetRegex);
+  });
+
+  test("local source with createAssetsFromLocalModules set to false", () => {
+    const localSource = "../";
+
+    const app = Testing.stubVersion(new App({ stackTraces: false }));
+    const stack = new TerraformStack(app, "MyStack");
+
+    const moduleOptionsFalse = new TerraformHclModule(
+      stack,
+      "moduleOptionsFalse",
+      {
+        source: localSource,
+        createAssetsFromLocalModules: false,
+      }
+    );
+
+    expect(moduleOptionsFalse.source).toEqual(localSource);
   });
 });
