0e585977a9d8c8b4f8c2530c79b417e098d6c659
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-05-23 10:36:11 +0200

    chore: format

--- packages/cdktf/lib/backends/http-backend.ts
+++ packages/cdktf/lib/backends/http-backend.ts
@@ -35,16 +35,16 @@ export class DataTerraformRemoteStateHttp extends TerraformRemoteState {
 }
 /**
  * Stores the state using a simple REST client.
- * 
- * State will be fetched via GET, updated via POST, and purged with DELETE. 
+ *
+ * State will be fetched via GET, updated via POST, and purged with DELETE.
  * The method used for updating is configurable.
- * 
- * This backend optionally supports state locking. 
- * When locking support is enabled it will use LOCK and UNLOCK requests providing the lock info in the body. 
- * The endpoint should return a 423: Locked or 409: Conflict with the holding lock info when 
- * it's already taken, 200: OK for success. Any other status will be considered an error. 
+ *
+ * This backend optionally supports state locking.
+ * When locking support is enabled it will use LOCK and UNLOCK requests providing the lock info in the body.
+ * The endpoint should return a 423: Locked or 409: Conflict with the holding lock info when
+ * it's already taken, 200: OK for success. Any other status will be considered an error.
  * The ID of the holding lock info will be added as a query parameter to state updates requests.
- * 
+ *
  * Read more about this backend in the Terraform docs:
  * https://www.terraform.io/language/settings/backends/http
  */
