6a5a0c67d088d82210242359023bd6f01ac1d699
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-06-10 21:52:06 +0200

    chore: split provider tests into one test per provider
    They are only triggered if something in the provider generator changes

---
+++ test/provider-tests/template/test.ts
@@ -0,0 +1,14 @@
+import { TestDriver } from "../../../test-helper";
+
+describe("full integration test", () => {
+  describe("deploy", () => {
+    test("build providers", async () => {
+      const driver = new TestDriver(__dirname);
+      await driver.setupTypescriptProject();
+      await driver.synth();
+
+      const stack = driver.synthesizedStack("using-all-providers");
+      expect(stack.byId("test")).toBeTruthy();
+    }, 600_000);
+  });
+});
