f2e273feed0a925430e13b7a52f0a6306afb9e64
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-07-21 14:40:43 +0200

    fix: use main stack and call class program

--- packages/cdktf-cli/templates/csharp/Program.cs
+++ packages/cdktf-cli/templates/csharp/Program.cs
@@ -2,15 +2,14 @@ using System;
 using Constructs;
 using HashiCorp.Cdktf;
 
-
 namespace MyCompany.MyApp
 {
-    class MyApp : Program
+    class Program
     {
         public static void Main(string[] args)
         {
             App app = new App();
-            new MyApp(app, "{{ $base }}");
+            new MainStack(app, "{{ $base }}");
             app.Synth();
             Console.WriteLine("App synth complete");
         }
