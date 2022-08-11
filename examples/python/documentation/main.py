77ca17a045418a89e15763e1a156b1ac71917385
Ansgar Mertens <ansgar@hashicorp.com>
2022-08-11 15:10:23 +0200

    chore: add snippet markers for python examples in assets and constructs docs and update the docs

--- examples/python/documentation/main.py
+++ examples/python/documentation/main.py
@@ -1,35 +1,44 @@
+# DOCS_BLOCK_START:assets,constructs
 from constructs import Construct
-from cdktf import App, TerraformStack
-from imports.aws import AwsProvider, s3
+import cdktf
+# DOCS_BLOCK_END:constructs
+import imports.aws as aws
+# DOCS_BLOCK_START:constructs
 import os
-
+# DOCS_BLOCK_END:assets
+import imports.kubernetes as kubernetes
 from my_constructs import KubernetesWebAppDeployment
+# DOCS_BLOCK_END:constructs
+# DOCS_BLOCK_START:assets,constructs
 
-class MyStack(TerraformStack):
-    def __init__(self, scope: Construct, ns: str):
-        super().__init__(scope, ns)
-        AwsProvider(self, 'Aws', region='eu-central-1')
 
-        # concepts/assets.mdx
-        bucket = s3.S3Bucket(self, "bucket",
-            bucket="demo"
-        )
+class MyStack(cdktf.TerraformStack):
+    def __init__(self, scope: Construct, name: str):
+        super().__init__(scope, name)
+        # DOCS_BLOCK_END:constructs
 
-        asset = TerraformAsset(self, "lambda-asset",
-            path=os.path.join(os.path.dirname(__file__), '..', 'lambda'),
-            type=AssetType.ARCHIVE
-        )
+        aws.AwsProvider(self, 'aws', region='eu-central-1')
 
-        s3.S3BucketObject(self, "lambda-archive",
-            bucket=bucket.bucket,
-            key=asset.file_name,
-            source=asset.path
-        )
+        bucket = aws.s3.S3Bucket(self, "bucket", bucket="demo")
 
-        # concepts/constructs.mdx
+        asset = cdktf.TerraformAsset(self, "lambda-asset",
+                                     path=os.path.join(os.path.dirname(
+                                         __file__), '..', 'lambda'),
+                                     type=cdktf.AssetType.ARCHIVE
+                                     )
+
+        aws.s3.S3BucketObject(self, "lambda-archive",
+                              bucket=bucket.bucket,
+                              key=asset.file_name,
+                              source=asset.path
+                              )
+        # DOCS_BLOCK_END:assets
+
+        # DOCS_BLOCK_START:constructs
         kubernetes.KubernetesProvider(self, "kind",
-            config_path=os.path.join(os.path.dirname(__file__), '..', 'kubeconfig.yaml')
-        )
+                                      config_path=os.path.join(os.path.dirname(
+                                          __file__), '..', 'kubeconfig.yaml')
+                                      )
 
         KubernetesWebAppDeployment(self, "deployment", {
             "image": "nginx:latest",
@@ -38,8 +47,12 @@ class MyStack(TerraformStack):
             "component": "frontend",
             "environment": "dev"
         })
+        # DOCS_BLOCK_END:constructs
+# DOCS_BLOCK_START:assets,constructs
 
-app = App()
-MyStack(app, "python-documentation")
+
+app = cdktf.App()
+MyStack(app, "demo")
 
 app.synth()
+# DOCS_BLOCK_END:assets,constructs
