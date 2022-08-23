981be66861e578f97f58ecc87010662246adcc96
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-08-23 14:32:35 +0200

    fix: support provisioners in JSII languages

---
+++ examples/python/provisioner/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from constructs import Construct
+from cdktf import App, TerraformStack
+from imports.null import NullProvider, Resource
+
+
+class MyStack(TerraformStack):
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
+
+        NullProvider(self, "provider")
+
+        Resource(self, "resource", provisioners=[{
+            "type": "local-exec",
+            "command": "echo 'hello world'"
+        }])
+
+
+app = App()
+MyStack(app, "python-provisioner")
+
+app.synth()
