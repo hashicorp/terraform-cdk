fa650e346fc9cac8d8328279704f049e7a5a59ba
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-09-09 18:03:55 +0200

    chore: run copyright script in lint-staged task

--- tools/add-copyright-header.sh
+++ tools/add-copyright-header.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-set -ex
+set -e
 scriptdir="$(cd $(dirname $0) && pwd)"
 
 for i in $(git ls-files | grep '.ts$'); do
-  if ! grep -m1 -q Copyright $i
-  then
-    cat $scriptdir/license-header.txt $i >$i.new && mv $i.new $i
-  fi
+  bash $scriptdir/add-copyright-header-to-file.sh $i
 done
