c837a9de1306a6f3cfbd751f56a1a346386d68c5
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-09-12 10:19:07 +0200

    chore: ignore template file

--- tools/add-copyright-header-to-file.sh
+++ tools/add-copyright-header-to-file.sh
@@ -3,7 +3,13 @@
 set -e
 scriptdir="$(cd $(dirname $0) && pwd)"
 
-if ! grep -m1 -q Copyright $1
-then
+IGNORED_FILES=(packages/cdktf-cli/templates/typescript/main.ts)
+
+if echo $IGNORED_FILES | grep -w -q $1; then
+  echo "Skipping $1"
+  exit 0
+fi
+
+if ! grep -m1 -q Copyright $1; then
 cat $scriptdir/license-header.txt $1 >$1.new && mv $1.new $1
 fi
