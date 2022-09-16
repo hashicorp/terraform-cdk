5d024a7ae649c4713d03bbf7886f891509aa5e14
Ansgar Mertens <ansgar@hashicorp.com>
2022-09-16 13:52:44 +0200

    feat(cli): include language used in cdktf init telemetry event

--- packages/cdktf-cli/bin/cmds/helper/init.ts
+++ packages/cdktf-cli/bin/cmds/helper/init.ts
@@ -36,6 +36,7 @@ import ciDetect from "@npmcli/ci-detect";
 import { isInteractiveTerminal } from "./check-environment";
 import { getTerraformVersion } from "./terraform-check";
 import * as semver from "semver";
+import { CdktfConfig } from "../../../lib/cdktf-config";
 
 const chalkColour = new chalk.Instance();
 
@@ -236,7 +237,12 @@ This means that your Terraform state file will be stored locally on disk in a fi
     await templateInfo.cleanupTemporaryFiles();
   }
 
-  await sendTelemetry("init", telemetryData);
+  const cdktfConfig = CdktfConfig.read(destination);
+
+  await sendTelemetry("init", {
+    ...telemetryData,
+    language: cdktfConfig.language,
+  });
 }
 
 function copyLocalModules(
