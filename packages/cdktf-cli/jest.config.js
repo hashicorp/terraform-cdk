434c266c07acdef82e80cc621ff5329c27acd9fb
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-09-20 17:02:50 +0200

    Revert "Use esbuild-jest instead of ts-jest"
    This reverts commit 9edde07357d22a9ccda3418d80190a6d349b1929.

--- packages/cdktf-cli/jest.config.js
+++ packages/cdktf-cli/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   testMatch: ['**/*.test.ts', '**/*.test.tsx'],
   transform: {
-    "^.+\\.tsx?$": "esbuild-jest"
+    "^.+\\.tsx?$": "ts-jest"
   },
   moduleFileExtensions: [
     "js",
