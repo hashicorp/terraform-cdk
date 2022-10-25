4183de241070ee0917aae988325e06e30a17039a
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-10-25 23:09:21 +0200

    chore: migrate to new github output syntax

--- tools/build-provider-test-matrix.sh
+++ tools/build-provider-test-matrix.sh
@@ -7,4 +7,4 @@ cd $(dirname $0)/..
 tests=$( cat ./test/provider-tests/providers.json | jq -c 'keys')
 matrix="{ \"target\": $tests }"
 echo $matrix
-echo "::set-output name=tests::$matrix"
+echo "tests=$matrix" >> $GITHUB_OUTPUT
