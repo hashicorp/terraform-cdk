ae6cdf98de7a54269a8f16eac88c6304f1c89007
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-11-03 15:29:06 +0100

    chore: add the provider command

---
+++ packages/cdktf-cli/src/bin/cmds/provider-list.ts
@@ -0,0 +1,22 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
+import yargs from "yargs";
+import { requireHandlers } from "./helper/utilities";
+import { Errors } from "../../lib/errors";
+import { BaseCommand } from "./helper/base-command";
+
+class ProviderListCommand extends BaseCommand {
+  public readonly command = "list";
+  public readonly describe = "List installed providers";
+
+  public readonly builder = (args: yargs.Argv) => args.showHelpOnFail(true);
+
+  public async handleCommand(argv: any) {
+    Errors.setScope("provider list");
+    // deferred require to keep cdktf-cli main entrypoint small (e.g. for fast shell completions)
+    const api = requireHandlers();
+    await api.providerList(argv);
+  }
+}
+
+module.exports = new ProviderListCommand();
