c0564b5eb3653f324a02e9d98bcbfe8b791d4c2c
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-11-08 18:05:43 +0100

    WIP

--- examples/csharp/documentation/AspectsStack.cs
+++ examples/csharp/documentation/AspectsStack.cs
@@ -1,30 +1,48 @@
 // DOCS_BLOCK_START:aspect
-using System;
-using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 using Constructs;
 using HashiCorp.Cdktf;
 using aws.S3Bucket;
-
+using Amazon.JSII.Runtime;
 
 namespace Examples
 {
-    class TagsAddingAspect : IAspect {
-        Dictionary<string, string> tagsToAdd;
-        public TagsAddingAspect(Dictionary<string, string> tagsToAdd) {
+    public class TagsAddingAspect: IAspect, Amazon.Jsii.Runtime.Deputy.DeputyBase
+    {
+        private readonly IDictionary<string, string> tagsToAdd;
+
+        public TagsAddingAspect(IDictionary<string, string> tagsToAdd)
+        {
             this.tagsToAdd = tagsToAdd;
         }
 
-        // This method is called on every Construct within the specified scope (resources, data sources, etc.).
-        public void Visit(IConstruct node) {
-            if ((node.GetType().GetProperty("tags") != null) && (node.GetType()GetProperty("tagsInput") != null)) {
-                // We need to take the input value to not create a circular reference
-                node.tagsInput = node.tagsInput.Concat(this.tagsToAdd).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        public void Visit(IConstruct node)
+        {
+            var nodeType = node.GetType();
+            var tagsProperty = nodeType.GetProperty("Tags");
+            var tagsInputProperty = nodeType.GetProperty("TagsInput");
+            if (tagsProperty != null && tagsInputProperty != null)
+            {
+                var inputTags = (IDictionary<string, string>) tagsInputProperty.GetValue(node);
+
+                IDictionary<string, string> newTags;
+                if (inputTags == null)
+                {
+                    newTags = tagsToAdd;
+                }
+                else
+                {
+                    newTags = new Dictionary<string, string>(tagsToAdd);
+                    foreach ((string key, string value) in inputTags)
+                    {
+                        newTags[key] = value;
+                    }
+                }
+                
+                tagsProperty.SetValue(node, newTags);
             }
         }
     }
-
 
     class MyAspectsStack : TerraformStack
     {
