dbf8dbde64f25466c23853269e6e45b9945263b6
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-11-11 17:24:40 +0100

    chore(docs): translate functions examples to c#

--- examples/csharp/documentation/Main.cs
+++ examples/csharp/documentation/Main.cs
@@ -21,6 +21,7 @@ namespace MyCompany.MyApp
             App app = new App();
             new Examples.MyAssetStack(app, "assets");
             new Examples.MyConstructsStack(app, "constructs");
+            new Examples.FunctionStack(app, "function");
             app.Synth();
             Console.WriteLine("App synth complete");
         }
