8ee4560716834397c547a2a6ba3adcc448d58c4c
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-12-02 17:00:46 +0100

    chore: copy main.wasm.gz to build

--- packages/@cdktf/hcl2json/build-go.sh
+++ packages/@cdktf/hcl2json/build-go.sh
@@ -3,4 +3,7 @@ set -euo pipefail
 
 GOOS=js GOARCH=wasm go get .
 GOOS=js GOARCH=wasm go build  -ldflags="-s -w" -o main.wasm
-gzip -9 -v -c main.wasm > build/main.wasm.gz
+gzip -9 -v -c main.wasm > main.wasm.gz
+
+# We need it in both root and build folder for npm pack
+cp main.wasm.gz build/main.wasm.gz
