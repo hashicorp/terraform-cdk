9ad48df5bb64b03528ae9b140faae1efc7a581e2
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-12-02 14:34:19 +0100

    chore: use an outdir from hcl2json

--- packages/@cdktf/hcl2json/build-go.sh
+++ packages/@cdktf/hcl2json/build-go.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 GOOS=js GOARCH=wasm go get .
 GOOS=js GOARCH=wasm go build  -ldflags="-s -w" -o main.wasm
-gzip -9 -v -c main.wasm > main.wasm.gz
+gzip -9 -v -c main.wasm > build/main.wasm.gz
