2256146db3a828ae6fcb0441510963c502da31aa
Mark DeCrane <mark.decrane@hashicorp.com>
2022-12-02 11:49:25 -0500

    changed version in init of csharp integration test

--- test/csharp/no-color-command-option-test/test.ts
+++ test/csharp/no-color-command-option-test/test.ts
@@ -8,13 +8,8 @@ describe("no-color option for cdktf deploy, diff, destroy", () => {
     /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
   let driver: TestDriver;
   beforeAll(async () => {
-    driver = new TestDriver(__dirname, {
-      CDKTF_DIST: "",
-      DISABLE_VERSION_CHECK: "true",
-    }); // reset CDKTF_DIST set by run-against-dist script & disable version check as we have to use an older version of cdktf-cli
-    await driver.setupCsharpProject({
-      init: { additionalOptions: "--cdktf-version 0.14.1" },
-    });
+    driver = new TestDriver(__dirname);
+    await driver.setupCsharpProject();
   }, 500_000);
 
   it("contains no color formatting in cdktf deploy", async () => {
