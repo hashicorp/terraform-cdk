ebd0943a946abb2ce052b589fdca2f0b6fb5919a
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-12-09 12:49:26 +0100

    chore: Add aspects examples, and use new layout for docs examples

---
+++ examples/typescript/documentation/aspect-tagging.ts
@@ -0,0 +1,51 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
+// DOCS_BLOCK_START:aspect-tagging
+import { Construct, IConstruct } from "constructs";
+import { Aspects, IAspect, TerraformStack, App } from "cdktf";
+import { AwsProvider } from "./.gen/providers/aws/provider";
+import { S3Bucket } from "./.gen/providers/aws/s3-bucket";
+
+// Not all constructs are taggable, so we need to filter it
+type TaggableConstruct = IConstruct & {
+  tags?: { [key: string]: string };
+  tagsInput?: { [key: string]: string };
+};
+
+function isTaggableConstruct(x: IConstruct): x is TaggableConstruct {
+  return "tags" in x && "tagsInput" in x;
+}
+
+export class TagsAddingAspect implements IAspect {
+  constructor(private tagsToAdd: Record<string, string>) {}
+
+  // This method is called on every Construct within the specified scope (resources, data sources, etc.).
+  visit(node: IConstruct) {
+    if (isTaggableConstruct(node)) {
+      // We need to take the input value to not create a circular reference
+      const currentTags = node.tagsInput || {};
+      node.tags = { ...this.tagsToAdd, ...currentTags };
+    }
+  }
+}
+
+export class AspectTaggingStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new AwsProvider(this, "aws", {
+      region: "us-west-2",
+    });
+
+    new S3Bucket(this, "bucket", {
+      bucket: "demo",
+      tags: {
+        owner: "cdktf",
+      },
+    });
+
+    // Add tags to every resource defined within this stack.
+    Aspects.of(this).add(new TagsAddingAspect({ createdBy: "cdktf" }));
+  }
+}
+// DOCS_BLOCK_END:aspect-tagging
--- examples/typescript/documentation/main.ts
+++ examples/typescript/documentation/main.ts
@@ -1,70 +1,12 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
-// DOCS_BLOCK_START:assets,constructs
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
-// DOCS_BLOCK_END:assets,constructs
-
-// DOCS_BLOCK_START:assets
-import { TerraformAsset, AssetType } from "cdktf";
-import { AwsProvider } from "./.gen/providers/aws/provider";
-import { S3Bucket } from "./.gen/providers/aws/s3-bucket";
-import { S3BucketObject } from "./.gen/providers/aws/s3-bucket-object";
-// DOCS_BLOCK_START:constructs
-import * as path from "path";
-// DOCS_BLOCK_END:assets
-
-import { KubernetesWebAppDeployment } from "./custom-constructs";
-import { KubernetesProvider } from "./.gen/providers/kubernetes/provider";
-
-// DOCS_BLOCK_END:constructs
-
-// DOCS_BLOCK_START:assets,constructs
-
-class MyStack extends TerraformStack {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
-
-    // DOCS_BLOCK_END:assets,constructs
-    // DOCS_BLOCK_START:assets
-    new AwsProvider(this, "aws", {
-      region: "us-west-2",
-    });
-
-    const bucket = new S3Bucket(this, "bucket", {
-      bucket: "demo",
-    });
-
-    const asset = new TerraformAsset(this, "lambda-asset", {
-      path: path.resolve(__dirname, "lambda"),
-      type: AssetType.ARCHIVE, // if left empty it infers directory and file
-    });
-
-    new S3BucketObject(this, "lambda-archive", {
-      bucket: bucket.bucket,
-      key: asset.fileName,
-      source: asset.path, // returns a posix path
-    });
-    // DOCS_BLOCK_END:assets
-
-    // DOCS_BLOCK_START:constructs
-    new KubernetesProvider(this, "kind", {
-      configPath: path.join(__dirname, "../kubeconfig.yaml"),
-    });
-
-    new KubernetesWebAppDeployment(this, "deployment", {
-      image: "nginx:latest",
-      replicas: 2,
-      app: "myapp",
-      component: "frontend",
-      environment: "dev",
-    });
-    // DOCS_BLOCK_END:constructs
-    // DOCS_BLOCK_START:assets,constructs
-  }
-}
+import { App } from "cdktf";
+import { AspectTaggingStack } from "./aspect-tagging";
+import { AspectValidationStack } from "./aspect-validation";
 
 const app = new App();
-new MyStack(app, "demo");
+
+new AspectTaggingStack(app, "aspect-tagging");
+new AspectValidationStack(app, "aspect-validation");
+
 app.synth();
-// DOCS_BLOCK_END:assets,constructs
