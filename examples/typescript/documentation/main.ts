2819e8e0f2917332cc5732941d3b3b945ec35ff4
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-12-09 12:52:48 +0100

    chore: add back assets example

--- examples/typescript/documentation/main.ts
+++ examples/typescript/documentation/main.ts
@@ -3,10 +3,12 @@
 import { App } from "cdktf";
 import { AspectTaggingStack } from "./aspect-tagging";
 import { AspectValidationStack } from "./aspect-validation";
+import { AssetsStack } from "./assets";
 
 const app = new App();
 
 new AspectTaggingStack(app, "aspect-tagging");
 new AspectValidationStack(app, "aspect-validation");
+new AssetsStack(app, "assets");
 
 app.synth();
