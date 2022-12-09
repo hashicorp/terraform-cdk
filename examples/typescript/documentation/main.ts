7f15e4fdfa4b224f2facc182d7e1d0070673d9b9
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-12-09 13:04:20 +0100

    chore: Add back constructs example

--- examples/typescript/documentation/main.ts
+++ examples/typescript/documentation/main.ts
@@ -4,11 +4,13 @@ import { App } from "cdktf";
 import { AspectTaggingStack } from "./aspect-tagging";
 import { AspectValidationStack } from "./aspect-validation";
 import { AssetsStack } from "./assets";
+import { ConstructsStack } from "./constructs";
 
 const app = new App();
 
 new AspectTaggingStack(app, "aspect-tagging");
 new AspectValidationStack(app, "aspect-validation");
 new AssetsStack(app, "assets");
+new ConstructsStack(app, "constructs");
 
 app.synth();
