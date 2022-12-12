49189e386768eae764d816e4bcc0b8f33655db54
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-12-12 16:33:38 +0100

    chore: fix script

--- packages/@cdktf/api/scripts/pre-pack.js
+++ packages/@cdktf/api/scripts/pre-pack.js
@@ -7,12 +7,44 @@ const packagePathsToCopy = Object.entries({
   "@cdktf/commons": "packages/@cdktf/commons",
 }).map(([packageName, value]) => [packageName, path.join(projectRoot, value)]);
 
+const localNodeModules = path.resolve(__dirname, "..", "node_modules");
+
+// Start by removing node_modules / everything symlinked here will be copied over
+fs.removeSync(localNodeModules);
+fs.mkdirSync(localNodeModules);
+
+console.log("Copying local packages to local node_modules");
 packagePathsToCopy.forEach(([packageName, packagePath]) => {
-  fs.copySync(
-    packagePath,
-    path.resolve(__dirname, "..", "node_modules", packageName),
-    {
-      recursive: true,
-    }
-  );
+  fs.copySync(packagePath, path.resolve(localNodeModules, packageName), {
+    recursive: true,
+  });
+});
+
+console.log("Copying global node_modules to local node_modules");
+// Copy all root level node modules
+fs.readdirSync(path.join(projectRoot, "node_modules")).forEach((file) => {
+  const from = path.join(projectRoot, "node_modules", file);
+  const to = path.join(localNodeModules, file);
+
+  if (file === ".bin") {
+    console.log(".bin", from);
+    return;
+  }
+
+  // ignore symlinks
+  if (fs.lstatSync(from).isSymbolicLink()) {
+    console.log("Symlink", from);
+    return;
+  }
+
+  // ignore local packages
+  if (file.startsWith("@cdktf")) {
+    console.log("@cdktf", from);
+    return;
+  }
+
+  console.log(`Copying ${from} to ${to}`);
+  fs.copySync(from, to, {
+    recursive: true,
+  });
 });
