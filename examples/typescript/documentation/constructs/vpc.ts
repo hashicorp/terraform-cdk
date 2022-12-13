ba9e159768dc8183c5d0352c82adb771a14a1d9b
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-12-13 12:39:30 +0100

    chore: Convert stacks typescript documentation

---
+++ examples/typescript/documentation/constructs/vpc.ts
@@ -0,0 +1,14 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
+import { Construct } from "constructs";
+
+export class Vpc extends Construct {
+  constructor(
+    public scope: Construct,
+    public id: string,
+    public props: Record<string, any> // Could be typed better, is not in this case
+  ) {
+    super(scope, id);
+    // Content is left to the readers imagination
+  }
+}
