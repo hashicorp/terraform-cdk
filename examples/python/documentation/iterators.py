caf501b5d3b23deebd7bdee66dc49aca51f9ca24
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-01-04 09:15:46 +0500

    fix: use map for complex iterator in python

--- examples/python/documentation/iterators.py
+++ examples/python/documentation/iterators.py
@@ -15,32 +15,33 @@ from imports.aws.s3_bucket import S3Bucket
 from cdktf import TerraformIterator, TerraformVariable, TerraformLocal
 # DOCS_BLOCK_END:iterators-define-iterators,iterators-iterators-complex-types
 
+
 class IteratorStackOne(TerraformStack):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
         AwsProvider(self, "aws", region="us-east-1")
 
         # DOCS_BLOCK_START:iterators-iterators-complex-types
-        list = TerraformLocal(self, "my-list", [
-            {
+        map = TerraformLocal(self, "my-map", {
+            "website": {
                 "name": "website-static-files",
                 "tags": {"app": "website"}
             },
-            {
+            "images": {
                 "name": "images",
                 "tags": {"app": "image-converter"}
             }
-        ])
+        })
 
         iterator = TerraformIterator.from_list(
-            list=list.as_list
+            map=map.as_any_map
         )
 
         s3Bucket = S3Bucket(self, "s3-bucket",
-            for_each=iterator,
-            bucket=iterator.get_string("name"),
-            tags=iterator.get_map("tags")
-        )
+                            for_each=iterator,
+                            bucket=iterator.get_string("name"),
+                            tags=iterator.get_map("tags")
+                            )
         # DOCS_BLOCK_END:iterators-iterators-complex-types
 
         # DOCS_BLOCK_START:iterators-count
@@ -66,42 +67,41 @@ class IteratorStackTwo(TerraformStack):
         AwsProvider(self, "aws", region="us-east-1")
         GithubProvider(self, "gh")
         # DOCS_BLOCK_START:iterators-define-iterators
-        
+
         list = TerraformVariable(self, "list",
-            type="list(string)"
-        )
+                                 type="list(string)"
+                                 )
 
         iterator = TerraformIterator.from_list(list=list.list_value)
 
         s3Bucket = S3Bucket(self, "bucket",
-            for_each=iterator,
-            bucket=Token.as_string(iterator.value)
-        )
+                            for_each=iterator,
+                            bucket=Token.as_string(iterator.value)
+                            )
         # DOCS_BLOCK_END:iterators-define-iterators
 
         # DOCS_BLOCK_START:iterators-list-attributes
         org_name = "my-org"
 
         GithubProvider(self, "github",
-            organization=org_name
-        )
+                       organization=org_name
+                       )
 
         github_team = Team(self, "core-team",
-            name="core"
-        )
+                           name="core"
+                           )
 
         org_members = DataGithubOrganization(self, "org",
-            name=org_name
-        )
+                                             name=org_name
+                                             )
 
         org_member_iterator = TerraformIterator.from_list(org_members.members)
 
         TeamMembers(self, "members",
-            team_id=github_team.id,
-            members=org_member_iterator.dynamic({
-                "username": Token().as_string(org_member_iterator.value),
-                "role": "maintainer"
-            })
-        )
+                    team_id=github_team.id,
+                    members=org_member_iterator.dynamic({
+                        "username": Token().as_string(org_member_iterator.value),
+                        "role": "maintainer"
+                    })
+                    )
         # DOCS_BLOCK_END:iterators-list-attributes
-
