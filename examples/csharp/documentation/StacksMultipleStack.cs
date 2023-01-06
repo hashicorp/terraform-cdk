f7bf1dec5b008cb07baec55b0405b373bcdb4234
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-01-06 10:58:09 +0100

    chore: remove comment

--- examples/csharp/documentation/StacksMultipleStack.cs
+++ examples/csharp/documentation/StacksMultipleStack.cs
@@ -7,53 +7,6 @@ using HashiCorp.Cdktf;
 using aws.Provider;
 using aws.Instance;
 
-
-// import { Construct } from "constructs";
-// import { App, TerraformStack } from "cdktf";
-// import { AwsProvider } from "@cdktf/provider-aws/lib/aws-provider";
-// import { Instance } from "@cdktf/provider-aws/lib/instance";
-
-// interface MyMultiStackConfig {
-//   environment: string;
-//   region?: string;
-// }
-
-// class MyMultiStack extends TerraformStack {
-//   constructor(scope: Construct, id: string, config: MyMultiStackConfig) {
-//     super(scope, id);
-
-//     const { region = "us-east-1" } = config;
-
-//     new AwsProvider(this, "aws", {
-//       region,
-//     });
-
-//     new Instance(this, "Hello", {
-//       ami: "ami-2757f631",
-//       instanceType: "t2.micro",
-//       tags: {
-//         environment: config.environment,
-//       },
-//     });
-//   }
-// }
-
-// const multiStackApp = new App();
-// new MyMultiStack(multiStackApp, "multiple-stacks-dev", {
-//   environment: "dev",
-// });
-// new MyMultiStack(multiStackApp, "multiple-stacks-staging", {
-//   environment: "staging",
-// });
-// new MyMultiStack(multiStackApp, "multiple-stacks-production-us", {
-//   environment: "production",
-//   region: "us-east-1",
-// });
-// new MyMultiStack(multiStackApp, "multiple-stacks-production-eu", {
-//   environment: "production",
-//   region: "eu-central-1",
-// });
-// multiStackApp.synth();
 namespace Examples
 {
     // DOCS_BLOCK_START:multiple-stacks
