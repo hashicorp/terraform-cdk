a861d23cb7db5703e46e34e4e4f1ed2ddb8da86d
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-01-10 16:48:34 +0100

    chore: fix example

--- examples/java/documentation/src/main/java/com/mycompany/app/modules/MainModuleExample.java
+++ examples/java/documentation/src/main/java/com/mycompany/app/modules/MainModuleExample.java
@@ -6,13 +6,13 @@ import com.hashicorp.cdktf.TerraformOutputConfig;
 import com.hashicorp.cdktf.TerraformStack;
 import software.constructs.Construct;
 import imports.my_local_module.MyLocalModule;
-import imports.my_local_module.MyLocalModuleOptions;
+import imports.my_local_module.MyLocalModuleConfig;
 public class MainModuleExample extends TerraformStack {
 
     public MainModuleExample(Construct scope, String id){
         super(scope, id);
 
-        MyLocalModule localModule = new MyLocalModule(this, "local-module", MyLocalModuleOptions.builder()
+        MyLocalModule localModule = new MyLocalModule(this, "local-module", MyLocalModuleConfig.builder()
                 .ipAddress("127.0.0.1")
                 .build()
         );
