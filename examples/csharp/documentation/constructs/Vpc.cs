58983ab9751297e1d0a70881fbf5ea90125ce687
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-01-30 09:47:17 +0100

    chore: more translations to C#

---
+++ examples/csharp/documentation/constructs/Vpc.cs
@@ -0,0 +1,12 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
+using Constructs;
+using System.Collections.Generic;
+
+class Vpc : Construct
+{
+    public Vpc(Construct scope, string id, Dictionary<string, object> props) : base(scope, id)
+    {
+        // Content is left to the readers imagination
+    }
+}
