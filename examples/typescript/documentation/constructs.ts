da8d535463a4ea2a4f7790951a404643e4f5a071
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-01-31 19:45:55 +0100

    chore: Add constructs-scope example in typescript

--- examples/typescript/documentation/constructs.ts
+++ examples/typescript/documentation/constructs.ts
@@ -1,9 +1,17 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
 
-// DOCS_BLOCK_START:constructs
+// DOCS_BLOCK_START:constructs-scope
+import { S3Bucket } from "./.gen/providers/aws/s3-bucket";
+import { S3BucketWebsiteConfiguration } from "./.gen/providers/aws/s3-bucket-website-configuration";
+import { AwsProvider } from "./.gen/providers/aws/provider";
+// DOCS_BLOCK_END:constructs-scope
+
+// DOCS_BLOCK_START:constructs,constructs-scope
 import { Construct } from "constructs";
 import { TerraformStack } from "cdktf";
+// DOCS_BLOCK_END:constructs,constructs-scope
+// DOCS_BLOCK_START:constructs
 import { KubernetesWebAppDeployment } from "./constructs/kubernetes-web-app-deployment";
 import { KubernetesProvider } from "./.gen/providers/kubernetes/provider";
 import * as path from "path";
@@ -26,3 +34,44 @@ export class ConstructsStack extends TerraformStack {
   }
 }
 // DOCS_BLOCK_END:constructs
+
+// DOCS_BLOCK_START:constructs-scope
+
+export class PublicS3Bucket extends Construct {
+  public bucket: S3Bucket;
+  constructor(scope: Construct, name: string) {
+    super(scope, name); // This creates a new scope since we extend from construct
+
+    // This bucket is in a different scope than the buckets
+    // defined in `MyStack`. Therefore, it does not need a unique name.
+    this.bucket = new S3Bucket(this, "bucket", {
+      bucketPrefix: name,
+    });
+
+    new S3BucketWebsiteConfiguration(this, "bucket-website", {
+      bucket: this.bucket.bucket,
+      indexDocument: {
+        suffix: "index.html",
+      },
+      errorDocument: {
+        key: "5xx.html",
+      },
+    });
+  }
+}
+
+export class ConstructsScopeStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new AwsProvider(this, "aws", {
+      region: "us-west-2",
+    });
+
+    // Both buckets are inside of the same stack, meaning they share
+    // the same scope. Therefore, their names must be unique.
+    new PublicS3Bucket(this, "first-bucket");
+    new PublicS3Bucket(this, "second-bucket");
+  }
+}
+// DOCS_BLOCK_END:constructs-scope
