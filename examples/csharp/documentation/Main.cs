dff1e33b39b65a29a3ccafa2f6b912bed09fb27f
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-01-31 10:52:49 +0100

    chore: add translations for remote state access example

--- examples/csharp/documentation/Main.cs
+++ examples/csharp/documentation/Main.cs
@@ -58,6 +58,8 @@ namespace MyCompany.MyApp
             new Examples.VariablesStack(app, "variables");
             new Examples.OutputVariableStack(app, "outputs", new Examples.OutputVariableStackConfig("example.com"));
             new Examples.OutputsUsageStack(app, "outputs-define");
+            new Examples.Producer(app, "cdktf-producer");
+            new Examples.Consumer(app, "cdktf-consumer");
 
             TerraformStack stack = new TerraformStack(app, "stack-escape-hatches");
             // DOCS_BLOCK_START:stack-escape-hatches
