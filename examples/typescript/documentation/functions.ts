5f645694f6516a26df81f5651690f9fef1875ed6
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-01-31 20:09:43 +0100

    chore: Add raw function snippet

--- examples/typescript/documentation/functions.ts
+++ examples/typescript/documentation/functions.ts
@@ -4,14 +4,16 @@
 import { TerraformStack } from "cdktf";
 import { Construct } from "constructs";
 import { AwsProvider } from "@cdktf/provider-aws/lib/aws-provider";
-import { DataAwsAvailabilityZones } from "@cdktf/provider-aws/lib/data-aws-availability-zones";
 // DOCS_BLOCK_END:functions
-// DOCS_BLOCK_START:operators,functions
+// DOCS_BLOCK_START:operators,functions,functions-raw
 import { Fn, TerraformOutput } from "cdktf";
-// DOCS_BLOCK_END:operators,functions
-// DOCS_BLOCK_START:operators
+// DOCS_BLOCK_END:operators,functions,functions-raw
+// DOCS_BLOCK_START:operators,functions-raw
 import { Op } from "cdktf";
-// DOCS_BLOCK_END:operators
+// DOCS_BLOCK_END:operators,functions-raw
+// DOCS_BLOCK_START:functions-raw,functions
+import { DataAwsAvailabilityZones } from "@cdktf/provider-aws/lib/data-aws-availability-zones";
+// DOCS_BLOCK_END:functions-raw,functions
 
 // DOCS_BLOCK_START:functions
 export class FunctionsStack extends TerraformStack {
@@ -22,12 +24,12 @@ export class FunctionsStack extends TerraformStack {
       region: "us-west-2",
     });
     // DOCS_BLOCK_END:functions
-    // DOCS_BLOCK_START:operators,functions
+    // DOCS_BLOCK_START:operators,functions,functions-raw
 
     const zones = new DataAwsAvailabilityZones(this, "zones", {
       state: "available",
     });
-    // DOCS_BLOCK_END:operators,functions
+    // DOCS_BLOCK_END:operators,functions,functions-raw
 
     // DOCS_BLOCK_START:functions
     new TerraformOutput(this, "first-zone", {
@@ -43,6 +45,15 @@ export class FunctionsStack extends TerraformStack {
       value: Op.div(Fn.lengthOf(zones.names), 2),
     });
     // DOCS_BLOCK_END:operators
+    // DOCS_BLOCK_START:functions-raw
+
+    // ...
+
+    new TerraformOutput(this, "half-of-the-zone-raw", {
+      value: `\${length(${zones.fqn}.names) / 2}`,
+    });
+    // DOCS_BLOCK_END:functions-raw
+
     // DOCS_BLOCK_START:functions
   }
 }
