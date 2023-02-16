d5deb139c089b8059ecfdc4c13607ce664925f2a
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-02-16 11:47:11 +0100

    chore: refactor tests

--- packages/@cdktf/hcl2cdk/test/coerceType.test.ts
+++ packages/@cdktf/hcl2cdk/test/coerceType.test.ts
@@ -1,3 +1,5 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
 import { coerceType } from "../lib/generation";
 import template from "@babel/template";
 import generate from "@babel/generator";
@@ -12,31 +14,38 @@ function generateCode(ast: any) {
 }
 
 describe("coerceType", () => {
-  it("should do nothing if the target type is undefined", () => {
-    const code = `"hello-world"`;
+  it.each([
+    { code: `"hello-world"`, type: "string", targetType: undefined },
+    { code: `"hello-world"`, type: "string", targetType: "string" },
+  ])("should change nothing for %p ", ({ code, type, targetType }) => {
     expect(
-      generateCode(coerceType(expressionify(code), "string", undefined))
+      generateCode(
+        coerceType(expressionify(code), type as any, targetType as any)
+      )
     ).toEqual(code);
   });
 
-  it("should do nothing if the target type is already correct", () => {
-    const code = `"hello-world"`;
-    expect(
-      generateCode(coerceType(expressionify(code), "string", "string"))
-    ).toEqual(code);
-  });
-
-  it("should add a Token.asNumber if a string is expected to be a number", () => {
-    const code = `"300"`;
-    expect(
-      generateCode(coerceType(expressionify(code), "string", "number"))
-    ).toEqual(`cdktf.Token.asNumber(${code})`);
-  });
-
-  it("should add a Token.asString if a number is expected to be a string", () => {
-    const code = `300`;
-    expect(
-      generateCode(coerceType(expressionify(code), "number", "string"))
-    ).toEqual(`cdktf.Token.asString(${code})`);
-  });
+  it.each([
+    {
+      code: `"300"`,
+      type: "string",
+      targetType: "number",
+      expectedCode: `cdktf.Token.asNumber("300")`,
+    },
+    {
+      code: `300`,
+      type: "number",
+      targetType: "string",
+      expectedCode: `cdktf.Token.asString(300)`,
+    },
+  ])(
+    "should use Token functions %p ",
+    ({ code, type, targetType, expectedCode }) => {
+      expect(
+        generateCode(
+          coerceType(expressionify(code), type as any, targetType as any)
+        )
+      ).toEqual(expectedCode);
+    }
+  );
 });
