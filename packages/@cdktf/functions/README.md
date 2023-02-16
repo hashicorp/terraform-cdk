dd6cab21abc0c3f7ad19606318dbb854bcac67ef
Ansgar Mertens <ansgar@hashicorp.com>
2023-02-16 11:56:04 +0100

    feat(functions): Introduce @cdktf/functions package

---
+++ packages/@cdktf/functions/README.md
@@ -0,0 +1,3 @@
+# @cdktf/functions
+
+This package contains bindings for Terraform functions that are re-exported by the `cdktf` package.
---
+++ packages/@cdktf/functions/ambient.d.ts
@@ -0,0 +1,7 @@
+// Copyright (c) HashiCorp, Inc
+// Licensed under the Mozilla Public License 2.0
+declare module 'child_process' {
+    interface SpawnOptions  {
+        signal?: AbortSignal;
+    }
+}
