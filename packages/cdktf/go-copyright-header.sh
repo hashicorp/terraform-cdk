d54b290e57d13f0d2461177cc9a4d71c580ca80f
Ansgar Mertens <ansgar@hashicorp.com>
2023-03-14 10:29:54 +0100

    fix(lib): Don't append copyright header to version file used for releasing Go

--- packages/cdktf/go-copyright-header.sh
+++ packages/cdktf/go-copyright-header.sh
@@ -6,8 +6,8 @@ LICENSE="SPDX-License-Identifier: MPL-2.0"
 
 # search for all files in the dist/go/cdktf directory and its subdirectories
 find "$DIRECTORY" -type f -print0 | while IFS= read -r -d '' FILE; do
-    # avoid appending to tarball file
-    if [[ "$FILE" != *.tgz ]]; then
+    # avoid appending to tarball file and to version
+    if [[ "$FILE" != *.tgz ]] && [[ "$FILE" != *version ]]; then
         # only add copyright to LICENSE, move to outer directory so its at root of Github Repo
         if [[ "$FILE" == *LICENSE* ]]; then
             awk -v s="$COPYRIGHT\n" 'BEGIN{print s}{print}' "$FILE" > temp && mv temp "$FILE" 2>/dev/null
