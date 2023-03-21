f533cb8167fb5065f625331277afe7416d6c5806
Ansgar Mertens <ansgar@hashicorp.com>
2023-03-21 09:55:48 +0100

    test(provider-generator): Add test case for provider with only block_types set

--- packages/@cdktf/provider-generator/lib/get/__tests__/generator/provider.test.ts
+++ packages/@cdktf/provider-generator/lib/get/__tests__/generator/provider.test.ts
@@ -24,3 +24,26 @@ test("generate provider", async () => {
   );
   expect(output).toMatchSnapshot();
 });
+
+test("generate provider with only block_types", async () => {
+  const code = new CodeMaker();
+  const workdir = fs.mkdtempSync(path.join(os.tmpdir(), "provider.test"));
+  const spec = JSON.parse(
+    fs.readFileSync(
+      path.join(
+        __dirname,
+        "fixtures",
+        "elasticstack-provider.test.fixture.json"
+      ),
+      "utf-8"
+    )
+  );
+  new TerraformProviderGenerator(code, spec).generateAll();
+  await code.save(workdir);
+
+  const output = fs.readFileSync(
+    path.join(workdir, "providers/elasticstack/provider/index.ts"),
+    "utf-8"
+  );
+  expect(output).toMatchSnapshot();
+});
