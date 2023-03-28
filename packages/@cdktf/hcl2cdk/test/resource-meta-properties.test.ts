cb6a75912573a4685bf9c0a965d5db36c0e4203c
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-03-28 12:12:55 +0200

    chore(hcl2cdk): test meta properties

---
+++ packages/@cdktf/hcl2cdk/test/resource-meta-properties.test.ts
@@ -0,0 +1,222 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
+import { testCase, Synth, binding } from "./helpers/convert";
+
+describe("meta-properties", () => {
+  describe("depends_on", () => {
+    testCase.test(
+      "depends on",
+      `
+      provider "aws" {
+        region = "us-east-1"
+      }
+      resource "aws_iam_role_policy" "example" {
+        name   = "example"
+        role   = "role"
+        policy = jsonencode({
+          "Statement" = [{
+            "Action" = "s3:*",
+            "Effect" = "Allow",
+          }],
+        })
+      }
+      
+      resource "aws_instance" "example" {
+        ami           = "ami-a1b2c3d4"
+        instance_type = "t2.micro"
+        iam_instance_profile = "aws_iam_instance_profile"
+        depends_on = [
+          aws_iam_role_policy.example
+        ]
+      }
+      `,
+      [binding.aws],
+      Synth.yes
+    );
+  });
+
+  describe("provider", () => {
+    testCase.test(
+      "passing providers",
+      `
+      provider "google" {
+        region = "us-central1"
+      }
+      
+      # alternate configuration, whose alias is "europe"
+      provider "google" {
+        alias  = "europe"
+        region = "europe-west1"
+      }
+
+      resource "google_compute_instance" "default" {
+        provider = google.europe
+        
+        name         = "test"
+        machine_type = "e2-medium"
+        zone         = "us-central1-a"
+      
+        tags = ["foo", "bar"]
+      
+        boot_disk {
+          initialize_params {
+            image = "debian-cloud/debian-11"
+            labels = {
+              my_label = "value"
+            }
+          }
+        }
+      
+        scratch_disk {
+          interface = "SCSI"
+        }
+      
+        network_interface {
+          network = "default"
+      
+          access_config {
+          }
+        }
+      
+        metadata = {
+          foo = "bar"
+        }
+      
+        metadata_startup_script = "echo hi > /test.txt"
+      }
+      `,
+      [binding.google],
+      Synth.yes
+    );
+  });
+
+  describe("lifecycle", () => {
+    testCase.test(
+      "all lifecycle properties",
+      `
+      resource "aws_iam_role_policy" "example" {
+        name   = "example"
+        role   = "role"
+        policy = jsonencode({
+          "Statement" = [{
+            "Action" = "s3:*",
+            "Effect" = "Allow",
+          }],
+        })
+      }
+
+      resource "aws_instance" "example" {
+        ami           = "ami-a1b2c3d4"
+        instance_type = "t2.micro"
+        iam_instance_profile = "aws_iam_instance_profile"
+        lifecycle {
+          create_before_destroy = true
+          prevent_destroy = true
+          ignore_changes = [
+            "ami",
+            "instance_type"
+          ]
+          replace_triggered_by = [
+            aws_iam_role_policy.example.id
+          ]
+        }
+      }
+      `,
+      [binding.aws],
+      Synth.yes
+    );
+
+    testCase.test(
+      "preconditions and postconditions",
+      `
+      resource "aws_instance" "example" {
+        ami           = "ami-a1b2c3d4"
+        instance_type = "t2.micro"
+        iam_instance_profile = "aws_iam_instance_profile"
+        lifecycle {
+          precondition {
+            condition = self.tags["Component"] == "nomad-server"
+            error_message = "Please add the nomad-server tag to this instance."
+          }
+
+          postcondition {
+            condition     = self.public_dns != ""
+            error_message = "EC2 instance must be in a VPC that has public DNS hostnames enabled."
+          }
+        }
+      }
+      `,
+      [binding.aws],
+      Synth.yes
+    );
+  });
+
+  describe("provisioner", () => {
+    testCase.test(
+      "file",
+      `
+      resource "aws_instance" "example" {
+        ami           = "ami-a1b2c3d4"
+        instance_type = "t2.micro"
+        iam_instance_profile = "aws_iam_instance_profile"
+        
+        provisioner "file" {
+          source      = "conf/myapp.conf"
+          destination = "/etc/myapp.conf"
+        }
+
+        provisioner "file" {
+          content     = "ami used: \${self.ami}"
+          destination = "/tmp/file.log"
+        }
+      }
+      `,
+      [binding.aws],
+      Synth.yes
+    );
+
+    testCase.test(
+      "local-exec",
+      `
+      resource "aws_instance" "example" {
+        ami           = "ami-a1b2c3d4"
+        instance_type = "t2.micro"
+        iam_instance_profile = "aws_iam_instance_profile"
+        
+        provisioner "local-exec" {
+          command = "echo \${self.private_ip} >> private_ips.txt"
+        }
+      }
+      `,
+      [binding.aws],
+      Synth.yes
+    );
+
+    testCase.test(
+      "remote-exec",
+      `
+      resource "aws_instance" "example" {
+        ami           = "ami-a1b2c3d4"
+        instance_type = "t2.micro"
+        iam_instance_profile = "aws_iam_instance_profile"
+        
+        connection {
+          type     = "ssh"
+          user     = "root"
+          password = "password"
+          host     = self.public_ip
+        }
+      
+        provisioner "remote-exec" {
+          inline = [
+            "puppet apply",
+            "consul join \${aws_instance.web.private_ip}",
+          ]
+        }
+      }
+      `,
+      [binding.aws],
+      Synth.yes
+    );
+  });
+});
