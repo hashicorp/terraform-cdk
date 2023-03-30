1dbd932170c863e30654f915c60419cf0af162cd
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-03-30 15:17:34 +0200

    chore: fix testcase

--- packages/@cdktf/hcl2cdk/test/resource-meta-properties.test.ts
+++ packages/@cdktf/hcl2cdk/test/resource-meta-properties.test.ts
@@ -210,6 +210,12 @@ describe("meta-properties", () => {
       provider "aws" {
         region = "us-east-1"
       }
+
+      resource "aws_instance" "web" {
+        ami           = "ami-a1b2c3d4"
+        instance_type = "t2.micro"
+      }
+
       resource "aws_instance" "example" {
         ami           = "ami-a1b2c3d4"
         instance_type = "t2.micro"
