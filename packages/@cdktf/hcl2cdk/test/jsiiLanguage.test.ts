a1af4493b22e07af851bf3d1e0563341e9c94fd8
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-03-31 13:46:08 +0200

    chore: update snapshots

--- packages/@cdktf/hcl2cdk/test/jsiiLanguage.test.ts
+++ packages/@cdktf/hcl2cdk/test/jsiiLanguage.test.ts
@@ -24,7 +24,7 @@ describe("JSII Language Support", () => {
     
     resource "aws_s3_bucket_object" "examplebucket_object" {
       key        = "someobject"
-      bucket     = aws_s3_bucket.examplebucket.name
+      bucket     = aws_s3_bucket.examplebucket.bucket
       source     = "index.html"
       kms_key_id = aws_kms_key.examplekms.arn
     }
