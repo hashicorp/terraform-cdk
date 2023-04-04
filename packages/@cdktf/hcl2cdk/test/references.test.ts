4752df9c28ec73dff3139b767b459d5a5dfb658d
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-04-04 10:40:11 +0200

    chore: try to fix test for synth

--- packages/@cdktf/hcl2cdk/test/references.test.ts
+++ packages/@cdktf/hcl2cdk/test/references.test.ts
@@ -140,7 +140,8 @@ describe("references", () => {
       }
       
       resource "aws_s3_bucket" "examplebucket" {
-        bucket = "examplebuckettftest"
+        count  = 2
+        bucket = "examplebuckettftest-\${count.index}"
         acl    = "private"
       }
       
