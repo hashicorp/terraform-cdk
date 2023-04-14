1f11272f78cc57053ac7e2d35f81a3ab780bfa9d
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-04-14 16:05:12 +0200

    fix(hcl2cdk): handle object expressions

--- packages/@cdktf/hcl2cdk/test/resources.test.ts
+++ packages/@cdktf/hcl2cdk/test/resources.test.ts
@@ -464,4 +464,55 @@ resource "kubernetes_secret" "secrets-xxx" {
       dataSources: ["aws_availability_zones"],
     }
   );
+
+  testCase.test(
+    "handles JSON encoded blocks",
+    `
+    provider "aws" {
+      region                      = "us-east-1"
+    }
+    resource "aws_ecs_task_definition" "client" {
+      family                   = "client"
+      requires_compatibilities = ["FARGATE"]
+      # required for Fargate launch type
+      memory       = 512
+      cpu          = 256
+      network_mode = "awsvpc"
+    
+      container_definitions = jsonencode([
+        {
+          name      = "client"
+          image     = "crccheck/hello-world"
+          cpu       = 0 # take up proportional cpu
+          essential = true
+    
+          portMappings = [
+            {
+              containerPort = 8000
+              hostPort      = 8000 # though, access to the ephemeral port range is needed to connect on EC2, the exact port is required on Fargate from a security group standpoint.
+              protocol      = "tcp"
+            }
+          ]
+    
+          # Fake Service settings are set via Environment variables
+          environment = [
+            {
+              name  = "NAME"
+              value = "client"
+            },
+            {
+              name  = "MESSAGE"
+              value = "Hello from the client!"
+            }
+          ]
+        }
+      ])
+    }
+    `,
+    [binding.aws],
+    Synth.yes,
+    {
+      resources: ["aws_ecs_task_definition"],
+    }
+  );
 });
