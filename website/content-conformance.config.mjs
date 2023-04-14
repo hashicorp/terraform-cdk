e28ae0cb467722fc0f5f2d9225e3ab0407affdf3
Bryce Kalow <bkalow@hashicorp.com>
2023-04-14 14:22:58 -0500

    adds content-check command and README update

---
+++ website/content-conformance.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  preset: 'base-docs',
+  rules: {
+    // We are disabling this rule until links to terraform.io in the generated api-reference documentation are updated
+    'ensure-valid-link-format': 'off'
+  }
+}
