a17c71b296d812c12b7c0322ee81b65d5c518171
Bryce Kalow <bkalow@hashicorp.com>
2023-04-17 15:47:01 -0500

    format

--- website/content-conformance.config.mjs
+++ website/content-conformance.config.mjs
@@ -4,9 +4,9 @@
  */
 
 export default {
-  preset: 'base-docs',
+  preset: "base-docs",
   rules: {
     // We are disabling this rule until links to terraform.io in the generated api-reference documentation are updated
-    'ensure-valid-link-format': 'off'
-  }
-}
+    "ensure-valid-link-format": "off",
+  },
+};
