4ad298c81dad0f7df96472ddae6713bb1accb96c
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-05-02 10:17:07 +0200

    refactor(cli): upgrade inquirer to @inquirer/prompts
    They take up way less space and let us get rid of our rxjs dependency

--- packages/cdktf-cli/src/bin/cmds/helper/error-reporting.ts
+++ packages/cdktf-cli/src/bin/cmds/helper/error-reporting.ts
@@ -1,13 +1,9 @@
-import * as inquirer from "inquirer";
+import { confirm } from "@inquirer/prompts";
 
 export async function askForCrashReportingConsent() {
-  const answer: { reportCrash: boolean } = await inquirer.prompt({
-    name: "reportCrash",
+  return await confirm({
     message:
       "Do you want to send crash reports to the CDKTF team? Refer to https://developer.hashicorp.com/terraform/cdktf/create-and-deploy/configuration-file#enable-crash-reporting-for-the-cli for more information",
-    type: "confirm",
     default: true,
   });
-
-  return answer.reportCrash;
 }
