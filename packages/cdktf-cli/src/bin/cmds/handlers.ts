3555a2254f3dd3b334e5de7c3af6e02a6ec0ead2
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-05-10 11:56:38 +0200

    feat(cli-core): add runGetInDir function
    A convenient helper that runs get in a certain directory
    
    Also had to refactor out some duplicate type definitions

--- packages/cdktf-cli/src/bin/cmds/handlers.ts
+++ packages/cdktf-cli/src/bin/cmds/handlers.ts
@@ -50,7 +50,6 @@ import {
   DependencyManager,
   ProviderConstraint,
   CdktfConfig,
-  ProviderDependencySpec,
   get as getLib,
   providerAdd as providerAddLib,
 } from "@cdktf/cli-core";
@@ -66,7 +65,7 @@ const chalkColour = new chalk.Instance();
 const config = readConfigSync();
 
 async function getProviderRequirements(provider: string[]) {
-  let providersFromConfig: (string | ProviderDependencySpec)[] = [];
+  let providersFromConfig: (string | TerraformDependencyConstraint)[] = [];
 
   try {
     const config = CdktfConfig.read();
