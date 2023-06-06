cf11bc2422cbcfa7f25c71e76b1a2ef12367179f
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-06-06 14:36:31 +0200

    chore: export bash variable

--- test/provider-tests/test-provider.sh
+++ test/provider-tests/test-provider.sh
@@ -8,6 +8,6 @@ set -ex
 # always run from this dir
 cd $(dirname $0)
 
-NODE_OPTIONS="--max-old-space-size=6656"
+export NODE_OPTIONS="--max-old-space-size=6656"
 node ./generate-provider-test.js "$1"
 ../run-against-dist npx jest "./providers/$1"
