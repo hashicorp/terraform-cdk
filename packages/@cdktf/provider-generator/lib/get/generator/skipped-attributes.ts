f373af96d21ba92c8d699b87230b6a690d466100
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-06-13 17:37:25 +0200

    chore: remove more resources

--- packages/@cdktf/provider-generator/lib/get/generator/skipped-attributes.ts
+++ packages/@cdktf/provider-generator/lib/get/generator/skipped-attributes.ts
@@ -7,7 +7,11 @@
  * To mitigate this issue we replace their entire tree of attributes with
  * a single any attribute. This can still be used, although less conveniently.
  */
-const SKIPPED_ATTRIBUTES: string[] = ["aws.quicksight_template.definition"];
+const SKIPPED_ATTRIBUTES: string[] = [
+  "aws.quicksight_template.definition",
+  "aws.quicksight_dashboard.definition",
+  "aws.quicksight_analysis.definition",
+];
 
 /**
  * We skip some deeply nested attributes to shorten the generated code.
