68a76815954631a51358baa6c7c9cd0c6d81aa0e
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-07-06 10:39:13 +0200

    refactor: move comment sanitization into helper

---
+++ packages/@cdktf/provider-generator/lib/get/generator/sanitized-comments.ts
@@ -0,0 +1,24 @@
+import { CodeMaker } from "codemaker";
+
+export function sanitizedComment(code: CodeMaker) {
+  let wasSanitized = false;
+  code.line(`/**`);
+  return {
+    line: (comment: string) => {
+      const sanitized = comment.replace(/\*\//g, "* /");
+      if (sanitized !== comment) {
+        wasSanitized = true;
+      }
+      code.line(` * ${sanitized}`);
+    },
+    end: () => {
+      if (wasSanitized) {
+        code.line(` *`);
+        code.line(
+          " * Note: The above comment contained a comment block ending sequence (* followed by /). We have introduced a space between to prevent syntax errors. Please ignore the space."
+        );
+      }
+      code.line(` */`);
+    },
+  };
+}
