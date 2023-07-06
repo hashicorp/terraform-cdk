9d417582ba746aa0bf88fdf832050f2e47348dfa
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-07-06 16:26:42 +0200

    chore: update snapshots

--- packages/@cdktf/provider-generator/lib/get/generator/sanitized-comments.ts
+++ packages/@cdktf/provider-generator/lib/get/generator/sanitized-comments.ts
@@ -9,21 +9,28 @@ export function sanitizedComment(code: CodeMaker) {
   let wasSanitized = false;
   code.line(`/**`);
   return {
-    line: (comment: string) => {
-      const sanitized = comment.replace(/\*\//g, "* /");
+    line: (comment: string | number) => {
+      const sanitized =
+        typeof comment === "string" ? comment.replace(/\*\//g, "* /") : comment;
+
+      if (sanitized === "") {
+        code.line(`*`);
+        return;
+      }
+
       if (sanitized !== comment) {
         wasSanitized = true;
       }
-      code.line(` * ${sanitized}`);
+      code.line(`* ${sanitized}`);
     },
     end: () => {
       if (wasSanitized) {
         code.line(` *`);
         code.line(
-          " * Note: The above comment contained a comment block ending sequence (* followed by /). We have introduced a space between to prevent syntax errors. Please ignore the space."
+          "* Note: The above comment contained a comment block ending sequence (* followed by /). We have introduced a space between to prevent syntax errors. Please ignore the space."
         );
       }
-      code.line(` */`);
+      code.line(`*/`);
     },
   };
 }
