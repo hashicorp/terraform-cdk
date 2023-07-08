7519e1cdc4aabf5953724813c198c40cad5a196a
djKooks <inylove82@gmail.com>
2023-07-08 22:11:42 +0900

    Add variable inside test case

--- packages/@cdktf/provider-generator/lib/get/__tests__/generator/fixtures/module-get-x.test.fixture.tf
+++ packages/@cdktf/provider-generator/lib/get/__tests__/generator/fixtures/module-get-x.test.fixture.tf
@@ -6,3 +6,9 @@ variable "get_caller_identity" {
   type        = bool
   default     = true
 }
+
+variable "get_subnet_ids" {
+  description = "RDS DB subnet group resource"
+  type        = list(tuple([string, string, number]))
+  default     = [["172.0.0.1", "172.0.0.2", 1],["172.0.10.1", "172.0.10.2", 2]]
+}
