0434ecb11a9ff8f2c025c12d234846849a2e20b3
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-08-15 17:31:23 +0200

    chore: add logging

--- packages/@cdktf/commons/src/gradle.ts
+++ packages/@cdktf/commons/src/gradle.ts
@@ -5,6 +5,7 @@
 
 import path from "path";
 import fs from "fs-extra";
+import { logger } from "./logging";
 
 export function isGradleProject(workingDirectory: string): boolean {
   const buildGradlePath = path.join(workingDirectory, "build.gradle");
@@ -13,6 +14,7 @@ export function isGradleProject(workingDirectory: string): boolean {
     fs.accessSync(buildGradlePath, fs.constants.R_OK | fs.constants.W_OK);
     return true;
   } catch {
+    logger.debug(`No build.gradle found at ${buildGradlePath}`);
     return false;
   }
 }
