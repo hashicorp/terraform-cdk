17f369c46a27c52703f07089897ecbd3598bbd01
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-08-16 11:16:47 +0200

    fix: use gradle dependencies for provider info

--- packages/@cdktf/commons/src/gradle.ts
+++ packages/@cdktf/commons/src/gradle.ts
@@ -6,6 +6,7 @@
 import path from "path";
 import fs from "fs-extra";
 import { logger } from "./logging";
+import { exec } from "./util";
 
 export function isGradleProject(workingDirectory: string): boolean {
   const buildGradlePath = path.join(workingDirectory, "build.gradle");
@@ -17,4 +18,128 @@ export function isGradleProject(workingDirectory: string): boolean {
     logger.debug(`No build.gradle found at ${buildGradlePath}`);
     return false;
   }
+}
+
+export async function getGradleDependencies() {
+  let output;
+  try {
+    output = await exec("gradle", ["dependencies", "--console=plain"], {
+      env: process.env,
+    });
+  } catch (e) {
+    logger.debug(`Unable to run 'gradle dependencies': ${e}`);
+    return undefined;
+  }
+
+  const lines = output.split(/\r\n|\r|\n/);
+
+  // find the implementation section
+  const implementationSection = lines.findIndex((line) =>
+    line.includes("implementation - ")
+  );
+  if (implementationSection === -1) {
+    logger.debug(
+      `Unable to find implementation section in output of 'gradle dependencies': ${output}`
+    );
+    return undefined;
+  }
+
+  const emptyLineRegex = /^\s*$/;
+  const implementationSectionLines = lines.slice(implementationSection + 1);
+  const sectionEnd = implementationSectionLines.findIndex((line) =>
+    emptyLineRegex.test(line)
+  );
+  const implementationLines = implementationSectionLines.slice(0, sectionEnd);
+
+  // find the api section
+  const apiSection = lines.findIndex((line) => line.includes("api - "));
+  if (apiSection === -1) {
+    logger.debug(
+      `Unable to find api section in output of 'gradle dependencies': ${output}`
+    );
+    return undefined;
+  }
+
+  const apiSectionLines = lines.slice(apiSection + 1);
+  const apiSectionEnd = apiSectionLines.findIndex((line) =>
+    emptyLineRegex.test(line)
+  );
+
+  const apiLines = apiSectionLines.slice(0, apiSectionEnd);
+
+  const prefixRegex = /^\s*[+\\-]+\s*/;
+  const suffixRegex = /\s+\([nc\*]\)\s*$/;
+
+  return [...implementationLines, ...apiLines]
+    .filter((line) => line !== "No dependencies")
+    .map((line) => line.replace(prefixRegex, "").replace(suffixRegex, ""));
+}
+
+export type DependencyInformation = {
+  group: string;
+  name: string;
+  version: string;
+};
+export function getDependencyInformationFromLine(
+  line: string
+): DependencyInformation | undefined {
+  const packageNameRegex = /^\s*([^:]+):([^:]+)(?::([^\s]+))?/;
+  const matches = line.match(packageNameRegex);
+  if (!matches) {
+    logger.debug(
+      "Unexpected format for gradle build. Please file an issue at https://cdk.tf/bug"
+    );
+    return undefined;
+  }
+
+  if (matches[3] === undefined) {
+    return {
+      group: "",
+      name: matches[1],
+      version: matches[2],
+    };
+  }
+
+  return {
+    group: matches[1],
+    name: matches[2],
+    version: matches[3],
+  };
+}
+
+/*
+ * Example output:
+  implementation - Implementation dependencies for the 'main' feature. (n)
+  +--- com.hashicorp:cdktf:0.18.0 (n)
+  +--- software.constructs:constructs:10.0.25 (n)
+  +--- junit:junit:4.13.2 (n)
+  \--- org.junit.jupiter:junit-jupiter:5.8.0 (n)
+*/
+export async function getGradlePackageVersion(packageName: string) {
+  const translationMap: Record<string, string> = {
+    jsii: "jsii-runtime",
+  };
+  const gradlePackageName = translationMap[packageName] || packageName;
+  logger.debug(
+    "Running 'gradle dependencies' to find package version",
+    gradlePackageName
+  );
+
+  const lines = await getGradleDependencies();
+  if (!lines || lines.length === 0) {
+    return undefined;
+  }
+
+  // loop through the subsequent lines to find the one starting with package name
+  for (const line of lines) {
+    if (line.includes(`:${gradlePackageName}:`)) {
+      const dep = getDependencyInformationFromLine(line);
+      if (dep) {
+        return dep.version;
+      }
+      return undefined;
+    }
+  }
+
+  return undefined;
 }
