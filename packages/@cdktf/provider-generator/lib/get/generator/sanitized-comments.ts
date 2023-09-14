08e25d4f549761367f33ea9371c5128c0b41578c
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-09-14 09:06:46 +0100

    fix(provider-generator): make sanitize comments function robust against diverse inputs
    Closes #3128

--- packages/@cdktf/provider-generator/lib/get/generator/sanitized-comments.ts
+++ packages/@cdktf/provider-generator/lib/get/generator/sanitized-comments.ts
@@ -9,19 +9,32 @@ export function sanitizedComment(code: CodeMaker) {
   let wasSanitized = false;
   code.line(`/**`);
   return {
-    line: (comment: string | number) => {
+    line: (comment: string | number | string[] | object) => {
+      // objects can not be valid comments
+      if (typeof comment === "object" && !Array.isArray(comment)) {
+        return;
+      }
+
+      const stringified = Array.isArray(comment)
+        ? comment.join("\n")
+        : comment.toLocaleString();
+
       const sanitized =
-        typeof comment === "string" ? comment.replace(/\*\//g, "* /") : comment;
+        typeof stringified === "string"
+          ? stringified.replace(/\*\//g, "* /")
+          : stringified;
 
       if (sanitized === "") {
         code.line(`*`);
         return;
       }
 
-      if (sanitized !== comment) {
+      if (sanitized !== stringified) {
         wasSanitized = true;
       }
-      code.line(`* ${sanitized}`);
+      sanitized.split("\n").forEach((line) => {
+        code.line(`* ${line}`);
+      });
     },
     end: () => {
       if (wasSanitized) {
