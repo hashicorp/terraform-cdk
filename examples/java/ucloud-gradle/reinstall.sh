c6c4028401af56afeda04295db6965b47c46e3cc
Mark DeCrane <mark.decrane@hashicorp.com>
2023-09-21 15:55:56 -0400

    fix: java gradle reinstall script

--- examples/java/ucloud-gradle/reinstall.sh
+++ examples/java/ucloud-gradle/reinstall.sh
@@ -5,4 +5,4 @@
 
 set -ex
 
-sed -i "s/implementation 'com.hashicorp:cdktf:0.0.0'/implementation files(\"..\/..\/..\/dist\/java\/com\/hashicorp\/cdktf\/0.0.0\/cdktf-0.0.0.jar\")/" build.gradle
+sed -i 's/"com\.hashicorp:cdktf:0\.0\.0"/files("..\/..\/..\/dist\/java\/com\/hashicorp\/cdktf\/0.0.0\/cdktf-0.0.0.jar")/g' build.gradle
