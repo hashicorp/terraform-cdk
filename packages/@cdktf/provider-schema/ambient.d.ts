60abd5a42eedc1b2b2ed6457b66a1bed04b9ce20
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-09-22 10:00:20 +0200

    chore: fix typo
    Co-authored-by: Ansgar Mertens <ansgar@hashicorp.com>

--- packages/@cdktf/provider-schema/ambient.d.ts
+++ packages/@cdktf/provider-schema/ambient.d.ts
@@ -1,6 +1,5 @@
 // Copyright (c) HashiCorp, Inc
 // Licensed under the Mozilla Public License 2.0
-declare module '@npmcli/ci-detect';
 
 declare module 'child_process' {
     interface SpawnOptions  {
