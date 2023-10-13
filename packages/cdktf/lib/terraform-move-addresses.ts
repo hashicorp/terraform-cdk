44457b441d2f55987b80d68ed455cb1e5ae6d120
Mark DeCrane <mark.decrane@hashicorp.com>
2023-10-13 09:23:43 -0400

    chore: addition of examples, and better naming

--- packages/cdktf/lib/terraform-move-addresses.ts
+++ packages/cdktf/lib/terraform-move-addresses.ts
@@ -3,14 +3,15 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { Construct } from "constructs";
+import { IConstruct } from "constructs";
 import { TerraformResource } from "./terraform-resource";
 import { TerraformStack } from "./terraform-stack";
+import { App } from "./app";
 
 /**
  *
  */
-export class TerraformMoveAddresses {
+export class TerraformResourceAddressMap {
   private _terraformAddressMap: Map<string, TerraformResource> = new Map<
     string,
     TerraformResource
@@ -23,18 +24,45 @@ export class TerraformMoveAddresses {
    * function to return the specificed resource address at a certain tag
    */
 
-  // Would trying to access this at a higher abstract lead to things not being present? since we only add stuff later on
-
   public add(resource: TerraformResource, tag: string): void {
     this._terraformAddressMap.set(tag, resource);
   }
 
-  public getPath(tag: string): TerraformResource | undefined {
+  public getResourceAddress(tag: string): TerraformResource | undefined {
     return this._terraformAddressMap.get(tag);
   }
 
-  public static addresses(scope: Construct) {
-    return (scope.node.root.node.children[0] as unknown as TerraformStack)
-      .moveAddresses; // TODO: see if there is a better way of doing this, or if theres any problems with it;
+  public static parentStackAddressMap(scope: IConstruct) {
+    // SO, in a nested construct, the stack is accessable at scope.node.scope ACTUALLY NOT TRUE
+    // -- only thought that was the case as scope.node.scope just happened to be the stack
+    // But, if you are already at the root level it is undefined
+    // what if we keep trying to access the scope until it is undefined
+
+    // if scope.scope === undefined we are at the stack level
+
+    let currAbstractionLevel: IConstruct | undefined = scope;
+    let oneHigher: IConstruct | undefined = currAbstractionLevel.node.scope;
+    while (!App.isApp(oneHigher)) {
+      currAbstractionLevel = currAbstractionLevel?.node.scope;
+      oneHigher = currAbstractionLevel?.node.scope;
+    }
+    console.log("result", currAbstractionLevel);
+    const stackId = currAbstractionLevel?.node.id;
+    const children = scope.node.root.node.children;
+    let i = 0;
+    for (const child of children) {
+      if (child.node.id === stackId) {
+        break;
+      } else {
+        i = i + 1;
+      }
+    }
+
+    console.log("result", scope.node.root.node.children[i]);
+    // AHHHH I give up, guess you can only do moves one stack at a time
+    return (scope.node.root.node.children[0] as TerraformStack)
+      .resourceAddresses; // TODO: see if there is a better way of doing this, or if theres any problems with it;
+
+    // when scope.node.scope == undefined, we are at the app
   }
 }
