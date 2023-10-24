537bfdff1eebca6d179a221de2edec92a631254b
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-10-24 14:57:55 +0200

    bring scalability into the game

--- examples/typescript/building-blocks/cdktf/connection.ts
+++ examples/typescript/building-blocks/cdktf/connection.ts
@@ -1,4 +1,7 @@
+import { Construct } from "constructs";
 import { Token } from "cdktf";
+import * as l1 from "../l1";
+
 // Cloud-Agnostic Building Blocks
 /**
  * Service connecting to the "hosting" service, e.g. Lambda connecting to DB
@@ -24,6 +27,9 @@ export interface DatbaseConfig {
 }
 
 export class DatabaseUser extends Plug {
+  constructor(public subject: { arn: string }) {
+    super();
+  }
   connect(db: DatabaseProvider): DatbaseConfig {
     db.connect(this);
 
@@ -34,6 +40,10 @@ export class DatabaseUser extends Plug {
       password: db.password,
     };
   }
+
+  get arn(): string {
+    return this.subject.arn;
+  }
 }
 export abstract class DatabaseProvider extends Socket {
   abstract get host(): string;
@@ -41,6 +51,10 @@ export abstract class DatabaseProvider extends Socket {
   abstract get user(): string;
   abstract get password(): string;
   abstract connect(subjectPlug: DatabaseUser): DatbaseConfig;
+
+  get dbArn(): string {
+    return "arn:aws:rds:us-east-1:123456789012:db:mysql-db";
+  }
 }
 /**
  * Generic database connector connecting one or many services to a database
@@ -68,13 +82,66 @@ export class EnvironmentDatabasePlug extends Plug {
 }
 
 export abstract class AwsDatabaseProvider extends DatabaseProvider {
-  connect(_subjectPlug: DatabaseUser): DatbaseConfig {
-    // TODO: Create required resources e.g. for request pooling etc.
+  constructor(public scope: Construct) {
+    super();
+  }
+  connect(subjectPlug: DatabaseUser): DatbaseConfig {
+    const role = new l1.ServiceRole(this.scope, "db-role", {});
+    new l1.AccessPolicy(this.scope, "db-access-policy", {
+      role: role,
+      from: subjectPlug.arn,
+      to: this.dbArn,
+    });
+
     return {
       host: this.host,
       port: this.port,
       user: this.user,
       password: this.password,
     };
+  }
+}
+
+export class ForwardingDatabaseProvider extends AwsDatabaseProvider {
+  constructor(public subject: l1.AwsDbResource) {
+    super(subject.node.scope!);
+  }
+
+  connect(subjectPlug: DatabaseUser): DatbaseConfig {
+    return super.connect(subjectPlug);
+  }
+
+  get plug(): DatabaseUser {
+    return new DatabaseUser(this.subject);
+  }
+
+  get host(): string {
+    return this.subject.host;
+  }
+  get port(): number {
+    return this.subject.port;
+  }
+  get user(): string {
+    return this.subject.user;
+  }
+  get password(): string {
+    return this.subject.password;
+  }
+}
+
+export abstract class ScaleableAwsDatabaseProvider extends AwsDatabaseProvider {
+  constructor(scope: Construct) {
+    super(scope);
+  }
+  connect(subjectPlug: DatabaseUser): DatbaseConfig {
+    // Scale the database by adding pg-boucer
+    const pgBouncer = new l1.PgBouncer(this.scope, "pg-bouncer", {});
+    const bouncerDbProvider = new ForwardingDatabaseProvider(pgBouncer);
+    const externalDbConfig = subjectPlug.connect(bouncerDbProvider);
+
+    const databaseConnectionConfig = super.connect(bouncerDbProvider.plug);
+    pgBouncer.config.dbConnection = databaseConnectionConfig;
+
+    return externalDbConfig;
   }
 }
--- examples/typescript/building-blocks/l1.ts
+++ examples/typescript/building-blocks/l1.ts
@@ -1,8 +1,29 @@
 import { Construct } from "constructs";
 
+export class AwsResource extends Construct {
+  get arn() {
+    return "arn:aws:resource";
+  }
+}
+
+export class AwsDbResource extends AwsResource {
+  get host(): string {
+    return "foo";
+  }
+  get port(): number {
+    return 123;
+  }
+  get user(): string {
+    return "foo";
+  }
+  get password(): string {
+    return "foo";
+  }
+}
+
 // L1 Constructs
 export interface RDSL1Config {}
-export class RdsL1 extends Construct {
+export class RdsL1 extends AwsResource {
   constructor(scope: Construct, ns: string, _config: RDSL1Config) {
     super(scope, ns);
   }
@@ -12,7 +33,7 @@ export interface LambdaL1Config {
   name: string;
   env: Record<string, string>;
 }
-export class LambdaL1 extends Construct {
+export class LambdaL1 extends AwsResource {
   constructor(scope: Construct, ns: string, public config: LambdaL1Config) {
     super(scope, ns);
   }
@@ -22,25 +43,43 @@ export interface ApiGatewayL1Config {
   paths: Record<string, LambdaL1>;
 }
 
-export class ApiGatewayL1 extends Construct {
+export class ApiGatewayL1 extends AwsResource {
   constructor(scope: Construct, ns: string, _config: ApiGatewayL1Config) {
     super(scope, ns);
   }
 }
-export class KubernetesClusterL1 extends Construct {
+export class KubernetesClusterL1 extends AwsResource {
   constructor(scope: Construct, ns: string, _config: any) {
     super(scope, ns);
   }
 }
 
-export class KubernetesDeploymentL1 extends Construct {
+export class KubernetesDeploymentL1 extends AwsResource {
   constructor(scope: Construct, ns: string, _config: any) {
     super(scope, ns);
   }
 }
 
-export class TfcProjectSecret extends Construct {
+export class TfcProjectSecret extends AwsResource {
   constructor(scope: Construct, ns: string, _config: string) {
+    super(scope, ns);
+  }
+}
+
+export class ServiceRole extends AwsResource {
+  constructor(scope: Construct, ns: string, _config: any) {
+    super(scope, ns);
+  }
+}
+
+export class AccessPolicy extends AwsResource {
+  constructor(scope: Construct, ns: string, _config: any) {
+    super(scope, ns);
+  }
+}
+
+export class PgBouncer extends AwsDbResource {
+  constructor(scope: Construct, ns: string, public config: any) {
     super(scope, ns);
   }
 }
