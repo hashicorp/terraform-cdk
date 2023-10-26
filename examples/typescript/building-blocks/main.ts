b61ee9dae8fc111ea87d0d9154ddd48f24b3af67
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-10-26 10:59:34 +0200

    move to real world

--- examples/typescript/building-blocks/main.ts
+++ examples/typescript/building-blocks/main.ts
@@ -2,11 +2,19 @@
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
-// import * as path from "path";
+import { AwsProvider } from "./.gen/providers/aws/provider";
+import { Lambda } from "./platform-team";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, ns: string) {
     super(scope, ns);
+    new AwsProvider(this, "aws", { region: "eu-central-1" });
+
+    new Lambda(this, "list-tweets", {
+      functionName: "list-tweets",
+      directory: "./lambdas/list-tweets",
+      language: "nodejs",
+    });
   }
 }
 
