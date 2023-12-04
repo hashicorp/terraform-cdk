8cd34ccb47bbf5f8bbb33761619aff7083ad074d
Mark DeCrane <mark.decrane@hashicorp.com>
2023-12-04 12:17:52 -0500

    fix: given provider name can differ from source

--- packages/@cdktf/provider-generator/lib/get/generator/resource-parser.ts
+++ packages/@cdktf/provider-generator/lib/get/generator/resource-parser.ts
@@ -11,7 +11,7 @@ import {
   isNestedTypeAttribute,
   Schema,
 } from "@cdktf/commons";
-import { ProviderName, FQPN, parseFQPN } from "@cdktf/provider-schema";
+import { ProviderName, FQPN } from "@cdktf/provider-schema";
 import {
   ResourceModel,
   AttributeTypeModel,
@@ -123,17 +123,17 @@ class Parser {
     fqpn: FQPN,
     type: string,
     schema: Schema,
-    terraformSchemaType: string
+    terraformSchemaType: string,
+    providerName: ProviderName
   ): ResourceModel {
-    const provider = parseFQPN(fqpn).name;
     let baseName = type;
-    if (baseName.startsWith(`${provider}_`)) {
-      baseName = baseName.substr(provider.length + 1);
+    if (baseName.startsWith(`${providerName}_`)) {
+      baseName = baseName.substr(providerName.length + 1);
     }
 
     const isProvider = terraformSchemaType === "provider";
     if (isProvider) {
-      baseName = `${provider}_${baseName}`;
+      baseName = `${providerName}_${baseName}`;
       if (!("attributes" in schema.block)) {
         schema.block = {
           attributes: {},
@@ -154,16 +154,16 @@ class Parser {
     const className = this.uniqueClassName(toPascalCase(baseName));
     // avoid naming collision - see https://github.com/hashicorp/terraform-cdk/issues/299
     const configStructName = this.uniqueClassName(`${className}Config`);
-    const fileName = getFileName(provider, baseName);
+    const fileName = getFileName(providerName, baseName);
 
-    const filePath = `providers/${toSnakeCase(provider)}/${fileName}`;
+    const filePath = `providers/${toSnakeCase(providerName)}/${fileName}`;
     let attributes = this.renderAttributesForBlock(
       new Scope({
         name: baseName,
         isProvider,
         parent: isProvider
           ? undefined
-          : new Scope({ name: provider, isProvider: true }),
+          : new Scope({ name: providerName, isProvider: true }),
       }),
       schema.block
     );
@@ -664,14 +664,21 @@ export class ResourceParser {
     fqpn: FQPN,
     type: string,
     schema: Schema,
-    terraformType: string
+    terraformType: string,
+    providerName: ProviderName
   ): ResourceModel {
     if (this.resources[type]) {
       return this.resources[type];
     }
 
     const parser = new Parser(this.uniqueClassnames);
-    const resource = parser.resourceFrom(fqpn, type, schema, terraformType);
+    const resource = parser.resourceFrom(
+      fqpn,
+      type,
+      schema,
+      terraformType,
+      providerName
+    );
     this.resources[type] = resource;
     return resource;
   }
