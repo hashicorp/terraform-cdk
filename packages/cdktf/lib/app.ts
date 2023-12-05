bade86f92bcd8c728bf8fc75ba7d5e3087f1d1fa
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-12-05 21:49:07 +0100

    fix: Dont use CDKTF_ prefix on env variables that are not for all commands

--- packages/cdktf/lib/app.ts
+++ packages/cdktf/lib/app.ts
@@ -84,7 +84,7 @@ export class App extends Construct {
     Object.defineProperty(this, APP_SYMBOL, { value: true });
 
     this.outdir = process.env.CDKTF_OUTDIR ?? config.outdir ?? "cdktf.out";
-    const envHclOutput = process.env.CDKTF_HCL_OUTPUT;
+    const envHclOutput = process.env.HCL_OUTPUT;
     let hclOutput = config.hclOutput || false;
     if (envHclOutput !== undefined) {
       hclOutput = envHclOutput === "true";
