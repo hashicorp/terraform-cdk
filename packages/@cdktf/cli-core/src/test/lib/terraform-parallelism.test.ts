723eb5dd9a1e92a92210da6a7bc8664a2d70a012
Ansgar Mertens <ansgar@hashicorp.com>
2023-12-05 14:32:26 +0100

    chore: update CI to run against TF 1.5 and 1.4

--- packages/@cdktf/cli-core/src/test/lib/terraform-parallelism.test.ts
+++ packages/@cdktf/cli-core/src/test/lib/terraform-parallelism.test.ts
@@ -72,7 +72,7 @@ const stackWithName = (name: string) => {
           },
         },
       },
-      terraformVersion: "1.2.8",
+      terraformVersion: "1.5.5",
       variables: {},
       outputs: {},
       resources: [
