68d2abce85059686f406c185986b5bbe8fc97f59
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-12-20 00:50:30 +0100

    chore: Update snapshots for multiple-stacks integration test

--- test/python/multiple-stacks/test.ts
+++ test/python/multiple-stacks/test.ts
@@ -1,6 +1,6 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
-import { TestDriver } from "../../test-helper";
+import { TestDriver, onlyHcl, onlyJson } from "../../test-helper";
 
 describe("python full integration test synth", () => {
   let driver: TestDriver;
@@ -10,13 +10,23 @@ describe("python full integration test synth", () => {
     await driver.setupPythonProject();
   });
 
-  test("synth generates JSON for both stacks", async () => {
+  onlyJson("synth generates JSON for both stacks", async () => {
     await driver.synth();
     expect(
       driver.synthesizedStack("python-simple-one").toString()
     ).toMatchSnapshot();
     expect(
       driver.synthesizedStack("python-simple-two").toString()
+    ).toMatchSnapshot();
+  });
+
+  onlyHcl("synth generates HCL for both stacks", async () => {
+    await driver.synth();
+    expect(
+      driver.synthesizedStackContentsRaw("python-simple-one").toString()
+    ).toMatchSnapshot();
+    expect(
+      driver.synthesizedStackContentsRaw("python-simple-two").toString()
     ).toMatchSnapshot();
   });
 });
