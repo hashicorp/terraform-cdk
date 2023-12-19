bf67cc450472dafc3eb9a99c3b0497b02ea6da75
Ansgar Mertens <ansgar@hashicorp.com>
2023-12-19 16:21:29 +0100

    chore: improve test, use relative paths for get instead of file:// absolute thing

--- test/typescript/modules-relative-paths/cdktf-project/main.ts
+++ test/typescript/modules-relative-paths/cdktf-project/main.ts
@@ -1,13 +1,16 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
-import { App, TerraformStack, Testing, LocalBackend } from "cdktf";
+import { App, TerraformStack, Testing } from "cdktf";
+import { Constants } from './.gen/modules/constants';
+import { Register } from './.gen/modules/register';
 
 export class HelloTerra extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // TODO: put code here
+    new Constants(this, "constants", {});
+    new Register(this, "register", {});
   }
 }
 
