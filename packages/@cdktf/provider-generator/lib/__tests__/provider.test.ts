12a52bc7fcd737d559f2d01b2fe146919d5e6268
Mark DeCrane <mark.decrane@hashicorp.com>
2024-01-02 14:54:26 -0500

    chore: add test

--- packages/@cdktf/provider-generator/lib/__tests__/provider.test.ts
+++ packages/@cdktf/provider-generator/lib/__tests__/provider.test.ts
@@ -42,6 +42,29 @@ function directorySnapshot(root: string) {
   return output;
 }
 
+function resourceTypesPresentInSnapshot(
+  snapshot: SynthOutput,
+  providerNameInPath: string
+) {
+  const resources: string[] = [];
+  const files = Object.keys(snapshot);
+  for (const file of files) {
+    const match = file.match(
+      `/providers\/${providerNameInPath}\/(.*?)\/index\.ts/`
+    );
+    // avoids any not resources from being pushed
+    if (
+      match &&
+      !match[1].includes("/") &&
+      !match[1].includes("data-") &&
+      !match[1].includes("provider")
+    ) {
+      resources.push(match[1]);
+    }
+  }
+  return resources;
+}
+
 describe("Provider", () => {
   it("generates a provider", async () => {
     const constraint = new TerraformProviderConstraint(
@@ -76,20 +99,9 @@ describe("Provider", () => {
         await maker.generate([constraint]);
         const snapshot = directorySnapshot(workdir);
 
-        const terraformResourceTypesPresent: string[] = [];
-        const files = Object.keys(snapshot);
-        for (const file of files) {
-          const match = file.match(/providers\/datadog\/(.*?)\/index\.ts/);
-          // avoids any not resources from being pushed
-          if (
-            match &&
-            !match[1].includes("/") &&
-            !match[1].includes("data-") &&
-            !match[1].includes("provider")
-          ) {
-            terraformResourceTypesPresent.push(match[1]);
-          }
-        }
+        const terraformResourceTypesPresent: string[] =
+          resourceTypesPresentInSnapshot(snapshot, "datadog");
+
         terraformResourceTypesPresent.forEach((resource) => {
           let terraformResourceType = resource.replace(/-/g, "_");
           if (!terraformResourceType.includes("datadog")) {
@@ -104,4 +116,33 @@ describe("Provider", () => {
         });
       });
     }, 600_000);
+  it("has name in constraint that does not match resolved name in fqpn", async () => {
+    const constraint = new TerraformProviderConstraint({
+      name: "dockerr",
+      source: "registry.terraform.io/kreuzwerker/docker",
+      version: "3.0.2",
+    });
+    return await mkdtemp(async (workdir) => {
+      const jsiiPath = path.join(workdir, ".jsii");
+      const maker = new ConstructsMaker(
+        {
+          codeMakerOutput: workdir,
+          outputJsii: jsiiPath,
+          targetLanguage: Language.TYPESCRIPT,
+        },
+        process.env.CDKTF_EXPERIMENTAL_PROVIDER_SCHEMA_CACHE_PATH
+      );
+      await maker.generate([constraint]);
+      const snapshot = directorySnapshot(workdir);
+
+      const terraformResourceTypesPresent: string[] =
+        resourceTypesPresentInSnapshot(snapshot, "dockerr");
+
+      terraformResourceTypesPresent.forEach((resource) => {
+        expect(
+          snapshot[`providers/dockerr/${resource}/index.ts`]
+        ).toBeDefined();
+      });
+    });
+  }, 600_000);
 });
