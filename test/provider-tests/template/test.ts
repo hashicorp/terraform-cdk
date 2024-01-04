2e152cdefb33e1c00554f2ca77dbd2bf94e43a01
Ansgar Mertens <ansgar@hashicorp.com>
2024-01-04 16:47:52 +0100

    fix: run test only for json output

--- test/provider-tests/template/test.ts
+++ test/provider-tests/template/test.ts
@@ -1,16 +1,20 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
-import { TestDriver } from "../../../test-helper";
+import { onlyJson, TestDriver } from "../../test-helper";
 
 describe("full integration test", () => {
   describe("deploy", () => {
-    test("build providers", async () => {
-      const driver = new TestDriver(__dirname);
-      await driver.setupTypescriptProject();
-      await driver.synth();
+    onlyJson(
+      "build providers",
+      async () => {
+        const driver = new TestDriver(__dirname);
+        await driver.setupTypescriptProject();
+        await driver.synth();
 
-      const stack = driver.synthesizedStack("using-all-providers");
-      expect(stack.byId("test")).toBeTruthy();
-    }, 600_000);
+        const stack = driver.synthesizedStack("using-all-providers");
+        expect(stack.byId("test")).toBeTruthy();
+      },
+      600_000
+    );
   });
 });
