7f4dad6ab80d5c867b883dd5df23a0a026a5621d
Mark DeCrane <mark.decrane@hashicorp.com>
2024-01-05 10:49:58 -0500

    chore: linting

--- test/typescript/modules-relative-paths/cdktf-project/main.ts
+++ test/typescript/modules-relative-paths/cdktf-project/main.ts
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
 import { App, TerraformStack, Testing } from "cdktf";
-import { Constants } from './.gen/modules/constants';
-import { Register } from './.gen/modules/register';
+import { Constants } from "./.gen/modules/constants";
+import { Register } from "./.gen/modules/register";
 
 export class HelloTerra extends TerraformStack {
   constructor(scope: Construct, id: string) {
