3f69af24a514eb1266c5b5af426cfe19b606c1d8
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-01-08 13:51:15 +0100

    chore: improve readability of findCommonLowestPath

--- packages/cdktf/test/terraform-module-asset.test.ts
+++ packages/cdktf/test/terraform-module-asset.test.ts
@@ -8,11 +8,19 @@ import { findLowestCommonPath } from "../lib/terraform-module-asset";
 describe("TerraformModuleAsset", () => {
   describe("findLowestCommonPath", () => {
     it.each([
-      { paths: ["./foo/bar", "./foo/baz", "./foo"], expected: "./foo/" },
-      { paths: ["../fuzz", "./foo/baz", "./foo/"], expected: "../" },
+      { paths: [], expected: undefined },
+      { paths: ["./"], expected: "." },
+      { paths: ["./foo/bar", "./foo/baz", "./foo"], expected: "foo" },
+      { paths: ["../fuzz", "./foo/baz", "./foo/"], expected: ".." },
     ])(
-      "should find the lowest common path",
-      ({ paths, expected }: { paths: string[]; expected: string }) => {
+      "should find the lowest common path for $paths expecting $expected",
+      ({
+        paths,
+        expected,
+      }: {
+        paths: string[];
+        expected: string | undefined;
+      }) => {
         expect(findLowestCommonPath(paths)).toEqual(expected);
       }
     );
