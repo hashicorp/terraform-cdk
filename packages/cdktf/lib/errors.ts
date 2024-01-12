4a1447021e7cc344e39196673d0558f6daa53b8e
Rose <rose.koron@hashicorp.com>
2024-01-12 14:26:21 -0800

    Half way through error codes

--- packages/cdktf/lib/errors.ts
+++ packages/cdktf/lib/errors.ts
@@ -8,58 +8,59 @@ import { TerraformStack } from "./terraform-stack";
 
 export const noAppFound = (constructPath: string) =>
   new Error(
-    `No app could be identified for the construct at path '${constructPath}', likely a TerraformStack.
-The scope of CDKTF's TerraformStack class is a single App instance created by 'const app = new App()'. The App is the root of your project that holds project configuration and validations.
-You can learn more about the App here: https://developer.hashicorp.com/terraform/cdktf/concepts/cdktf-architecture#app-class:~:text=and%20Resource.-,App%20Class,-Each%20CDKTF%20project
+    `No app could be identified for the construct at path '${constructPath}' (likely a TerraformStack).
+The scope of CDKTF's TerraformStack class is a single App instance created by 'const app = new App()'. The App is your project's root, holding project configuration and validations.
+Learn more about the App: https://developer.hashicorp.com/terraform/cdktf/concepts/cdktf-architecture#app-class:~:text=and%20Resource.-,App%20Class,-Each%20CDKTF%20project
     `
   );
 
 export const appValidationFailure = (errorList: string) =>
   new Error(
-    `App level validation failed with the following errors:\n  ${errorList}
+    `App-level validation failed with the following errors:\n  ${errorList}
 Validations allow for dynamic verification of your project.
-To skip validations, add 'skipValidation: true' to your App config
+To skip validations, add 'skipValidation: true' to your App config.
     `
   );
 
 export const providerVersionMismatch = () =>
   new Error(
-    `Version mismatch detected: The provider bindings seem to have been built for an older version of CDKTF. 
+    `Version mismatch detected: Your provider bindings are built for an older version of CDKTF. 
 Upgrade your pre-built provider or re-run cdktf get with a more recent version (>= 0.10) of the cdktf-cli.
     `
   );
 
 export const assetOutOfScopeOfCDKTFJson = (id: string, configPath: string) =>
   new Error(
-    `TerraformAsset ${id} was created with a relative path '${configPath}', but no cdktf.json file was found to base it on.
+    `TerraformAsset ${id} was created with a relative path '${configPath}', but we cannot find the cdktf.json within your current working directory '${process.cwd()}'
 
-The cdktf.json is needed to establish the base for the relative path (e.g the '.' in './foo/bar'). We cannot find the cdktf.json within your current working directory '${process.cwd()}'
-To avoid this, please place a cdktf.json at the root of your project. Learn more here: https://developer.hashicorp.com/terraform/cdktf/create-and-deploy/configuration-file
-To learn more about TerraformAsset read here: https://developer.hashicorp.com/terraform/cdktf/concepts/assets
+The cdktf.json file is needed to establish the base for the relative path (the '.' in './foo/bar'). 
+
+Place a cdktf.json at the root of your project. Learn more: https://developer.hashicorp.com/terraform/cdktf/create-and-deploy/configuration-file
+Learn more about TerraformAsset: https://developer.hashicorp.com/terraform/cdktf/concepts/assets
 `
   );
 
 export const assetExpectsDirectory = (id: string, configPath: string) =>
   new Error(
-    `TerraformAsset ${id} expects path to be a directory, a file was passed: '${configPath}'
-To learn more about TerraformAsset read here: https://developer.hashicorp.com/terraform/cdktf/concepts/assets
+    `TerraformAsset ${id} expects path to point to a directory. Instead, this file was passed: '${configPath}'. Please correct the path to point to a directory.
+Learn more about TerraformAsset: https://developer.hashicorp.com/terraform/cdktf/concepts/assets
     `
   );
 
 export const assetTypeNotImplemented = () =>
-  new Error(`Asset type is not implemented
-Possible Asset types include: FILE, DIRECTORY, and ARCHIVE
-The values are to be taken from the AssetType Enum.
-To learn more about TerraformAsset read here: https://developer.hashicorp.com/terraform/cdktf/concepts/assets
+  new Error(`Asset type is not implemented.
+Possible Asset types include: FILE, DIRECTORY, and ARCHIVE.
+Take one of these values from the AssetType Enum.
+Learn more about TerraformAsset: https://developer.hashicorp.com/terraform/cdktf/concepts/assets
   `);
 
 export const dynamicBlockNotSupported = (_foreachExpression: string) =>
   new Error(
-    `Tried to directly resolve a TerraformDynamicBlock which is not supported.
-The TerraformDynamicBlock is created by calling '.dynamic' on a TerraformIterator instance.
-Dynamic blocks are only supported on block attributes of resources, data sources and providers.
+    `We do not support directly resolving a TerraformDynamicBlock. Dynamic blocks are only supported on block attributes of resources, data sources, and providers.
 
-The expression used for the dynamic block: .dynamic({
+You can create a TerraformDynamicBlock by calling '.dynamic' on a TerraformIterator instance.
+
+The following expression was used for the dynamic block: .dynamic({
   ${_foreachExpression}
 })
 `
@@ -67,44 +68,41 @@ The expression used for the dynamic block: .dynamic({
 
 export const unresolvedTokenInConstructId = (id: string) =>
   new Error(
-    `You cannot use a Token (e.g. a reference to an attribute) as the id of a construct.
-The following id was passed: "${id}"
-Ids of constructs must be known at synthesis time and the value of tokens is only known when Terraform runs, hence you cannot use tokens in construct ids.`
+    `You cannot use a token (e.g., a reference to an attribute) as the id of a construct. Ids of constructs must be known at synthesis time, and token values are only known when Terraform runs. Please use a concrete value for your constructs ID instead. 
+You passed the following id: "${id}"
+`
   );
 
 export const iteratorOnResourceWithCount = () =>
   new Error(
-    `Cannot create iterator from a resource with a count argument.
-The resource passed to the iterator has a count argument which determines how many instances of the resource are created.
-Please re-use the same TerraformCount used in this resource on the resource where you planned to use this iterator instead.
+    `You cannot create an iterator from a resource with a count argument.
+Instead, reuse this resource's TerraformCount instance in the resource you want to use as an iterator.
 
-If you need to use the iterator to populate a list attribute, replace the count on the resource with an iterator passed into the forEach argument.`
+If you want to use an iterator to populate a list attribute, replace the count on the resource with an iterator passed into the forEach argument.`
   );
 
 export const iteratorOnResourceWithoutForEach = () =>
-  new Error(`Cannot create an iterator from resource without a forEach argument.
-The resource passed to the iterator does not have a forEach argument, meaning only a single instance of it will be created.
-If you want to create more instances of this resource pass an iterator to the forEach argument of the resource first.
+  new Error(`Cannot create an iterator from a resource without a forEach argument. If you want to create more instances of this resource, pass an iterator to the forEach argument of the resource first.
   `);
 
 export const modulesWithSameAlias = (alias: string) =>
-  new Error(`Error: Multiple providers have the same alias: "${alias}"
-When passing multiple providers of the same type to modules, each provider must have a unique alias.
+  new Error(`Multiple providers have the same alias: "${alias}"
+Each provider must have a unique alias when passing multiple providers of the same type to modules.
   `);
 
 export const moveTargetAlreadySet = (
   target: string,
   friendlyUniqueId: string | undefined
 ) =>
-  new Error(`Target "${target}" has already been used for the construct at ${friendlyUniqueId}.
+  new Error(`Target "${target}" has already been used for the construct ${friendlyUniqueId}.
 Target must be a string that is unique across all resources in the same stack.`);
 
 export const moveTargetNotSet = (target: string, entries: string) =>
   new Error(`Target "${target}" has not been set.
-You tried to move a resource to a target that has not been set on any resource.
-Call .addMoveTarget("${target}") on the instance of the resource you want to move this one to.
+You tried to move a resource without a target.
+Call .addMoveTarget("${target}") on the instance of the resource you want to move this resource to.
 
-Alternatively you can use one of the following targets (in case you had a typo):
+Alternatively, you can use one of the following targets (in case you had a typo):
 ${entries}
 `);
 
@@ -113,14 +111,14 @@ export const movedToResourceOfDifferentType = (
   originalResourceType: string,
   destinationResourceType: string
 ) =>
-  new Error(`You have tried to move a resource to a different type which is not supported.
+  new Error(`We do not support moving resources to a different type.
 
-The move target "${moveTarget}" corresponding to the resource of type ${destinationResourceType} to move to differs from the resource of type ${originalResourceType} being moved from.
+The destination "${moveTarget}"'s resource type ${destinationResourceType} differs from the original resource type (${originalResourceType}) you are trying to move.
 
-If this was intentional, you can instead try importing the existing resource into your target as described in the import docs:
+If this was intentional, try importing the existing resource into your target instead. Learn more:
 https://developer.hashicorp.com/terraform/cdktf/concepts/resources#importing-resources
 
-Please note, that you will manually need to remove the original resource from your state file after importing it, using the "terraform state rm" command in the output directory of the stack this resource is defined in. Read more about the command here:
+Note that you must manually remove the original resource from your state file after importing it. Use the "terraform state rm" command in the output directory of the stack you defined this resource in. Learn more:
 https://developer.hashicorp.com/terraform/cli/commands/state/rm
 `);
 
@@ -130,7 +128,7 @@ export const resourceGivenTwoMoveOperationsByTargetAndId = (
   newMove: { to: string; from: string }
 ) =>
   new Error(`
-${id} has been given two separate moved operations.
+The resource ${id} has been given two different move operations.
 
 Move target: "${existingTarget}"
 Move by id: {
@@ -140,7 +138,7 @@ Move by id: {
 
 Only one move operation can occur per plan/apply. Remove one of the operations.
 
-To learn more about moving resources see: https://developer.hashicorp.com/terraform/cdktf/examples-and-guides/refactoring#:~:text=Moving%20%26%20Renaming%20Resources%20Within%20a%20Stack
+Learn more about moving resources: https://developer.hashicorp.com/terraform/cdktf/examples-and-guides/refactoring#:~:text=Moving%20%26%20Renaming%20Resources%20Within%20a%20Stack
 `);
 
 export const resourceGivenTwoMoveOperationsById = (
@@ -149,7 +147,7 @@ export const resourceGivenTwoMoveOperationsById = (
   newMove: { to: string; from: string }
 ) =>
   new Error(`
-${id} has been given two separate moved operations.
+The resource ${id} has been given two different move operations.
 
 {
   from: ${existingMove.from}
@@ -162,7 +160,7 @@ ${id} has been given two separate moved operations.
 
 Only one move operation can occur per plan/apply. Remove one of the operations.
 
-To learn more about moving resources see: https://developer.hashicorp.com/terraform/cdktf/examples-and-guides/refactoring#:~:text=Moving%20%26%20Renaming%20Resources%20Within%20a%20Stack
+Learn more about moving resources: https://developer.hashicorp.com/terraform/cdktf/examples-and-guides/refactoring#:~:text=Moving%20%26%20Renaming%20Resources%20Within%20a%20Stack
 `);
 
 export const resourceGivenTwoMoveOperationsByTarget = (
@@ -170,28 +168,26 @@ export const resourceGivenTwoMoveOperationsByTarget = (
   existingMoveTarget: string,
   newMoveTarget: string
 ) =>
-  new Error(`The resource ${resourceId} has been given two moveTargets: "${newMoveTarget}" and "${existingMoveTarget}"
+  new Error(`The resource ${resourceId} has been given two different moveTargets: "${newMoveTarget}" and "${existingMoveTarget}"
 
-A resource can only be moved once per plan/apply
+A resource can only be moved once per plan/apply.
 
-To learn more about moving resources see: https://developer.hashicorp.com/terraform/cdktf/examples-and-guides/refactoring#:~:text=Moving%20%26%20Renaming%20Resources%20Within%20a%20Stack
+Learn more about moving resources: https://developer.hashicorp.com/terraform/cdktf/examples-and-guides/refactoring#:~:text=Moving%20%26%20Renaming%20Resources%20Within%20a%20Stack
 `);
 
 export const stackContainsDisallowedChar = (
   stackId: string,
   invalidChar: string
 ) =>
-  new Error(`Can not create Terraform stack with id "${stackId}". It contains a glob character: "${invalidChar}"
+  new Error(`Cannot create Terraform stack with id "${stackId}". It contains an invalid glob character: "${invalidChar}".
 
-Glob characters are disallowed in stack names due to their use in deploying/destroying multiple stacks 
-
-Such as 'cdktf deploy \`*-production\`' which deploys all stacks with names that end in '-production'
+Glob characters are not supported in stack names because we use them to deploy and destroy multiple stacks. For example, 'cdktf deploy \`*-production\`' deploys all stacks with names that end in '-production'.
 `);
 
 export const stackIdContainsWhitespace = (stackId: string) =>
-  new Error(`Can not create TerraformStack with id "${stackId}". It contains a whitespace character.
+  new Error(`Cannot create TerraformStack with id "${stackId}". It contains a whitespace character.
 
-Please remove any whitespace characters in your TerraformStack id like so: "${stackId.replace(
+Please remove any whitespace characters in your TerraformStack id, like so: "${stackId.replace(
     /\s/g,
     ""
   )}"
@@ -200,26 +196,26 @@ Please remove any whitespace characters in your TerraformStack id like so: "${st
 export const noStackForConstruct = (constructPath: string, hint: string) =>
   new Error(`No stack could be identified for the construct at path '${constructPath}'${hint}
 
-Constructs can only be used as a part of a TerraformStack. While Constructs represent a collection of infrastructure, they must be used within a TerraformStack to be apart of a dedication Terraform configuration.
+You can only use constructs as part of a TerraformStack. 
 
-To learn more about Constructs vs. TerraformStacks see here: https://developer.hashicorp.com/terraform/cdktf/concepts/constructs#:~:text=Constructs%20vs.%20Stacks
+To learn more about Constructs vs. TerraformStacks, refer to: https://developer.hashicorp.com/terraform/cdktf/concepts/constructs#:~:text=Constructs%20vs.%20Stacks
 `);
 
 export const stackHasCircularDependency = (
   thisStack: TerraformStack,
   dependency: TerraformStack
 ) =>
-  new Error(`Can not add dependency ${dependency} to ${thisStack} since it would result in a loop
+  new Error(`Cannot add dependency ${dependency} to ${thisStack}, because it would cause a circular dependency.
 
-This is caused by the TerraformStack ${dependency} already being dependent on ${thisStack} directly, or dependent on a TerraformStack within ${thisStack}
+TerraformStack ${dependency} is already dependent on ${thisStack} directly, or dependent on a TerraformStack within ${thisStack}.
   
-To learn more about cross-stack references see here: https://developer.hashicorp.com/terraform/cdktf/concepts/stacks#:~:text=Cross%2DStack%20References
+To learn more about circular dependency references, refer to: https://developer.hashicorp.com/terraform/cdktf/concepts/stacks#:~:text=Cross%2DStack%20References
 `);
 
 export const stackValidationFailure = (errorList: string) =>
   new Error(`Validation failed with the following errors:\n  ${errorList}
   
-If you wish to ignore these validations, pass 'skipValidation: true' to your App config
+If you wish to ignore these validations, pass 'skipValidation: true' to your App configuration.
 `);
 
 export const matchersPathIsNotDirectory = (functionName: string) =>
