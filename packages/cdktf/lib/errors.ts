1da5c554062bc5562399156d132c2a1afe91cac4
Rose <rose.koron@hashicorp.com>
2024-01-12 16:52:00 -0800

    Finish reviewing error codes

--- packages/cdktf/lib/errors.ts
+++ packages/cdktf/lib/errors.ts
@@ -368,34 +368,55 @@ export const cannotCalcIdForEmptySetOfComponents = () =>
     
     This happens when you attempt to create a unique id, but do not pass in any construct node ids. This means your construct likely has no parent, which is not allowed. 
     
-    Please make sure your construct has a parent, for example: new App(this) or new TerraformStack(this, 'stack').`
+    Please make sure your construct has a parent. For example: 'new App(this)' or 'new TerraformStack(this, 'stack')'.`
   );
 
 export const stringValueAddedToReferenceList = (listToken: string[]) =>
   new Error(
-    `Cannot add elements to list token, got: ${listToken}. We expect the elements of a tokenized string array to be a single string token, e.g. ["&{TfToken[Token.1]}"]. In this case the one element in the array consist of more than one value. To add values to a tokenized list use Terraform Functions instead, e.g. Fn.concat([yourReferencedList, ["my", "new", "items"]]).`
+    `Cannot add elements to list token, got: ${listToken}. 
+    
+    We expect the elements of a tokenized string array to be a single token string. For example, ["&{TfToken[Token.1]}"]. 
+    
+    One element in your array consists of more than a single value. To add values to a tokenized list, use Terraform functions instead. For example: 'Fn.concat([yourReferencedList, ["my", "new", "items"]])'.`
   );
 
 export const cannotConcatenateStringsInTokenizedStringArray = (
   listToken: string
 ) =>
   new Error(
-    `Cannot concatenate strings in a tokenized string array, got: ${listToken}. We expect the elements of a tokenized string array to be a single string token, e.g. ["&{TfToken[Token.1]}"]. In this case the one element in the array consist of something different than a single token, likely by mutating the value inside this list. This leads to CDKTF not being able to resolve the token into the appropriate runtime value. Please don't mutate tokenized string arrays.`
+    `Cannot concatenate strings in a tokenized string array, got: ${listToken}.
+    
+    We expect the elements of a tokenized string array to be a single token string. For example, ["&{TfToken[Token.1]}"]. One element in your array consists of something other than a single token. 
+    
+    You are likely mutating the value inside your array, which CDKTF cannot resolve into the appropriate runtime value.`
   );
 
 export const numberValueAddedToReferenceList = (listToken: number[]) =>
   new Error(
-    `Cannot add elements to list token, got: ${listToken}. Tokens are used to represent runtime values (e.g. references to attributes of resource or data sources), they don't contain the values since the values are only available at apply time, whereas the Tokens are present during compile time. You tried to add a value to a tokenized list, this should be done via Terraform function: Fn.concat([yourReferencedList, [42, 43, 44]]).`
+    `Cannot add elements to list token, got: ${listToken}. 
+    
+    We use tokens to represent runtime values (e.g., references to attributes of resource or data sources). List tokens don't contain the actual values because those are only available at apply time, whereas tokens are present during compile time. 
+    
+    If you want to add a value to a tokenized list, use a Terraform function instead. For example: Fn.concat([yourReferencedList, [42, 43, 44]]).`
   );
 
 export const mapValueAddedToReferenceList = (mapToken: string) =>
   new Error(
-    `Cannot add elements to map token, got: ${mapToken}. This can happen if you merge multiple maps or if you add a new key value pair to the map token. Tokens are used to represent runtime values (e.g. references to attributes of resource or data sources), they don't contain the values since the values are only available at apply time, whereas the Tokens are present during compile time. Instead of adding the value to the tokenized map you need to use a Terraform Function, e.g. Fn.merge([yourReferencedMap, { your: 'value' }]) that creates a new map Token for a value that represents what you want to express. For example this might get rendered as merge(my_resource.resource_name.one_specific_map_attribute, { your: ' value' })`
+    `Cannot add elements to map token, got: ${mapToken}. This can happen if you merge multiple maps, or if you add a new key value pair to the map token.
+    
+    We use tokens to represent runtime values (e.g., references to attributes of resource or data sources). List tokens don't contain the actual values because those are only available at apply time, whereas tokens are present during compile time. 
+    
+    Instead of adding the value to the tokenized map, use a Terraform function. For example: Fn.merge([yourReferencedMap, { your: 'value' }]). This creates a new map Token for a value, which could get rendered as:
+    merge(my_resource.resource_name.one_specific_map_attribute, { your: ' value' })`
   );
 
 export const cannotConcatenateStringsInTokenizedMap = (tokenizedMap: any) =>
   new Error(
-    `Cannot concatenate strings in a tokenized map, got: ${tokenizedMap}. We expect the key of a tokenized map to be a string token, e.g. { "&{TfToken[Token.1]}": "String Map Token Value" }. In this case the key in the map consists of more than a string token (e.g. the Token and a static string or multiple Token). This can happen if you mutate the key of the map, which is not allowed. Please don't mutate tokenized Maps as it makes it impossible for CDKTF to translate them to their intended values.`
+    `Cannot concatenate strings in a tokenized map, got: ${tokenizedMap}.
+    
+    We expect the elements of a tokenized string array to be a single token string. For example, ["&{TfToken[Token.1]}"]. A key in your map consists of more than a string token (e.g., the Token and a static string or multiple Tokens).
+    
+    This can happen if you mutate the key of the map. Do not mutate tokenized Maps, because it makes it impossible for CDKTF to translate them.`
   );
 
 export const doesNotImplementDependableTrait = (instance: IDependable) =>
@@ -408,7 +429,11 @@ export const canOnlyEncodePositiveIntegers = () =>
 
 export const indexTooLargeToEncode = (index: number) =>
   new Error(
-    `Got an index too large to encode into a Token :${index}. This happens if you have an enormous amount of Tokens in a single CDKTF application. Most likely you are creating numeric tokens in an infinite loop, e.g. by calling 'Token.asNumber()' a lot. If you reach the maximum number of allowed Tokens (a limitation imposed by the way we encode Tokens / separate them from real numeric values) you need to work around this issue by splitting up your application into multiple applications.`
+    `Received an index too large to encode into a Token :${index}. 
+    
+    This happens if you have too many Tokens in a single CDKTF application. This can also occur if you are accidentally creating numeric tokens in an infinite loop. For example, calling 'Token.asNumber()' too many times.
+    
+    If you reach the maximum number of allowed Tokens (a limitation imposed by the way we encode Tokens and separate them from real numeric values) you can work around this issue by dividing a single application into multiple applications.`
   );
 
 export const argToIntrinsicMustBePlainValue = (
@@ -416,9 +441,9 @@ export const argToIntrinsicMustBePlainValue = (
   creationStack: string[]
 ) =>
   new Error(
-    `You can only use a plain value (not a function) when creating an Intrinsic token. We got the value '${value}' created at:\n${creationStack.join(
+    `You can only use a plain value (not a function) while creating an Intrinsic token. We received the value '${value}' created at:\n${creationStack.join(
       "\n"
-    )}. If you want to use a function, please use the Lazy class, e.g. Lazy.anyValue({ produce: () => "Hello World" }).`
+    )}. If you want to use a function, please use the Lazy class. For example:  Lazy.anyValue({ produce: () => "Hello World" }).`
   );
 
 export const intrinsicNewError = (message: string, createdAt: string) =>
@@ -426,39 +451,39 @@ export const intrinsicNewError = (message: string, createdAt: string) =>
 
 export const unableToResolveCircularReference = (pathName: string) =>
   new Error(`Unable to resolve object tree with circular reference at '${pathName}'.
-This error is thrown if the depth of the object tree is greater than 200 to protect against cyclic references.
-To resolve this inspect the construct creating the cyclic reference (most likely in '${pathName}') and make sure
-it does not create an infinite nesting of constructs.`);
+
+The depth of your object tree is over 200, indicating you probably have a cyclical reference. Inspect the construct creating this cyclic reference (most likely in '${pathName}') and ensure it does not infinitely nest constructs.`);
 
 export const cannotResolveFunction = (pathName: string, obj: any) =>
-  new Error(`Trying to resolve a non-data object (e.g. a function) at '${pathName}': ${obj}. Only tokens are supported for lazy evaluation.
-If you want to have a lazily computed value, please use the Lazy class, e.g. Lazy.stringValue({ produce: () => "Hello World" })`);
+  new Error(`Trying to resolve a non-data object (e.g., a function) at '${pathName}': ${obj}. Only tokens are supported for lazy evaluation.
 
-const LIST_ERROR_EXPLANATION = `In CDKTF we represent lists where the value is only known at runtime (versus compile / synth time) as
-Arrays with a single element that is a string token, e.g. ["Token.1"]. This is because at compile time we
-don't know the length of the list, so far CDKTF did not invoke Terraform to communicate with the cloud provider.
-This is done at a later stage on the synthesized static JSON file.
-As we don't know the length of the list not the content we can not differentiate if the list was accessed at the first index,
-the last index, or as part of a loop. To avoid this ambiguity:
+If you want to use a lazily computed value, use the Lazy class. For example:  Lazy.stringValue({ produce: () => "Hello World" })`);
 
-- If you want to access a singular item use 'Fn.element(list, 0)' (not 'list[0]')
-- If you want to loop over the list use 'TerraformIterator.fromList(list)' (not 'for (const item of list)' or 'list.forEach(item => ...)')
+const LIST_ERROR_EXPLANATION = `In CDKTF, we represent lists, with values unknown until after runtime, as arrays with a single element— a string token (["Token.1"]).
 
-To learn more about tokens see https://developer.hashicorp.com/terraform/cdktf/concepts/tokens
-To learn more about iterators see https://developer.hashicorp.com/terraform/cdktf/concepts/iterators`;
 
-const MAP_ERROR_EXPLANATION = `In CDKTF we represent maps where the value is only known at runtime (versus compile / synth time) as
-objects with a single key-value pair where the value is a string token, e.g. { "&{TfToken[Token.1]}": "String Map Token Value" }.
-This is because the length of the list isn't know at compile time, as CDKTF has not yet invoked Terraform to communicate with the cloud provider.
-This is done at a later stage on the synthesized static JSON file.
-As we don't know the contents of the map we do not know which key was accessed, or if the map was accessed as part of a loop.
-To avoid this ambiguity:
+We do this because CDKTF does not know the length of the list at compile time, meaning CDKTF has yet to invoke Terraform to communicate with the cloud provider. CDKTF later invokes Terraform on the synthesized static JSON file.
 
-- If you want to access a singular item use 'Fn.lookup(map, key)' (not 'map[key]')
-- If you want to loop over the map use 'TerraformIterator.fromMap(map)' (not 'for (const [key, value] of map)' or 'Object.entries(map).forEach((key, value) => ...)')
+Because we don't know the length of the list, we can not differentiate if the list was accessed at the first or last index, or as part of a loop. To avoid this ambiguity:
 
-To learn more about tokens see https://developer.hashicorp.com/terraform/cdktf/concepts/tokens
-To learn more about iterators see https://developer.hashicorp.com/terraform/cdktf/concepts/iterators`;
+- If you want to access a singular item, use 'Fn.element(list, 0)'. Do not use 'list[0]'.
+- If you want to loop over the list use 'TerraformIterator.fromList(list)'. Do not use a 'for' or 'forEach' loop.
+
+Learn more about tokens: https://developer.hashicorp.com/terraform/cdktf/concepts/tokens
+Learn more about iterators: https://developer.hashicorp.com/terraform/cdktf/concepts/iterators`;
+
+const MAP_ERROR_EXPLANATION = `
+In CDKTF, we represent maps, with values unknown until after runtime, as objects with a single key-value pair where the value is a string token. For example: '{ "&{TfToken[Token.1]}": "String Map Token Value" }'.
+
+We do this because CDKTF does not know the length of the list at compile time, meaning CDKTF has yet to invoke Terraform to communicate with the cloud provider. CDKTF later invokes Terraform on the synthesized static JSON file.
+
+Because we don't know the contents of the map, we can not differentiate if the map was accessed, or used as part of a loop. To avoid this ambiguity:
+
+- To access a singular item use 'Fn.lookup(map, key)'. Do not use 'map[key]'.
+- If you want to loop over the map use 'TerraformIterator.fromMap(map)'. Do not use a 'for' loop or iterate over 'Object.entries(map)' with 'forEach'.
+
+Learn more about tokens: https://developer.hashicorp.com/terraform/cdktf/concepts/tokens
+Learn more about iterators: https://developer.hashicorp.com/terraform/cdktf/concepts/iterators`;
 
 export const encodedListTokenInScalarStringContext = () =>
   new Error(`Found an encoded list token string in a scalar string context.
@@ -473,8 +498,12 @@ export const encodedMapTokenInScalarNumberContext = () =>
 ${MAP_ERROR_EXPLANATION}`);
 
 export const constructsCannotBeResolved = (pathName: string) =>
-  new Error(`Trying to resolve() a Construct at '${pathName}'. 
-This often means that there is an unintended cyclic dependency in your construct tree, leading to the resolution being stuck in an infinite loop which will eventually fail. This can happen if the scope passed into this construct is also part of its subtree in some way. To resolve this issue, please refactor your code to avoid this cyclic dependency by changing the scope of this construct.`);
+  new Error(`Trying to 'resolve()' a Construct at '${pathName}'. 
+
+This often means an unintended cyclic dependency in your construct tree, leading to the resolution stuck in an infinite loop that fails.
+This can happen if the scope passed into this construct is also part of its subtree. 
+
+To resolve this issue, refactor your code to avoid this cyclic dependency by changing the scope of this construct.`);
 
 export const mapKeyMustResolveToString = (
   pathName: string,
@@ -482,14 +511,14 @@ export const mapKeyMustResolveToString = (
   resolvedKey: any
 ) =>
   new Error(
-    `At "${pathName}" the key "${key}" is used in a map so it must resolve to a string, but it resolves to a ${typeof resolvedKey} with the value ${JSON.stringify(
+    `At "${pathName}" a map uses the key "${key}", meaning that key must resolve to a string. However, it resolves to a ${typeof resolvedKey} with the value ${JSON.stringify(
       resolvedKey
-    )}. This means that the token used as the key is not resolving into string, you must change the Token so that it does.`
+    )}. To fix this, change the Token to resolve to a string.`
   );
 
-const unknownTokenExplanation = `This means that you are trying to access a Token value that does not exist. This can only happen if the Token is e.g. from another CDK (i.e. AWS CDK, CDK8s, etc.) and is not known to the CDKTF Application. You either need to make the other CDK resolve this token before CDKTF tries to resolve the token or work around using this token at all. 
+const unknownTokenExplanation = `This means you are trying to access a Token value that does not exist. This can only happen if the Token is from another CDK (i.e., AWS CDK, CDK8s, etc.) and is unknown to this CDKTF Application. You either need to make the other CDK resolve this Token before CDKTF tries to resolve it, or work around using this Token at all. 
 
-If this error occurs without another CDK (or a value looking like a Token) being involved, please file a bug.`;
+If this error occurs without another CDK (or a value looking like a Token) involved, please file a bug.`;
 
 export const unknownNumberTokenFound = () =>
   new Error(
@@ -501,7 +530,7 @@ export const unrecognizedTokenKey = (key: string) =>
 
 export const IdIncludesUnresolvedTokens = (unresolvedTokens: string) =>
   new Error(
-    `This construct (or its parent construct) was configured with an ID that contains a Token: ${unresolvedTokens}. This is not allowed as IDs must be known statically during synthesis. The values of tokens are only known during apply, therefore they cannot be used in IDs. Please use a concrete value for your constructs ID instead.`
+    `This construct (or its parent construct) is configured with an ID that contains a Token: ${unresolvedTokens}. This is not allowed because IDs must be known statically during synthesis. Token values are only known during the apply stage, therefore they cannot be used in IDs. Please use a concrete value for your constructs ID instead.`
   );
 
 export const assetCanNotCreateZipArchive = (
