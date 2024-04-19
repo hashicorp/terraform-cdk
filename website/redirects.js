3a98ea8f9c6a048bb706308acdf9e13e79099408
Rose <rose.koron@hashicorp.com>
2024-04-19 12:41:34 -0700

    Add redirect file

---
+++ website/redirects.js
@@ -0,0 +1,11 @@
+/**
+ * Redirects in this file are intended to be for documentation content only. The redirects will be applied to developer.hashicorp.com.
+ */
+module.exports = [
+  // HCP Terraform rebranding
+  {
+    source: "/terraform/cdktf/create-and-deploy/terraform-cloud",
+    destination: "/terraform/cdktf/create-and-deploy/hcp-terraform",
+    permanent: true,
+  },
+];
