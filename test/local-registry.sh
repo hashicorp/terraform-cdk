109720df9e7ab2833a2d4738577238005c90f1ee
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-06-17 10:06:02 +0200

    chore: pin version

--- test/local-registry.sh
+++ test/local-registry.sh
@@ -5,20 +5,20 @@
 custom_registry_url=localhost:4873
 original_npm_registry_url=`npm get registry`
 original_yarn_registry_url=`yarn config get registry`
-default_verdaccio_package=verdaccio@^5.26.1
+default_verdaccio_package=verdaccio@5.26.1
 
 function startLocalRegistry {
   # Start local registry
   tmp_registry_log=`mktemp`
   echo "Verdaccio Registry log file: $tmp_registry_log"
-  
+
   storage_dir="$(dirname $1)/storage"
   echo "Cleaning storage dir ($storage_dir).."
   rm -rf $storage_dir
 
   nohup npx ${VERDACCIO_PACKAGE:-$default_verdaccio_package} -c $1 &>$tmp_registry_log &
   verdaccio_pid="$!"
-  
+
   # Wait for Verdaccio to boot
   echo "Waiting for local Registry to start"
   grep -q 'http address' <(tail -f $tmp_registry_log)
