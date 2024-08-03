0c036dbac395d7e6f1752cfa11457f2c9f21c815
Deniz Adrian <adrian@hisolutions.com>
2024-08-03 11:12:42 +0200

    fix(devcontainer): ensure devcontainer can be used
    without prior knowledge on if version control information should've
    been included in builds in the past: go tries to embed version control
    information by default since 1.18 (https://tip.golang.org/doc/go1.18),
    which fails during the devcontainer build. therefore, disable it.

--- packages/@cdktf/hcl-tools/build-go.sh
+++ packages/@cdktf/hcl-tools/build-go.sh
@@ -13,7 +13,7 @@ export GOWORK=off
 echo "Running go get.."
 GOOS=js GOARCH=wasm go get .
 echo "Running go build.."
-GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o main.wasm
+GOOS=js GOARCH=wasm go build -ldflags="-s -w" -buildvcs=false -o main.wasm
 echo "Zipping wasm build.."
 gzip -9 -v -c main.wasm > main.wasm.gz
 echo "Done."
