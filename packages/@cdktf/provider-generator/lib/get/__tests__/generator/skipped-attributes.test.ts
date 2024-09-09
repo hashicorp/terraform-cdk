fb4d3f540ca05609ac86990893f5563bdc4dd1fa
Ansgar Mertens <ansgar@hashicorp.com>
2024-09-09 10:46:06 +0200

    chore(tests): Add test for skipping attribute type attributes

--- packages/@cdktf/provider-generator/lib/get/__tests__/generator/skipped-attributes.test.ts
+++ packages/@cdktf/provider-generator/lib/get/__tests__/generator/skipped-attributes.test.ts
@@ -6,7 +6,7 @@ import * as path from "path";
 import { TerraformProviderGenerator } from "../../generator/provider-generator";
 import { CodeMaker } from "codemaker";
 
-test("skips attributes in the disallow list", async () => {
+test("skips block type attributes in the disallow list", async () => {
   const code = new CodeMaker();
   const workdir = fs.mkdtempSync(
     path.join(os.tmpdir(), "skip-attributes.test")
@@ -34,6 +34,41 @@ test("skips attributes in the disallow list", async () => {
   expect(
     fs.existsSync(
       path.join(workdir, "providers/aws/quicksight-template/index-structs")
+    )
+  ).toBe(false);
+});
+
+test("skips attribute type attributes in the disallow list", async () => {
+  const code = new CodeMaker();
+  const workdir = fs.mkdtempSync(
+    path.join(os.tmpdir(), "skip-attributes.test")
+  );
+  const spec = JSON.parse(
+    fs.readFileSync(
+      path.join(
+        __dirname,
+        "fixtures",
+        "data_aws_quicksight_analysis.fixture.json"
+      ),
+      "utf-8"
+    )
+  );
+  new TerraformProviderGenerator(code, spec).generateAll();
+  await code.save(workdir);
+
+  const output = fs.readFileSync(
+    path.join(workdir, "providers/aws/data-aws-quicksight-analysis/index.ts"),
+    "utf-8"
+  );
+  expect(output).toMatchSnapshot(`data-quicksight-analysis`);
+
+  // There should also be no index structs
+  expect(
+    fs.existsSync(
+      path.join(
+        workdir,
+        "providers/aws/data-aws-quicksight-analysis/index-structs"
+      )
     )
   ).toBe(false);
 });
