73d99faa2a9f632015cfe74f006b5b906cef1926
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2025-01-30 10:40:59 +0100

    chore(examples): Upgrade go version to 1.22 for example/aws, and fix problems due to AWS provider updates

--- examples/go/aws/main.go
+++ examples/go/aws/main.go
@@ -5,8 +5,8 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-cdk/examples/go/aws/generated/eks"
-	"github.com/hashicorp/terraform-cdk/examples/go/aws/generated/hashicorp/aws/awsprovider"
-	"github.com/hashicorp/terraform-cdk/examples/go/aws/generated/hashicorp/aws/ec2"
+	"github.com/hashicorp/terraform-cdk/examples/go/aws/generated/hashicorp/aws/instance"
+	"github.com/hashicorp/terraform-cdk/examples/go/aws/generated/hashicorp/aws/provider"
 
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -16,11 +16,11 @@ import (
 func NewExampleCdktfGoAwsStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
-	awsprovider.NewAwsProvider(stack, jsii.String("aws"), &awsprovider.AwsProviderConfig{
+	provider.NewAwsProvider(stack, jsii.String("aws"), &provider.AwsProviderConfig{
 		Region: jsii.String("us-east-1"),
 	})
 
-	ec2.NewInstance(stack, jsii.String("Hello"), &ec2.InstanceConfig{
+	instance.NewInstance(stack, jsii.String("Hello"), &instance.InstanceConfig{
 		Ami:          jsii.String("ami-2757f631"),
 		InstanceType: jsii.String("t2.micro"),
 		Tags: &map[string]*string{
@@ -30,7 +30,7 @@ func NewExampleCdktfGoAwsStack(scope constructs.Construct, id string) cdktf.Terr
 
 	eks.NewEks(stack, jsii.String("EKS"), &eks.EksConfig{
 		ClusterName:    jsii.String("my-eks"),
-		Subnets:        jsii.Strings("a", "b"),
+		SubnetIds:      jsii.Strings("a", "b"),
 		VpcId:          jsii.String("id"),
 		ClusterVersion: jsii.String("1.17"),
 	})
